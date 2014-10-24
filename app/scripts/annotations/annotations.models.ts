745e523c9e408488d50c7d1772b2c7922cae2674
Matthew Schranz <mschranz@oicr.on.ca>
2014-10-24 14:18:27 -0400

    feat(participant): Updates UI to new mockups.
    - Updates UI/Layout.
    - Adds proper linking back to search page for results.
    
    Closes #52

--- app/scripts/annotations/annotations.models.ts
+++ app/scripts/annotations/annotations.models.ts
@@ -4,20 +4,31 @@ module ngApp.annotations.models {
   import ICollection = ngApp.models.ICollection;
   import IEntity = ngApp.models.IEntity;
 
+  export interface INote {
+    addedBy: string;
+    dateAdded: string;
+    dateEdited: string;
+    editedBy: string;
+    id: number;
+    text: string;
+  }
+
   export interface IAnnotations extends ICollection {
     hits: IAnnotation[];
   }
 
   export interface IAnnotation extends IEntity {
-    id: string;
-    uuid: string;
-    notes: any;
-    itemId: string;
-    itemType: string;
-    participantId: string;
-    project: any;
+    classification: string;
+    approved: boolean;
     category: string;
-    created: any;
+    createdBy: string;
+    dateCreated: string;
+    id: number;
+    item: string;
+    type: string;
+    project: any;
+    rescinded: boolean;
     status: string;
+    notes: INote[];
   }
 }

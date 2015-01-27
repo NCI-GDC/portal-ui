47e42cafe26caae726299581e8386c7d730eb684
Daniel Stern <dstern@oicr.on.ca>
2015-01-27 11:07:00 -0500

    feat(table): add reset button to table
    - Adds a prominent button next to drag and drop columns
    - Clicking this button resets the column order and visibility default
    
    Closes #261

--- app/scripts/components/tables/tables.directives.ts
+++ app/scripts/components/tables/tables.directives.ts
@@ -17,10 +17,50 @@ module ngApp.components.tables.directives {
       replace: true,
       templateUrl: "components/tables/templates/arrange-columns.html",
       link:function(scope:any){
-        scope.models = {};
-        scope.onMoved = function($index){
-          scope.list.splice($index,1);
+
+        function init() {
+          scope.listMap = scope.list.map(function (elem) {
+            var composite = _.pick(elem, "id", "displayName", "hidden", "sortable", "canReorder");
+            if (!composite.canReorder === false) {
+              composite.canReoder = true;
+            }
+            return composite;
+          });
         }
+
+        init();
+
+        scope.onMoved = function($index){
+          scope.listMap.splice($index,1);
+          scope.list = scope.listMap.map(function(elem){
+            return _.find(scope.list,function(li){
+              return li.id === elem.id;
+            });
+          });
+
+          scope.list.pristine = false;
+        };
+
+        scope.$watch('listMap',function(newval){
+          if (newval) {
+
+            scope.listMap.forEach(function (listElem) {
+              var matchingHeader = _.find(scope.list, function (li) {
+                return li.id === listElem.id;
+              });
+              matchingHeader.hidden = listElem.hidden;
+            });
+          }
+        },true);
+
+
+        scope.$watch(function(){
+          return scope.list && scope.list.pristine;
+        },function(pristine){
+          if (pristine) {
+            init();
+          }
+        });
       }
     };
   }
@@ -54,10 +94,38 @@ module ngApp.components.tables.directives {
 
 
 
+  function ResetTable(): ng.IDirective {
+    return {
+      restrict: "EA",
+      scope: {
+        target:"="
+      },
+      replace:true,
+      templateUrl: "components/tables/templates/reset-table.html",
+      controller: function($scope,$element){
+        var config = $scope.target;
+        var originalOrder = _.pluck(config.headings,'id');
+        $scope.reset = function(){
+
+          config.headings = originalOrder.map(function(id){
+            var heading = _.find(config.headings,function(head){return head.id === id});
+            if (heading) {
+              heading.hidden = false;
+              return heading;
+            }
+          });
+
+          config.headings.pristine = true;
+        };
+      }
+    }
+  }
+
 
   angular.module("tables.directives", ["tables.controllers"])
       .directive("exportTable", ExportTable)
       .directive("sortTable", SortTable)
+      .directive("resetTable", ResetTable)
       .directive("arrangeColumns", ArrangeColumns);
 }
 

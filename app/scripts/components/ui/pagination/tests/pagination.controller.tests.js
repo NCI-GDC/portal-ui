53a0de289b11c064d32b1df0c869203f5deb1b22
Matthew Schranz <mschranz@oicr.on.ca>
2015-01-30 13:45:25 -0500

    chore(pagination): Add tests for pagination.
    - Adds tests focused on the PagingController used in the directive.
    
    Closes #274

---
+++ app/scripts/components/ui/pagination/tests/pagination.controller.tests.js
@@ -0,0 +1,59 @@
+describe('Pagination:', function () {
+
+  // Initialization of the AngularJS application before each test case
+  beforeEach(module('ngApp.components', 'location.services'));
+
+  describe('PagingController:', function () {
+
+    it('should update url with paging changes with no update', inject(function ($rootScope, $controller, LocationService) {
+      var scope = $rootScope.$new();
+      scope.paging = {
+        page: 1,
+        size: 10,
+        from: 1
+      };
+      scope.page = "test";
+
+      var wc = $controller('PagingController', { $scope: scope, LocationService: LocationService });
+      wc.refresh();
+
+      var paging = LocationService.pagination()[scope.page];
+      expect(paging.size).to.equal(10);
+      expect(paging.from).to.equal(1);
+    }));
+
+    it('should emit event for custom update', inject(function ($rootScope, $controller, LocationService) {
+      var scope = $rootScope.$new();
+      scope.paging = {
+        page: 1,
+        size: 10,
+        from: 1
+      };
+      scope.update = true;
+      scope.page = "test";
+
+      var wc = $controller('PagingController', { $scope: scope, LocationService: LocationService });
+      var emitCallBack = sinon.spy(scope, "$emit");
+      wc.refresh();
+      expect(emitCallBack).to.have.been.calledOnce;
+    }));
+
+    it('should update paging size', inject(function ($rootScope, $controller, LocationService) {
+      var scope = $rootScope.$new();
+      scope.paging = {
+        page: 1,
+        size: 10,
+        from: 1
+      };
+      scope.page = "test";
+
+      var wc = $controller('PagingController', { $scope: scope, LocationService: LocationService });
+      var refreshCallBack = sinon.spy(wc, "refresh");
+      wc.setCount({}, 25);
+      expect(refreshCallBack).to.have.been.calledOnce;
+      expect(scope.paging.size).to.equal(25);
+    }));
+
+  });
+
+});

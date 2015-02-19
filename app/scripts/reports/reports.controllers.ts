afdc88b34d11abfd30e6e8d6faf6d757cc587a7b
Daniel Stern <dstern@oicr.on.ca>
2015-02-19 16:15:09 -0500

    feat(reports): added graph

--- app/scripts/reports/reports.controllers.ts
+++ app/scripts/reports/reports.controllers.ts
@@ -274,5 +274,70 @@ module ngApp.reports.controllers {
             }
           }
        })
+      .directive("reportsPieChart",function(){
+          return {
+            restrict:"A",
+              scope: {
+                data:'='
+              },
+              controller:function($scope, $element){
+             var width = 450,
+                height = 250,
+                radius = Math.min(width, height) / 2;
+
+            var color = d3.scale.ordinal()
+                .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+            var arc = d3.svg.arc()
+                .outerRadius(radius - 10)
+                .innerRadius(0);
+
+            var pie = d3.layout.pie()
+                .sort(null)
+                .value(function(d) { return d.population; });
+
+            var svg = d3.select($element[0]).append("svg")
+                .attr("width", width)
+                .attr("height", height)
+              .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+//            d3.csv("data.csv", function(error, data) {
+//            debugger;
+//              var data = $scope.data;
+//              var data = [{
+//                age:25,
+//                population: 100000
+//              },{
+//                age:45,
+//                population: 76000
+//              }];
+                  
+                  var data = $scope.data.map(function(a){return {age:a.data_type,population:a.count}})
+
+              data.forEach(function(d) {
+                d.population = +d.population;
+              });
+
+              var g = svg.selectAll(".arc")
+                  .data(pie(data))
+                .enter().append("g")
+                  .attr("class", "arc");
+
+              g.append("path")
+                  .attr("d", arc)
+                  .style("fill", function(d) { return color(d.data.age); });
+
+              g.append("text")
+                  .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+                  .attr("dy", ".35em")
+                  .attr('class','pie-text')
+                  .style("text-anchor", "middle")
+                  .text(function(d) { return d.data.age; });
+
+//            });
+              }
+          }
+       })
       .controller("ReportsController", ReportsController);
 }

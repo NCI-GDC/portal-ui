d0ba54d2e174d250024ecfc5e91dd89b4c051221
Daniel Stern <dstern@oicr.on.ca>
2015-02-20 13:39:27 -0500

    feat(graph): scaffold the map

--- app/scripts/reports/reports.controllers.ts
+++ app/scripts/reports/reports.controllers.ts
@@ -37,77 +37,80 @@ module ngApp.reports.controllers {
           display_name:["File","Size"],
           scale:'ordinal',
           dimensional:true
-        },{
-          id:'file_count',
-          display_name:["File","Count"],
-          scale:'ordinal',
-          dimensional:true
-        },{
-          id:'Clinical',
-          display_name:['Clinical'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Raw microarray data',
-          display_name:['Array'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Raw sequencing data',
-          display_name:['Seq'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Simple nucleotide variation',
-          display_name:['SNV'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Copy number variation',
-          display_name:['CNV'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Structural rearrangement',
-          display_name:['SV'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Gene expression',
-          display_name:['Exp'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Protein expression',
-          display_name:['PExp'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'Other',
-          display_name:['Other'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'DNA methylation',
-          display_name:['Meth'],
-          scale:'ordinal',
-          is_subtype:true,
-          dimensional:true
-        },{
-          id:'primary_site',
-          display_name:["Primary","Site"],
-          scale:'linear',
-          dimensional:true
-        }];
+        }
+//                          ,{
+//          id:'file_count',
+//          display_name:["File","Count"],
+//          scale:'ordinal',
+//          dimensional:true
+//        },{
+//          id:'Clinical',
+//          display_name:['Clinical'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Raw microarray data',
+//          display_name:['Array'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Raw sequencing data',
+//          display_name:['Seq'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Simple nucleotide variation',
+//          display_name:['SNV'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Copy number variation',
+//          display_name:['CNV'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Structural rearrangement',
+//          display_name:['SV'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Gene expression',
+//          display_name:['Exp'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Protein expression',
+//          display_name:['PExp'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'Other',
+//          display_name:['Other'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },{
+//          id:'DNA methylation',
+//          display_name:['Meth'],
+//          scale:'ordinal',
+//          is_subtype:true,
+//          dimensional:true
+//        },
+//                          {
+//          id:'primary_site',
+//          display_name:["Primary","Site"],
+//          scale:'linear',
+//          dimensional:true
+//        }
+                         ];
           
           var aggregations = a.reduce(function(a,b){
 //            debugger;
@@ -123,6 +126,7 @@ module ngApp.reports.controllers {
             } else {
               a[b.archive.disease_code] = {
                 file_size:b.file_size,
+                project_code:b.archive.disease_code
               };
               
               a[b.archive.disease_code][b.data_type] = 1;
@@ -166,9 +170,88 @@ module ngApp.reports.controllers {
 ////          return a;
 //        },{});    
 //          
-          console.log("Aggregations from reports...",aggregations);
+//          console.log("Aggregations from reports...",aggregations);
           
 //               debugger;
+          
+          
+        var config = {
+
+          /* the id of the tag the table will be generated into */
+          container:"#pc",
+
+          /* default scale value, not useful */
+          scale:"ordinal",
+
+          /* Ordered list of columns. Only titles appearing here appear in the table */
+          columns:columns.map(function(c){return c.id}),
+
+          /* ???
+         * The value that all the other values are divided by?
+         * Has something to do with dimensions?
+         **/
+          ref:"lang_usage",
+
+          /**
+         * No idea what title_column does.
+        **/
+          title_column:"project_code",
+
+          /**
+         * Not really a scale map, more a map of what kind of column it will be.
+         * Ordinal is the more geometry-oriented choice
+         */
+          scale_map:columns.reduce(function(a,b){
+            a[b.id] = b.scale || 'ordinal';
+            return a;
+          },{}),
+
+          /**
+         * Interconnected with ref and dimension, possibly.
+         * No idea what this does, really.
+         */
+          use:{
+            "project_code":"project_code"
+          },
+
+          /**
+         * The order each column will appear in.
+         * Don't know how well this is implemented.
+         */
+          sorting:{
+            "project_code":d3.descending
+          },
+
+          /**
+         *  Don't know what "d" is here.
+         *  If defined for a column, formats the labels.
+         *  Might not be implemented anywhere.
+         */
+          formats:{
+            "primary_site":"d"
+          },
+
+          /**
+         *  Not known what this is. Any values in columns that are not in dimensions causes an error.
+         */
+          dimensions:columns.filter(function(c){return c.dimensional}).map(function(c){return c.id}),
+
+          /**
+         *  Name for each column.
+         **/
+          column_map:columns.reduce(function(a,b){
+            a[b.id] = b.display_name || ['Untitled'];
+            return a;
+          },{}),
+
+          /**
+         * Related to animation
+         */
+          duration:1000,
+        };
+        
+        $scope.githutConfig = config;
+        $scope.githutData = d3.values(aggregations);
           
           $scope.filesByProject = a.reduce(function(a,b){
              var project = b.archive.disease_code;
@@ -561,8 +644,8 @@ module ngApp.reports.controllers {
           duration:1000,
         };
         
-        $scope.githutConfig = config;
-        $scope.githutData = d3.values(aggregations);
+//        $scope.githutConfig = config;
+//        $scope.githutData = d3.values(aggregations);
 
 
 //        new ParallelCoordinates(d3.values(aggregations),config);

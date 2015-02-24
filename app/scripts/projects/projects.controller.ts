85baf156cf3b61f833963c0e777fa0013251f6df
Daniel Stern <dstern@oicr.on.ca>
2015-02-24 11:47:45 -0500

    feat(graph): correct columns order

--- app/scripts/projects/projects.controller.ts
+++ app/scripts/projects/projects.controller.ts
@@ -182,15 +182,15 @@ function githutTable(data){
         is_subtype:true,
          dimensional:true
     },{
-        id:'file_size',
-        display_name:["File","Size"],
-        scale:'ordinal',
-        dimensional:true
-    },{
         id:'file_count',
         display_name:["File","Count"],
         scale:'ordinal',
          dimensional:true
+    },{
+        id:'file_size',
+        display_name:["File","Size"],
+        scale:'ordinal',
+        dimensional:true
     },{
         id:'primary_site',
         display_name:["Primary","Site"],

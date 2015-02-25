9554e28de1125966118748a7794370b594c8167c
Matthew Schranz <mschranz@oicr.on.ca>
2015-02-25 16:20:17 -0500

    feat(app): Add NCI Warning Banner.
    - Warning banner is show if no cookie is present from
      previously pressing it or forced to be if 'showWarning'
      is present in the query string.
    
    Closes #380

--- app/scripts/core/core.controller.ts
+++ app/scripts/core/core.controller.ts
@@ -3,15 +3,27 @@ module ngApp.core.controllers {
   import INotifyService = ng.cgNotify.INotifyService;
 
   export interface ICoreController {
+    showWarning: boolean;
   }
 
   class CoreController implements ICoreController {
+    showWarning: boolean = false;
 
     /* @ngInject */
     constructor(public $scope: ng.IScope,
                 private $rootScope: ngApp.IRootScope,
                 private CartService: ICartService,
-                private notify: INotifyService) {
+                private notify: INotifyService,
+                $location: ng.ILocationService,
+                private $cookies: ng.ICookiesService) {
+
+      var showWarning = $location.search()["showWarning"];
+
+      if (showWarning) {
+        this.$rootScope.showWarning = true;
+      } else if (!$cookies["NCI-Warning"]) {
+        this.$rootScope.showWarning = true;
+      }
 
       $scope.$on("undo", (event, action) => {
         if (action === "added") {
@@ -30,12 +42,16 @@ module ngApp.core.controllers {
         this.$rootScope.$broadcast("gdc-cancel-request");
       };
 
+      this.$rootScope.closeWarning = () => {
+        this.$rootScope.showWarning = false;
+        this.$cookies["NCI-Warning"] = true;
+      };
+
     }
 
   }
 
   angular
-      .module("core.controller", [
-      ])
+      .module("core.controller", ["ngCookies"])
       .controller("CoreController", CoreController);
 }

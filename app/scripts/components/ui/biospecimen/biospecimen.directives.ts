1363d1dc5ce2d99673b9132daed8735fee88fdc8
Christine Yu <christine.yu@oicr.on.ca>
2015-03-05 17:25:40 -0500

    fix(participant): expand all biospecimen

--- app/scripts/components/ui/biospecimen/biospecimen.directives.ts
+++ app/scripts/components/ui/biospecimen/biospecimen.directives.ts
@@ -11,11 +11,28 @@ module ngApp.components.ui.biospecimen.directives {
       restrict: "E",
       replace: true,
       scope: {
-        participant: "="
+        participant: "=",
+        expanded: "="
       },
       templateUrl: "components/ui/biospecimen/templates/biospecimen.html",
       controller: "BiospecimenController as bc",
       link: ($scope: IBiospecimenScope, elem: ng.IAugmentedJQuery, attr: ng.IAttributes, ctrl: IBiospecimenController) => {
+
+        if($scope.expanded) {
+          function expandAll(obj) {
+              if(angular.isObject(obj)) {
+                  _.forEach(obj, function(v,k) {
+                      v.expanded = true;
+                      expandAll(v);
+                  });
+              }
+          }
+          _.forEach($scope.participant.samples, function(sample) {
+            sample.expanded = true;
+            expandAll(sample);
+          });
+        }
+
         $scope.expandTree = (event: any, doc: any) => {
           if (event.which === 1 || event.which === 13) {
             doc.expanded = !doc.expanded;

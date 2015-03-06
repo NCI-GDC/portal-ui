51038e0af1766efc40889872cb285e5088888e65
Daniel Stern <dstern@oicr.on.ca>
2015-03-06 12:55:22 -0500

    fix(pc): file size filtered

--- app/scripts/projects/projects.controller.ts
+++ app/scripts/projects/projects.controller.ts
@@ -299,6 +299,15 @@ function githutTable(data){
     formats:{
         "primary_site":"d"
     },
+    filters:{
+        "file_size":function bytesToSize(bytes) {
+           if(bytes == 0) return '0';
+           var k = 1000;
+           var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+           var i = Math.floor(Math.log(bytes) / Math.log(k));
+           return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+        }
+    },
 
     /**
      *  Not known what this is. Any values in columns that are not in dimensions causes an error.

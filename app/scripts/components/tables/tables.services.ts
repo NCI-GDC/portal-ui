c611a6dd63954a426ce95a2f29fad6fda13f97d2
Daniel Stern <dstern@oicr.on.ca>
2015-03-24 09:38:20 -0400

    fix(table): fix functionality for first funciton

--- app/scripts/components/tables/tables.services.ts
+++ app/scripts/components/tables/tables.services.ts
@@ -169,19 +169,18 @@ module ngApp.components.tables.services {
         /**
          * Returns the ultimate text value for an entry in a table based on the heading defintion and the whole row.
          */
-        getTemplate(heading:TableiciousColumnDefinition,field:any,row,scope, $filter: ng.IFilterService) {
-            //TODO::FIXME - field and row values in arguments are never right
-
-
+        getTemplate(scope, $filter: ng.IFilterService) {
+			
+			var result;
+			var heading = scope.heading;
+			var id = heading.id;
             var row = scope.$parent.datum;
             var field = {
-                val: this.getValueFromRow(row, heading.id),
+                val: this.getValueFromRow(row, id),
                 id: heading.id
             }
 
-            var result;
-
-            var id = heading.id;
+         
 
             var template = heading.template;
             if (heading.template) {

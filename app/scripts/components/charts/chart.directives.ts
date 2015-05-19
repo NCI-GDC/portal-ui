c14080232fde46a58860c7e1f35aeca17daa2689
Matthew Schranz <mschranz@oicr.on.ca>
2015-05-19 16:34:12 -0400

    feat(charts): Legend on hover

--- app/scripts/components/charts/chart.directives.ts
+++ app/scripts/components/charts/chart.directives.ts
@@ -155,6 +155,9 @@ module ngApp.components.charts {
       },
       templateUrl: "components/charts/templates/donut-chart.html",
       link: function($scope: IDonutChartScope, element: ng.IAugmentedJQuery) {
+        // Used to namespace each resize event
+        var id = "." + $window.Math.round($window.Math.random() * 120000);
+
         $scope.$watch('data',function(a){
           updateChart();
         });
@@ -170,37 +173,40 @@ module ngApp.components.charts {
           return getNestedValue(item, path);
         }
 
-        // Account for padding on left/right
-        var margin = { left: 30 };
-        var width = element.find(".chart-container")[0].clientWidth - margin.left;
-        var height = (parseInt($scope.height, 10) || 500);
-        var data = $scope.data;
-        var config = $scope.config;
-
-        var color = d3.scale.category20();
-        var outerRadius = height / 2 - 20,
-            innerRadius = outerRadius / 2,
-            cornerRadius = 10;
-
-        var pie = d3.layout.pie()
-            .sort(null)
-            .value(function(d) {
-              return getNestedValue(d, config.sortKey.split("."));
-            });
-
-        var arc = d3.svg.arc()
-            .padRadius(outerRadius)
-            .innerRadius(innerRadius);
-
         function updateChart() {
-          var data = $scope.data;
-          var fontSize = 18;
+          $window.$($window).off("resize" + id);
 
-          $window.jQuery($window).off("resize");
+          $window.$($window).on("resize" + id, _.debounce(() => {
+            updateChart();
+          }, 150));
 
           if (element.find(".chart-container > svg").length) {
             d3.select(element.find(".chart-container > svg")[0]).remove();
           }
+
+          // Account for padding on left/right
+          var margin = { left: 30 };
+          var width = element.find(".chart-container")[0].clientWidth - margin.left;
+          var height = (parseInt($scope.height, 10) || 500);
+          var data = $scope.data;
+          var config = $scope.config;
+
+          var color = d3.scale.category20();
+          var outerRadius = height / 2 - 20,
+              innerRadius = outerRadius / 2,
+              cornerRadius = 10;
+
+          var pie = d3.layout.pie()
+              .sort(null)
+              .value(function(d) {
+                return getNestedValue(d, config.sortKey.split("."));
+              });
+
+          var arc = d3.svg.arc()
+              .padRadius(outerRadius)
+              .innerRadius(innerRadius);
+          var data = $scope.data;
+          var fontSize = 18;
 
           if (!data || !data.length) {
             return;
@@ -245,8 +251,20 @@ module ngApp.components.charts {
               .attr("class", "arc");
 
           var gPath = g.append("path");
+          var legendData = {
+            title: $scope.title,
+            elem: element.find(".chart-container > svg > g"),
+            parent: element.find(".chart-container"),
+            data: []
+          };
 
           gPath.attr("d", arc)
+              .each(function(d, i) {
+                legendData.data.push({
+                  color: color(i),
+                  data: d.data
+                });
+              })
               .style("fill", function(d, i) { return color(i); })
               .attr("state", function(d) {
                 return config.state ? "true" : "false";
@@ -275,11 +293,19 @@ module ngApp.components.charts {
                   return val + ", " + d.data.doc_count + " " + label;
                 });
               })
-              .on("mouseout.text", setDefaultText);
+              .on("mouseout.text", setDefaultText)
+              .on("mouseover.legend", function() {
+                element.find(".donut-chart-legend").removeClass("invisible");
+              })
+              .on("mouseout.legend", function() {
+                element.find(".donut-chart-legend").addClass("invisible");
+              });
+
+          $scope.legendData = legendData;
           
           if (data.length > 1) {
-            gPath.on("mouseover", arcTween(outerRadius - 10, 0))
-                 .on("mouseout", arcTween(outerRadius - 20, 150));
+            gPath.on("mouseover.tween", arcTween(outerRadius - 10, 0))
+                 .on("mouseout.tween", arcTween(outerRadius - 20, 150));
           }
 
           function setDefaultText() {
@@ -300,37 +326,35 @@ module ngApp.components.charts {
 
           setDefaultText();
 
-          $window.jQuery($window).on("resize", updateChart);
-        }
+          function goToState(d) {
+            if (!config.state || (!config.state[d.data.key] &&
+                !config.state["default"])) {
+              return;
+            }
 
-        function goToState(d) {
-          if (!config.state || (!config.state[d.data.key] &&
-              !config.state["default"])) {
-            return;
+            if (config.state[d.data.key]) {
+              var state = config.state[d.data.key];
+
+              $state.go(state.name, state.params, {inherit: false});
+              return;
+            }
+
+            var state = config.state["default"],
+                params = {
+                  filters: state.params.filters(d.data.key)
+                };
+
+            $state.go(state.name, params, {inherit: false});
           }
 
-          if (config.state[d.data.key]) {
-            var state = config.state[d.data.key];
-
-            $state.go(state.name, state.params, {inherit: false});
-            return;
+          function arcTween(outerRadius, delay) {
+            return function() {
+              d3.select(this).transition().delay(delay).attrTween("d", function(d) {
+                var i = d3.interpolate(d.outerRadius, outerRadius);
+                return function(t) { d.outerRadius = i(t); return arc(d); };
+              });
+            };
           }
-
-          var state = config.state["default"],
-              params = {
-                filters: state.params.filters(d.data.key)
-              };
-
-          $state.go(state.name, params, {inherit: false});
-        }
-
-        function arcTween(outerRadius, delay) {
-          return function() {
-            d3.select(this).transition().delay(delay).attrTween("d", function(d) {
-              var i = d3.interpolate(d.outerRadius, outerRadius);
-              return function(t) { d.outerRadius = i(t); return arc(d); };
-            });
-          };
         }
 
         updateChart();
@@ -338,8 +362,68 @@ module ngApp.components.charts {
     };
   }
 
+  /* @ngInject */
+  function DonutChartLegend($window: ng.IGDCWindowService): ng.IDirective {
+    return {
+      restrict: "A",
+      replace: true,
+      scope: {
+        data: "="
+      },
+      templateUrl: "components/charts/templates/donut-chart-legend.html",
+      link: function($scope, elem) {
+        $scope.limit = 10;
+
+        $scope.$watch("data", function(newVal) {
+          if (newVal) {
+            $scope.data.data.sort(function(a, b) {
+              if (a.data.doc_count < b.data.doc_count) {
+                return 1;
+              }
+
+              if (a.data.doc_count > b.data.doc_count) {
+                return -1;
+              }
+
+              return 0;
+            });
+
+            $scope.displayedData = $scope.data.data.slice(0, 10);
+
+            _.defer(() => {
+              var rect = $scope.data.elem[0].getBoundingClientRect();
+              var LEGEND_WIDTH = 275;
+              var offset = $scope.data.elem.offset();
+              var height;
+
+              if (elem.height() < $scope.data.parent.height()) {
+                height = ($scope.data.parent.height() - elem.height()) / 2;
+                height += 30;
+              }
+
+              if (elem.height() > $scope.data.parent.height()) {
+                height = 0;
+              }
+
+              elem.css("top", height + "px");
+
+              var left = rect.width + (($scope.data.parent.width() - rect.width) / 2) + 20;
+
+              if (rect.width + offset.left + LEGEND_WIDTH < $window.innerWidth) {
+                elem.css("left", left + "px");
+              } else {
+                elem.css("right", left + "px");
+              }
+            });
+          }
+        });
+      }
+    }
+  }
+
   angular.module("components.charts", [])
       .directive("donutChart", DonutChart)
+      .directive("donutChartLegend", DonutChartLegend)
       .directive("pieChart", PieChart);
 }
 

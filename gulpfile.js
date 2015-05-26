ed5b533bc7ed8862b0b5bbefbcb63f38300d27f0
Shane Wilson <shane.wilson@oicr.on.ca>
2015-05-26 09:58:53 -0400

    chore(build): remove prod again

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ var AUTOPREFIXER_BROWSERS = [
   'bb >= 10'
 ];
 
-var production = process.env.NODE_ENV === "production";
+var production = false;// process.env.NODE_ENV === "production";
 $.util.log('Environment', $.util.colors.blue(production ? 'Production' : 'Development'));
 
 // <paths>

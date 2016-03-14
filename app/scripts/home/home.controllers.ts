fa410ee3a1709fb6caab217d3ad54fb9cc54fd37
Michael Moncada <michael.moncada@oicr.on.ca>
2016-03-14 14:21:16 -0400

    feat(app) - Home Portal Creation
    - Added Basic Skeletal Layout
    - Added Quick Search
    - Added Bar Chart (Primary Site Case Count)
    - Added dynamic query counts under quick search widget.
    - Centralized quicksearch logic
    - Added logic to control menu nav options
    - Added GDC apps shortcut in nav
    
    Closes #765

--- app/scripts/home/home.controllers.ts
+++ app/scripts/home/home.controllers.ts
@@ -1,14 +1,262 @@
 module ngApp.home.controllers {
   import ICoreService = ngApp.core.services.ICoreService;
 
+  import IProjects = ngApp.projects.models.IProjects;
+  import TableiciousConfig = ngApp.components.tables.directives.tableicious.TableiciousConfig;
+
+  import IHomeService = ngApp.home.services.IHomeService;
+
+
+
   export interface IHomeController {
+    getChartFilteredData() : any[];
+    getChartTooltipFunction(): any;
+    setChartDataFilter(): void;
+    getProjectStatsList(): any[];
+    getExampleSearchQueries(): any[];
+    refresh(): void;
   }
 
+  /*
+  export interface IHomeScope extends ng.IScope {
+  }*/
+
   class HomeController implements IHomeController {
+
+    projectData: IProjects;
+    projectStatsOrdering: any;
+    projectStatsList: any[];
+    projectChartData: any[];
+    numberFilter: any;
+    tooltipFn: any;
+    exampleSearchQueries: any[];
+    defaultParams: any;
+
     /* @ngInject */
-    constructor(private CoreService: ICoreService) {
-      CoreService.setPageTitle("Welcome");
+    constructor(private HomeService: IHomeService, private ProjectTableModel: TableiciousConfig,
+                private CoreService: ICoreService, private $filter: ng.ui.IFilterService) {
+
+      CoreService.setPageTitle("Welcome to The Genomics Data Commons Data Portal");
+
+
+      this.numberFilter = $filter("number");
+
+      this.tooltipFn = _.bind(function (d) {
+        var str = "";
+
+        if (arguments.length === 0) {
+          return str;
+        }
+
+        str = "<h4>" + d._key + "</h4>\n<p>" +
+          this.numberFilter(d._count) + " cases (" + this.numberFilter(d.fileCount) + " files)\n" +
+          "</p>";
+
+        return str;
+      }, this);
+
+
+      this.projectStatsList = [
+        {title: "Projects", value: 0, icon: "icon-gdc-projects", url: "/projects/t"},
+        {title: "Cases", value: 0, icon: "icon-gdc-cases", url: "/search/c"},
+        {title: "Files", value: 0, icon: "fa fa-file-o", url: "/search/f"},
+        {title: "Primary Sites", value: 0, icon: "cancer_type_hardcode", url: "/projects/t"},
+        //{title: "Cancer Types", value: 0, icon: "fa fa-heartbeat", url: "/projects/t"},
+        {title: "Downloads to Date", value: 0, icon: "fa fa-download", url: "#"}
+      ];
+
+
+      this.projectStatsOrdering = {projects: 0, cases: 1, files: 2, cancerTypes: 3, downloads: 4};
+
+      this.exampleSearchQueries = [
+        {
+          description: "Brain cancer cases over the age of 40 at diagnosis",
+          filters: {"op":"and","content":[{"op":"in","content":{"field":"cases.project.primary_site","value":["Brain"]}},{"op":">=","content":{"field":"cases.clinical.age_at_diagnosis","value":[14965]}}]},
+          caseCount: null,
+          fileCount: null
+        },
+        {
+          description: "All female cases from the TARGET-NBL project",
+          filters: {"op":"and","content":[{"op":"in","content":{"field":"cases.project.project_id","value":["TARGET-NBL"]}},{"op":"in","content":{"field":"cases.clinical.gender","value":["female"]}}]},
+          caseCount: null,
+          fileCount: null
+        },
+        {
+          description: "All Asian cases with disease type Thyroid Carcinoma project",
+          filters: {"op":"and","content":[{"op":"in","content":{"field":"cases.project.disease_type","value":["Thyroid Carcinoma"]}},{"op":"in","content":{"field":"cases.clinical.race","value":["asian"]}}]},
+          caseCount: null,
+          fileCount: null
+        },
+      ];
+
+
+      this.defaultParams =  {
+        fields: this.ProjectTableModel.fields,
+        expand: this.ProjectTableModel.expand,
+        facets: [
+          "disease_type",
+          "program.name",
+          "project_id",
+          "primary_site",
+          "summary.experimental_strategies.experimental_strategy",
+          "summary.data_types.data_type"
+        ],
+        size: 100
+      };
+
+      this.refresh();
+
     }
+
+    fetchExampleSearchQueryStats() {
+      var _controller = this,
+          exampleQueries = _controller.exampleSearchQueries,
+          defaultParams = _controller.defaultParams;
+
+      for (var i = 0; i < exampleQueries.length; i++) {
+        var query = exampleQueries[i];
+
+       (function(q) {
+
+         var params = _.cloneDeep(defaultParams);
+         params.filters = q.filters;
+         _controller.HomeService.getParticipants(params).then(
+            function (projectData) {
+              q.caseCount = _.get(projectData, 'pagination.total', 0);
+            }
+          );
+
+         _controller.HomeService.getFiles(params).then(
+           function (projectData) {
+             q.fileCount = _.get(projectData, 'pagination.total', 0);
+           }
+         );
+
+
+        })(query);
+
+      }
+    }
+
+    getExampleSearchQueries() {
+      return this.exampleSearchQueries;
+    }
+
+
+    transformProjectData(data) {
+      var _controller = this,
+          hits = _.get(data, 'hits', false);
+
+      if (! hits) {
+        return;
+      }
+
+
+      // reduce the array keyed on projectID
+      var primarySites = _.reduce(hits, function(primarySiteData, project) {
+
+        var primarySite = project.primary_site;
+
+        _controller.projectStatsList[_controller.projectStatsOrdering.projects].value += 1;
+
+        if (! primarySite) {
+          console.warn("Project has no primary site using project id instead: ", project);
+          primarySite = project.project_id;
+        }
+
+        if (! _.isArray(primarySiteData[primarySite])) {
+          primarySiteData[primarySite] = [];
+        }
+
+        primarySiteData[primarySite].push(project);
+
+
+        return primarySiteData;
+
+      }, {});
+
+      var primarySiteIDs = _.keys(primarySites);
+
+      if (primarySiteIDs.length === 0) {
+        return;
+      }
+
+      _controller.projectChartData = _.filter(
+        _.map(primarySiteIDs, function(pID) {
+
+
+          var primarySiteData =  primarySites[pID],
+              caseCount = 0,
+              fileCount = 0;
+
+          for (var i = 0; i < primarySiteData.length; i++) {
+            caseCount += +(_.get(primarySiteData[i], 'summary.case_count', 0));
+            fileCount += +(_.get(primarySiteData[i], 'summary.file_count', 0));
+          }
+
+
+          _controller.projectStatsList[_controller.projectStatsOrdering.cases].value += caseCount;
+          _controller.projectStatsList[_controller.projectStatsOrdering.files].value += fileCount;
+
+
+          /* _key and _count are required data properties for the marked bar chart */
+          return {_key: pID, values: primarySiteData, _count: caseCount, fileCount: fileCount}
+      }), function(d) { return d._count > 0; });
+
+
+      _controller.projectStatsList[_controller.projectStatsOrdering.cancerTypes].value += primarySiteIDs.length;
+
+
+
+
+      console.log( _controller.projectStatsList);
+
+
+    }
+
+
+    getChartFilteredData() {
+      return this.projectChartData;
+    }
+    
+    getChartTooltipFunction() {
+      return this.tooltipFn;
+    }
+
+    setChartDataFilter() {
+
+    }
+
+    getProjectStatsList() {
+      return this.projectStatsList;
+    }
+
+    getProjects(filters:Object = null) {
+
+     var params = this.defaultParams;
+
+      if (filters) {
+        params.filters = filters;
+      }
+
+      return this.HomeService.getProjects(params);
+    }
+
+    fetchAllProjects() {
+      this.getProjects()
+        .then((projectData) => {
+          this.projectData = projectData;
+          this.chartData = this.transformProjectData(projectData);
+        });
+    }
+
+    refresh() {
+
+      this.fetchExampleSearchQueryStats();
+      this.fetchAllProjects();
+
+    }
+
   }
 
   angular
---
+++ app/scripts/home/home.services.ts
@@ -0,0 +1,156 @@
+module ngApp.home.services {
+
+  import IProjects = ngApp.projects.models.IProjects;
+  import ILocationService = ngApp.components.location.services.ILocationService;
+  import IRootScope = ngApp.IRootScope;
+  import IParticipants = ngApp.participants.IParticipant;
+  import IFiles = ngApp.files.models.IFiles;
+
+  export interface IHomeService {
+    getProjects(params?: Object): ng.IPromise<IProjects>;
+    getParticipants(params: Object = {}): ng.IPromise<IParticipants>;
+    getFiles(params: Object = {}): ng.IPromise<IFiles>;
+  }
+
+  class HomeService implements IHomeService {
+
+    private projectsDataSource: restangular.IElement;
+    private participantsDataSource: restangular.IElement;
+    private filesDataStore: restangular.IElement;
+
+    /* @ngInject */
+    constructor(Restangular: restangular.IService, private LocationService: ILocationService,
+                private $rootScope: IRootScope, private $q: ng.IQService) {
+      this.projectsDataSource = Restangular.all("projects");
+      this.participantsDataSource = Restangular.all("cases");
+      this.filesDataStore = Restangular.all("files");
+    }
+
+    prepParams(params:Object = {}) {
+
+      var p = _.cloneDeep(params);
+
+      if (_.has(p, "fields")) {
+        p["fields"] = p["fields"].join();
+      }
+
+      if (_.has(p, "expand")) {
+        p["expand"] = p["expand"].join();
+      }
+
+      if (_.has(params, "facets")) {
+        p["facets"] = p["facets"].join();
+      }
+
+      return p;
+    }
+
+    getProjects(params:Object = {}):ng.IPromise<IProjects> {
+
+      var params = this.prepParams(params);
+
+      // Testing is expecting these values in URL, so this is needed.
+      var paging = params.paging || {
+          size: 20,
+          from: 1
+        };
+
+      var defaults = {
+        size: params.size || paging.size,
+        from:  params.from || paging.from,
+        sort: paging.sort || "summary.case_count:desc",
+        filters: params.filters || this.LocationService.filters()
+      };
+
+      var abort = this.$q.defer();
+      var prom:ng.IPromise<IProjects> = this.projectsDataSource.withHttpConfig({
+          timeout: abort.promise
+        })
+        .get("", angular.extend(defaults, params)).then((response):IFiles => {
+          return response["data"];
+        });
+
+      var eventCancel = this.$rootScope.$on("gdc-cancel-request", () => {
+        abort.resolve();
+        eventCancel();
+      });
+
+      return prom;
+    }
+
+
+    getParticipants(params: Object = {}): ng.IPromise<IParticipants> {
+
+      var params = this.prepParams(params);
+
+      // Testing is expecting these values in URL, so this is needed.
+      var paging = params.paging || {
+          size: 20,
+          from: 1
+        };
+
+      var defaults = {
+        size: paging.size,
+        from: paging.from,
+        sort: paging.sort || 'case_id:asc',
+        filters: params.filters || this.LocationService.filters()
+      };
+
+
+      var abort = this.$q.defer();
+      var prom: ng.IPromise<IParticipants> = this.participantsDataSource.withHttpConfig({
+          timeout: abort.promise
+        })
+        .get("", angular.extend(defaults, params)).then((response): IParticipants => {
+          return response["data"];
+        });
+
+      var eventCancel = this.$rootScope.$on("gdc-cancel-request", () => {
+        abort.resolve();
+        eventCancel();
+      });
+
+      return prom;
+    }
+
+    getFiles(params: Object = {}): ng.IPromise<IFiles> {
+
+      var params = this.prepParams(params);
+
+      // Testing is expecting these values in URL, so this is needed.
+      var paging = params.paging || {
+          size: 20,
+          from: 1
+        };
+
+      var defaults = {
+        size: paging.size,
+        from: paging.from,
+        sort: paging.sort || "file_name:asc",
+        filters: params.filters || this.LocationService.filters()
+      };
+
+      var abort = this.$q.defer();
+      var prom: ng.IPromise<IFiles> = this.filesDataStore.withHttpConfig({
+          timeout: abort.promise
+        })
+        .get("", angular.extend(defaults, params)).then((response): IFiles => {
+          return response["data"];
+        });
+
+      var eventCancel = this.$rootScope.$on("gdc-cancel-request", () => {
+        abort.resolve();
+        eventCancel();
+      });
+
+      return prom;
+    }
+
+
+
+  }
+
+  angular
+    .module("home.services", [])
+    .service("HomeService", HomeService);
+}

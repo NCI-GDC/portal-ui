365143f95e15a205746f4dba0523014297817893
Alex Wilmer <alex.wilmer@oicr.on.ca>
2016-03-31 12:36:35 -0400

    feat(components): transition to tab from table
    Closes #1839

---
+++ app/scripts/components/summary-card/tests/summary-card.controllers.tests.js
@@ -0,0 +1,33 @@
+describe('Summary Card Controller:', function () {
+
+  // Initialization of the AngularJS application before each test case
+  beforeEach(module('ngApp.components', 'location.services'));
+  beforeEach(module(function ($stateProvider) { $stateProvider.state('state-two', { url: '/' }); }));
+
+  describe('addFilters:', function () {
+
+    it('should return undefined if no filters in config', inject(function ($rootScope, $controller, LocationService) {
+      var scope = $rootScope.$new();
+      scope.config = {};
+      var summaryCard = $controller('SummaryCardController', {
+        $scope: scope, LocationService: LocationService
+      });
+      expect(summaryCard.addFilters()).to.be.undefined;
+    }));
+
+    it('should be called correctly', inject(function ($rootScope, $controller, LocationService) {
+      var scope = $rootScope.$new();
+      scope.config = {
+        default: { filters: function () {} }
+      };
+      var summaryCard = $controller('SummaryCardController', {
+        $scope: scope, LocationService: LocationService
+      });
+      var addFiltersCallBack = sinon.spy(summaryCard, "addFilters");
+      summaryCard.addFilters();
+      expect(addFiltersCallBack).to.have.been.calledOnce;
+    }));
+
+  });
+
+});

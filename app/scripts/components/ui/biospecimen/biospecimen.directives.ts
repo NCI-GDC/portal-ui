dd5f1c02618f2337ae86e6a100e023db922c4380
Alex Wilmer <alex.wilmer@oicr.on.ca>
2016-04-29 14:05:56 -0400

    refactor(components): 106 unhardcode treeview
     - fix issue where empty entities would still display
    
    Closes #106

--- app/scripts/components/ui/biospecimen/biospecimen.directives.ts
+++ app/scripts/components/ui/biospecimen/biospecimen.directives.ts
@@ -7,7 +7,6 @@ module ngApp.components.ui.biospecimen.directives {
     expandAll(event: any, participant: any, expand: boolean): void;
     search(searchTerm: string, participant: any, field: string): void;
     searchTerm: string;
-    foundBySearch(term: string, field: string): boolean;
     found: any[];
   }
 
@@ -46,23 +45,92 @@ module ngApp.components.ui.biospecimen.directives {
           if (searchTerm) {
             $scope.found = BiospecimenService.search(searchTerm, participant, idFields);
             if ($scope.found.length) {
-              ctrl.displayBioSpecimenDocument(
-                { which: 1 }, // https://github.com/angular/angular.js/issues/6370
-                $scope.found[0].entity, $scope.found[0].type
-              );
+              $scope.$emit('displayBioSpecimenDocument', {
+                doc: $scope.found[0].entity,
+                type: $scope.found[0].type
+              })
               participant.biospecimenTreeExpanded = BiospecimenService.allExpanded(participant);
             }
           } else $scope.found = [];
-        };
-
-        $scope.foundBySearch = (term: string, field: string) => {
-          if (term === '') return false;
-          return ($scope.found || []).some(x => x.entity[field] === term);
         };
       }
     };
   }
 
+  function Tree() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'components/ui/biospecimen/templates/tree.html',
+      scope: {
+        entities: '=',
+        type: '=',
+        depth: '=',
+        searchTerm: '=',
+        activeDoc: '=',
+      },
+      link: scope => {
+        scope.expandTree = (event: any, doc: any) => {
+          if (event.which === 1 || event.which === 13) {
+            doc.expanded = !doc.expanded;
+            event.target.focus();
+          }
+        };
+
+        scope.hasEntities = (entities, type) => entities.some(x => x[type.s + '_id']);
+      }
+    }
+  }
+
+  function TreeItem($compile) {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'components/ui/biospecimen/templates/tree-item.html',
+      scope: {
+        entity: '=',
+        type: '=',
+        depth: '=',
+        searchTerm: '=',
+        activeDoc: '=',
+      },
+      link: (scope, el, attrs) => {
+        scope.expandTree = (event: any, doc: any) => {
+          if (event.which === 1 || event.which === 13) {
+            doc.expanded = !doc.expanded;
+            event.target.focus();
+          }
+        };
+
+        scope.displayBioSpecimenDocument = (event: any, doc: any, type: string): void => {
+          if (event.which === 1 || event.which === 13) {
+            scope.$emit('displayBioSpecimenDocument', { doc: doc, type: type });
+          }
+        }
+
+        el.append($compile([
+          '<tree',
+            'depth="depth + 1"',
+            'data-ng-repeat="childType in ',
+            '[',
+              '{ p: \'portions\', s: \'portion\' },',
+              '{ p: \'aliquots\', s: \'aliquot\' },',
+              '{ p: \'analytes\', s: \'analyte\' },',
+              '{ p: \'slides\', s: \'slide\' }',
+            ']"',
+            'entities="entity[childType.p]"',
+            'data-ng-if="entity[childType.p]"',
+            'type="childType"',
+            'search-term="searchTerm"',
+            'active-doc="activeDoc"',
+          '></tree>'
+        ].join(' '))(scope))
+      }
+    }
+  }
+
   angular.module("biospecimen.directives", ["biospecimen.controllers", "biospecimen.services"])
-      .directive("biospecimen", Biospecimen);
+      .directive("biospecimen", Biospecimen)
+      .directive("tree", Tree)
+      .directive("treeItem", TreeItem)
 }

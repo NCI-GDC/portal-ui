24577d78718b4db634cb04285e26e17ab5e185e9
Shane Wilson <shane.wilson@oicr.on.ca>
2016-10-05 15:40:24 -0400

    update to rr4-beta4

--- packages/components/Links/InternalLinkWithContext.js
+++ packages/components/Links/InternalLinkWithContext.js
@@ -1,41 +1,34 @@
 /* @flow */
 
 import React from 'react';
-import { location as RRLocationPropType } from 'react-router/PropTypes';
-import { compose, getContext } from 'recompose';
+import { LocationSubscriber } from 'react-router/locationBroadcast';
 import InternalLink from './InternalLink';
 
 import type { LinkPropsType } from './types';
 
-type LocationType = {|
-  pathname: string,
-|};
-
-type PropsType = {|
-  location: LocationType,
-|} & LinkPropsType;
-
 const mergeQuery = () => ({});
 const diffQuery = () => ({});
 
-const InternalLinkWithContext = ({ pathname, query, merge, diff, location, ...rest }: PropsType) => {
-  const pn = pathname || location.pathname;
+const InternalLinkWithContext = ({ pathname, query, merge, diff, ...rest }: LinkPropsType) => (
+  <LocationSubscriber>{
+    contextLocation => {
+      const pn = pathname || contextLocation.pathname;
 
-  const q = (merge || diff)
-    ? merge
-      ? mergeQuery()
-      : diffQuery()
-    : query;
+      const q = (merge || diff)
+        ? merge
+          ? mergeQuery()
+          : diffQuery()
+        : query;
 
-  return (
-    <InternalLink
-      pathname={pn}
-      query={q}
-      {...rest}
-    />
-  );
-};
+      return (
+        <InternalLink
+          pathname={pn}
+          query={q}
+          {...rest}
+        />
+      );
+    }
+  }</LocationSubscriber>
+);
 
-export default compose(
-  getContext({ location: RRLocationPropType })
-)(InternalLinkWithContext);
+export default InternalLinkWithContext;
--- packages/containers/Pagination.js
+++ packages/containers/Pagination.js
@@ -1,80 +1,78 @@
-// /* @flow */
+/* @flow */
 
-// import React from 'react';
-// import Relay from 'react-relay';
-// import { compose } from 'recompose';
-// import { createContainer } from 'recompose-relay';
-// import Link from 'react-router/Link';
+import React from 'react';
+import Relay from 'react-relay';
+import { compose } from 'recompose';
+import { createContainer } from 'recompose-relay';
+import Link from 'components/Links/Link';
 
-// import { prepareJsonParam } from 'routes/utils';
+type PropsType = {
+  pagination: {
+    count: number,
+    offset: number,
+    size: number,
+    sort: string,
+    total: number,
+  },
+  pathname: string,
+  relay: {
+    route: {
+      params: {
+        filters: mixed,
+        first: number,
+        offset: number,
+      },
+    },
+  },
+};
 
-// type PropsType = {
-//   pagination: {
-//     count: number,
-//     offset: number,
-//     size: number,
-//     sort: string,
-//     total: number,
-//   },
-//   pathname: string,
-//   relay: {
-//     route: {
-//       params: {
-//         filters: mixed,
-//         first: number,
-//         offset: number,
-//       },
-//     },
-//   },
-// };
+const Pagination = (props: PropsType) => {
+  const filters = (
+    props.relay.route.params.filters
+      ? props.relay.route.params.filters
+      : null
+  );
 
-// const Pagination = (props: PropsType) => {
-//   const filters = (
-//     props.relay.route.params.filters
-//       ? prepareJsonParam(props.relay.route.params.filters)
-//       : null
-//   );
+  const merge = query => ({
+    ...props.relay.route.params,
+    ...query,
+  });
 
-//   const merge = query => ({
-//     ...props.relay.route.params,
-//     ...query,
-//   });
+  const to = offset => ({
+    pathname: props.pathname,
+    query: merge({
+      offset,
+      filters,
+    }),
+  });
 
-//   const to = offset => ({
-//     pathname: props.pathname,
-//     query: merge({
-//       offset,
-//       filters,
-//     }),
-//   });
+  const params = props.relay.route.params;
+  const pagination = props.pagination;
 
-//   const params = props.relay.route.params;
-//   const pagination = props.pagination;
+  return (
+    <div>
+      <Link merge query={{ to: to(0) }}>{' << '}</Link>
+      <Link merge query={{ to: to(params.offset - params.first) }}>{' < '}</Link>
+      <Link merge query={{ to: to(params.offset + params.first) }}>{' > '}</Link>
+      <Link merge query={{ to: to(pagination.total - (pagination.total % pagination.size)) }}>{' >> '}</Link>
+    </div>
+  );
+};
 
-//   return (
-//     <div>
-//       <Link to={to(0)}>{' << '}</Link>
-//       <Link to={to(params.offset - params.first)}>{' < '}</Link>
-//       <Link to={to(params.offset + params.first)}>{' > '}</Link>
-//       <Link to={to(pagination.total - (pagination.total % pagination.size))}>{' >> '}</Link>
-//     </div>
-//   );
-// };
+const PaginationQuery = {
+  fragments: {
+    pagination: () => Relay.QL`
+      fragment on ESPagination {
+        total
+        size
+        count
+        offset
+        sort
+      }
+    `,
+  },
+};
 
-// const PaginationQuery = {
-//   fragments: {
-//     pagination: () => Relay.QL`
-//       fragment on ESPagination {
-//         total
-//         size
-//         count
-//         offset
-//         sort
-//       }
-//     `,
-//   },
-// };
-
-// export default compose(
-//   createContainer(PaginationQuery)
-// )(Pagination);
+export default compose(
+  createContainer(PaginationQuery)
+)(Pagination);

b84b2b8e641ff0b2c23a0962e55dd3939bdf0dcd
Christine Yu <christine.yu@oicr.on.ca>
2016-10-07 17:41:49 -0400

    feat(gene): PRTL-300 add it

---
+++ app/scripts/genes/genes.controllers.ts
@@ -0,0 +1,23 @@
+module ngApp.genes.controllers {
+
+  class GeneController {
+
+    /* @ngInject */
+    constructor(private gene: any, private $scope: ng.IScope) {
+      $scope.gene = gene;
+      this.renderReact();
+    }
+
+    renderReact () {
+      ReactDOM.render(
+        React.createElement(ReactComponents.Gene, { gene: this.gene }),
+        document.getElementById('react-root')
+      );
+    };
+
+  }
+
+  angular
+      .module("genes.controller", [])
+      .controller("GeneController", GeneController);
+}

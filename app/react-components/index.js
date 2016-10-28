9da967cb2ecbe26dd44712b085dc906e2a043c6c
Alex Wilmer <djazium@gmail.com>
2016-10-28 14:47:07 -0400

    :sparkles: [PRTL-375] improved page layout (#1110)

--- app/react-components/index.js
+++ app/react-components/index.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import Project from './Project';
 import Gene from './Gene';
 import Mutation from './Mutation';
+import SideNavLayout from './layouts/SideNavLayout'
 
 // expose to angular
 window.ReactComponents = {
@@ -12,6 +13,7 @@ window.ReactComponents = {
   Project,
   Gene,
   Mutation,
+  SideNavLayout,
 };
 
 window.Project = Project;

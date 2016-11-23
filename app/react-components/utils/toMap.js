8201cd14e3a6166f5da8ca831bc793c132a9227c
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2016-11-23 16:27:29 -0500

    :sparkles: [PRTL-392] survival to tsv

---
+++ app/react-components/utils/toMap.js
@@ -0,0 +1,14 @@
+function idFirst(a, b) {
+    if(a[0] === 'id') {
+        return -1;
+    } else if (b[0] === 'id') {
+        return 1;
+    }
+
+    return 0;
+}
+
+export default function toMap(obj, sort = idFirst) {
+  const entries = Object.entries(obj);
+  return new Map(sort ? entries.sort(sort) : entries);
+}

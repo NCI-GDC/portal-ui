c8d6f6765535f14920d878b923d58d33e32038a9
Alex Wilmer <alex.wilmer@oicr.on.ca>
2016-11-29 15:19:31 -0500

    :bug: [PRTL-431] rename donors to cases in json

--- app/react-components/components/ProteinLolliplot.js
+++ app/react-components/components/ProteinLolliplot.js
@@ -137,9 +137,14 @@ let ProteinLolliplot = ({
       >
         Reset
       </Button>
-      <DownloadVisualizationButton 
+      <DownloadVisualizationButton
         svg="#protein-viewer-root svg.chart"
-        data={$scope.proteinLolliplotData}
+        data={Object.keys($scope.proteinLolliplotData)
+          .reduce((acc, key) => ({
+            ...acc,
+            [key === 'donors' ? 'cases' : key]: $scope.proteinLolliplotData[key],
+          }), {})
+        }
         stylePrefix="#protein-viewer-root"
         slug={`protein_viewer-${gene.symbol}-${moment().format('YYYY-MM-DD')}`}
       />

3a9bb8c17c66e19e16e3b1a95e864681aec4d828
Alex Wilmer <alex.wilmer@oicr.on.ca>
2016-12-09 16:20:25 -0500

    :art::memo: knit/socks eslint (NO FIX)

--- app/react-components/components/DownloadVisualizationButton.js
+++ app/react-components/components/DownloadVisualizationButton.js
@@ -3,7 +3,7 @@ import downloadSvg from 'download-svg';
 
 import DropDownButton from './DropDownButton';
 import saveFile from '../utils/filesaver';
-import toTsvString, {mapArrayToTsvString} from '../utils/toTsvString';
+import toTsvString, { mapArrayToTsvString } from '../utils/toTsvString';
 import Download from '../theme/icons/Download';
 
 function getSelector(el) {
@@ -22,15 +22,14 @@ const DownloadVisualizationButton = ({
   data,
   slug = 'export',
   stylePrefix,
-  disabled,
   style,
   noText,
   tsvData,
   ...props
 }) => (
   <DropDownButton
-    icon={!noText && <Download/>}
-    label={noText ? <span><Download/><div style={styles.hidden}>Download</div></span> : 'Download'}
+    icon={!noText && <Download />}
+    label={noText ? <span><Download /><div style={styles.hidden}>Download</div></span> : 'Download'}
     style={style}
     options={[
       ...(svg ? [
@@ -39,7 +38,7 @@ const DownloadVisualizationButton = ({
           onClick: () => {
             downloadSvg({
               svg: getSelector(svg),
-              stylePrefix: stylePrefix,
+              stylePrefix,
               fileName: `${slug}.svg`,
             });
           },
@@ -49,7 +48,7 @@ const DownloadVisualizationButton = ({
           onClick: () => {
             downloadSvg({
               svg: getSelector(svg),
-              stylePrefix: stylePrefix,
+              stylePrefix,
               fileName: `${slug}.png`,
               scale: 2,
               useCanvg: true,
@@ -65,7 +64,7 @@ const DownloadVisualizationButton = ({
               JSON.stringify(data, null, 2),
               'JSON',
               `${slug}.json`
-            )
+            );
           },
         },
       ] : []),
@@ -77,7 +76,7 @@ const DownloadVisualizationButton = ({
               tsvData.forEach ? mapArrayToTsvString(tsvData) : toTsvString(tsvData),
               'TSV',
               `${slug}.tsv`
-            )
+            );
           },
         },
       ] : []),

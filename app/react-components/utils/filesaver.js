0f3abf58c60d9e08d1e49da9ad46d8c5f7c5de04
Alex Wilmer <djazium@gmail.com>
2016-12-15 14:31:28 -0500

    :memo::art: lint utils folder (#1194)

--- app/react-components/utils/filesaver.js
+++ app/react-components/utils/filesaver.js
@@ -1,13 +1,13 @@
 import { saveAs } from 'filesaver.js';
 
 export default function download(body: string, format: string, filename: string): void {
-  let type = {
+  const type = {
     JSON: 'application/json',
     TSV: 'text/tab-separated-values',
     CSV: 'text/csv',
     XML: 'text/xml',
-    BLOB: 'octet/stream'
+    BLOB: 'octet/stream',
   }[format.toUpperCase()];
 
-  saveAs(new Blob([ body ], { type }), filename);
+  saveAs(new Blob([body], { type }), filename);
 }
--- app/react-components/utils/toTsvString.js
+++ app/react-components/utils/toTsvString.js
@@ -1,21 +1,21 @@
 /* @flow */
 
 export default (xs: Array<Object>): string => {
-  let result = ``;
+  let result = '';
   if (xs) {
-    result = Object.keys(xs[0]).join(`\t`) + `\n`;
-    xs.forEach(x => result += Object.keys(x).map(key => x[key]).join(`\t`) + `\n`);
+    result = `${Object.keys(xs[0]).join('\t')}\n`;
+    xs.forEach(x => { result += `${Object.keys(x).map(key => x[key]).join('\t')}\n`; });
   }
   return result;
 };
 
 export function mapArrayToTsvString(xs: Array<Map<string, string>>): string {
-  let result = ``;
-  xs[0].forEach((val, key) => result += key + `\t`);
-  result += `\n`;
+  let result = '';
+  xs[0].forEach((val, key) => { result += `${key}\t`; });
+  result += '\n';
   xs.forEach(x => {
-    x.forEach(val => result += val + `\t`);
-    result += `\n`;
+    x.forEach(val => { result += `${val}\t`; });
+    result += '\n';
   });
   return result;
 }

7508fab4b4e3604bc1c350e63657c5dd8cf4e8ab
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-05-29 10:25:36 -0400

    for build as well

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
 const webpackConfig = require('@knit/webpack-config-socks-app');
 
+const libs = webpackConfig.entry.libs;
+libs.unshift(libs.splice(libs.indexOf('babel-polyfill'), 1)[0]);
+
 module.exports = webpackConfig;

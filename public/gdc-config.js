c6bb29afc08661766945dffebc83ac13a77fa2b3
Chang Wang <garnwraly@gmail.com>
2017-06-14 13:53:35 -0400

    internalize angular compiled code under public/ folder

---
+++ public/gdc-config.js
@@ -0,0 +1,14 @@
+// config.js
+angular.module("ngApp.config", [])
+  .constant("config", {
+    "version": "1.4.1",
+    "commitLink": "https://github.com/NCI-GDC/portal-ui/commit/1416c89",
+    "commitHash": "1416c89",
+    "api": "https://gdc-api.nci.nih.gov/v0",
+    "auth":"https://gdc-portal.nci.nih.gov/auth",
+    "auth_api": "https://gdc-portal.nci.nih.gov/auth/api",
+    "supportedAPI": "1",
+    "tag": "https://github.com/NCI-GDC/portal-ui/releases/tag/1.4.1",
+    "production": false,
+    "fake_auth": false,
+  });

b9a70edf936799f6d182345a5a9e14e3a6a9fabc
Chang Wang <cheapsteak@gmail.com>
2017-06-22 10:01:54 -0400

    ✨ Integrate New Relic page actions and session traces for perf tracking (#1851)
    * ✨ Integrate New Relic page actions and session traces for perf tracking
    
    * ✨ include occurences, genes, donors in oncogrid:fetch
    
    * ✨ add more performance context for survivalplot data fetching

--- src/packages/@ncigdc/components/Oncogrid/OncogridWrapper.js
+++ src/packages/@ncigdc/components/Oncogrid/OncogridWrapper.js
@@ -224,11 +224,16 @@ const OncoGridWrapper = compose(
         maxCases: MAX_CASES,
         maxGenes: MAX_GENES,
       });
-      performanceTracker.end('oncogrid:fetch', {
-        currentFilters,
+
+      const performanceContext = {
+        donors: responses.cases.length,
+        genes: responses.genes.length,
+        occurences: responses.occurences.length,
         maxCases: MAX_CASES,
         maxGenes: MAX_GENES,
-      });
+      };
+
+      performanceTracker.end('oncogrid:fetch', performanceContext);
       if (!wrapperRefs[uniqueGridClass]) return;
 
       performanceTracker.begin('oncogrid:process');
@@ -295,12 +300,6 @@ const OncoGridWrapper = compose(
         impacts,
         consequenceTypes: filteredConsequenceTypes,
       });
-
-      const performanceContext = {
-        donors: responses.cases.length,
-        genes: responses.genes.length,
-        occurences: responses.occurences.length,
-      };
 
       performanceTracker.end('oncogrid:process', performanceContext);
 

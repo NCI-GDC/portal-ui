004dcf8ad918919446a23345b18b9951446d15eb
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-06-27 15:43:32 -0400

    ✨ [PRTL-936] add pagination to associated entities

---
+++ src/packages/@ncigdc/components/LocalPaginationTable.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Showing from '@ncigdc/components/Pagination/Showing';
+import Pagination from '@ncigdc/components/Pagination';
+import withRouter from '@ncigdc/utils/withRouter';
+import { compose } from 'recompose';
+import { Row } from '../uikit/Flex/index';
+
+const enhance = compose(withRouter);
+const LocalPaginationTable = ({
+  children,
+  prefix,
+  query,
+  data,
+  buttons,
+  style,
+}) => {
+  const size = parseInt(query[`${prefix}_size`] || 10, 10);
+  const offset = parseInt(query[`${prefix}_offset`] || 0, 10);
+  const enablePagination = data.length > 10;
+  const params = {
+    ...query,
+    [`${prefix}_size`]: size,
+    [`${prefix}_offset`]: enablePagination ? offset : 0,
+  };
+  const tableData = enablePagination ? data.slice(offset, offset + size) : data;
+
+  return (
+    <div style={style}>
+      <Row
+        style={{
+          backgroundColor: 'white',
+          padding: '1rem',
+          justifyContent: 'space-between',
+          alignItems: 'flex-end',
+        }}
+      >
+        {enablePagination
+          ? <Showing
+              docType="projects"
+              prefix={prefix}
+              params={params}
+              total={data.length}
+            />
+          : <span />}
+        {buttons}
+      </Row>
+      {React.Children.map(children, child =>
+        React.cloneElement(child, { data: tableData }),
+      )}
+      {enablePagination &&
+        <Pagination prefix={prefix} params={params} total={data.length} />}
+    </div>
+  );
+};
+
+export default enhance(LocalPaginationTable);

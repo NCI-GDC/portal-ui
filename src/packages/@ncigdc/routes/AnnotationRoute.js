121c1e851e4aa00b7dbab28c71fd59b3a5e000db
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-06-27 15:24:01 -0400

    🐛 only remount entity page components if id changes

--- src/packages/@ncigdc/routes/AnnotationRoute.js
+++ src/packages/@ncigdc/routes/AnnotationRoute.js
@@ -1,37 +1,6 @@
 /* @flow */
-/* eslint fp/no-class:0 */
-
-import React from 'react';
-import { connect } from 'react-redux';
-import Relay from 'react-relay/classic';
-
-import { handleStateChange } from '@ncigdc/dux/relayProgress';
 import AnnotationPage from '@ncigdc/containers/AnnotationPage';
-import { prepareNodeParams } from '@ncigdc/utils/routes';
-import NotFound from '@ncigdc/components/NotFound';
-import Loader from '@ncigdc/uikit/Loaders/Loader';
 
-import { nodeQuery } from './queries';
+import { makeEntityPage } from './utils';
 
-class AnnotationRoute extends Relay.Route {
-  static routeName = 'AnnotationPageRoute';
-  static queries = nodeQuery;
-  static prepareParams = prepareNodeParams('Annotation');
-}
-
-export default connect()((routeProps: mixed) =>
-  <Relay.Renderer
-    Container={AnnotationPage}
-    queryConfig={new AnnotationRoute(routeProps)}
-    environment={Relay.Store}
-    onReadyStateChange={handleStateChange(routeProps)}
-    render={({ error, props }) => {
-      if (error) {
-        return <NotFound />;
-      } else if (props) {
-        return <AnnotationPage {...props} />;
-      }
-      return <Loader />;
-    }}
-  />,
-);
+export default makeEntityPage({ entity: 'Annotation', Page: AnnotationPage });
--- src/packages/@ncigdc/routes/CaseRoute.js
+++ src/packages/@ncigdc/routes/CaseRoute.js
@@ -1,27 +1,19 @@
 /* @flow */
-/* eslint fp/no-class:0 */
-
-import React from 'react';
-import Relay from 'react-relay/classic';
 import { parse } from 'query-string';
-import { connect } from 'react-redux';
 
-import { handleStateChange } from '@ncigdc/dux/relayProgress';
 import CasePage from '@ncigdc/containers/CasePage';
 import {
   parseIntParam,
   parseFilterParam,
   parseJSURLParam,
 } from '@ncigdc/utils/uri';
-import NotFound from '@ncigdc/components/NotFound';
-import Loader from '@ncigdc/uikit/Loaders/Loader';
 
-import { nodeAndViewerQuery } from './queries';
+import { makeEntityPage } from './utils';
 
-class CaseRoute extends Relay.Route {
-  static routeName = 'CasePageRoute';
-  static queries = nodeAndViewerQuery;
-  static prepareParams = ({ match: { params }, location: { search } }) => {
+export default makeEntityPage({
+  entity: 'Case',
+  Page: CasePage,
+  prepareParams: ({ match: { params }, location: { search } }) => {
     const q = parse(search);
 
     const caseFilters = {
@@ -48,22 +40,5 @@ class CaseRoute extends Relay.Route {
       fmTable_filters: parseFilterParam(q.filters, null),
       caseFilters,
     };
-  };
-}
-
-export default connect()((routeProps: mixed) =>
-  <Relay.Renderer
-    Container={CasePage}
-    queryConfig={new CaseRoute(routeProps)}
-    environment={Relay.Store}
-    onReadyStateChange={handleStateChange(routeProps)}
-    render={({ error, props }) => {
-      if (error) {
-        return <NotFound />;
-      } else if (props) {
-        return <CasePage {...props} />;
-      }
-      return <Loader />;
-    }}
-  />,
-);
+  },
+});

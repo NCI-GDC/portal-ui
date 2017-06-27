5bb6f5de22aad325280155ffaa4529d7a1e109d9
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-06-27 15:24:01 -0400

    🐛 only remount entity page components if id changes

--- src/packages/@ncigdc/routes/ProjectRoute.js
+++ src/packages/@ncigdc/routes/ProjectRoute.js
@@ -1,144 +1,102 @@
 /* @flow */
-/* eslint fp/no-class:0 */
-
-import React from 'react';
-import Relay from 'react-relay/classic';
 import { parse } from 'query-string';
-import { connect } from 'react-redux';
 
-import NotFound from '@ncigdc/components/NotFound';
-import Loader from '@ncigdc/uikit/Loaders/Loader';
-import { handleStateChange } from '@ncigdc/dux/relayProgress';
 import ProjectPage from '@ncigdc/containers/ProjectPage';
-import type { TPrepareNodeParams } from '@ncigdc/utils/routes/types';
 import { parseFilterParam } from '@ncigdc/utils/uri';
 import { replaceFilters } from '@ncigdc/utils/filters';
-import { nodeAndViewerQuery } from './queries';
+import caseHasMutation from '@ncigdc/utils/filters/prepared/caseHasMutation';
+import { makeEntityPage } from './utils';
 
-const prepareNodeParams: TPrepareNodeParams = type => ({
-  location: { search },
-  match: { params },
-}) => {
-  const q = parse(search);
-  const clinicalFilters = {
-    op: 'AND',
-    content: [
-      {
-        op: '=',
-        content: {
-          field: 'project.project_id',
-          value: params.id,
-        },
-      },
-      {
-        op: 'OR',
-        content: [
-          {
-            op: 'NOT',
-            content: {
-              field: 'cases.demographic.demographic_id',
-              value: 'MISSING',
-            },
-          },
-          {
-            op: 'NOT',
-            content: {
-              field: 'cases.diagnoses.diagnosis_id',
-              value: 'MISSING',
-            },
-          },
-          {
-            op: 'NOT',
-            content: {
-              field: 'cases.family_histories.family_history_id',
-              value: 'MISSING',
-            },
-          },
-          {
-            op: 'NOT',
-            content: {
-              field: 'cases.exposures.exposure_id',
-              value: 'MISSING',
-            },
-          },
-        ],
-      },
-    ],
-  };
-
-  const biospecimenFilters = {
-    op: 'AND',
-    content: [
-      {
-        op: '=',
-        content: {
-          field: 'project.project_id',
-          value: params.id,
-        },
-      },
-      {
-        op: 'NOT',
-        content: {
-          field: 'cases.samples.sample_id',
-          value: 'MISSING',
-        },
-      },
-    ],
-  };
-
-  const qq: Object = {
-    ...q,
-    clinicalFilters: replaceFilters(
-      clinicalFilters,
-      parseFilterParam(q.filters, null),
-    ),
-    biospecimenFilters: replaceFilters(
-      biospecimenFilters,
-      parseFilterParam(q.filters, null),
-    ),
-    mutatedFilters: {
-      op: 'AND',
+const clinicalFilters = {
+  op: 'AND',
+  content: [
+    {
+      op: 'OR',
       content: [
-        { op: '=', content: { field: 'project.project_id', value: params.id } },
         {
-          op: 'IN',
-          content: { field: 'cases.available_variation_data', value: ['ssm'] },
+          op: 'NOT',
+          content: {
+            field: 'cases.demographic.demographic_id',
+            value: 'MISSING',
+          },
+        },
+        {
+          op: 'NOT',
+          content: {
+            field: 'cases.diagnoses.diagnosis_id',
+            value: 'MISSING',
+          },
+        },
+        {
+          op: 'NOT',
+          content: {
+            field: 'cases.family_histories.family_history_id',
+            value: 'MISSING',
+          },
+        },
+        {
+          op: 'NOT',
+          content: {
+            field: 'cases.exposures.exposure_id',
+            value: 'MISSING',
+          },
         },
       ],
     },
-    annotationsFilters: {
-      op: 'AND',
-      content: [
-        { op: '=', content: { field: 'project.project_id', value: params.id } },
-      ],
-    },
-  };
-
-  return {
-    id: btoa(`${type}:${params.id}`),
-    ...qq,
-  };
+  ],
 };
 
-class ProjectRoute extends Relay.Route {
-  static routeName = 'ProjectPageRoute';
-  static queries = nodeAndViewerQuery;
-  static prepareParams = prepareNodeParams('Project');
-}
+const biospecimenFilters = {
+  op: 'AND',
+  content: [
+    {
+      op: 'NOT',
+      content: {
+        field: 'cases.samples.sample_id',
+        value: 'MISSING',
+      },
+    },
+  ],
+};
 
-export default connect()((routeProps: mixed) =>
-  <Relay.Renderer
-    Container={ProjectPage}
-    queryConfig={new ProjectRoute(routeProps)}
-    environment={Relay.Store}
-    onReadyStateChange={handleStateChange(routeProps)}
-    render={({ error, props }) => {
-      if (error) {
-        return <NotFound />;
-      } else if (props) {
-        return <ProjectPage {...props} />;
-      }
-      return <Loader />;
-    }}
-  />,
-);
+export default makeEntityPage({
+  entity: 'Project',
+  Page: ProjectPage,
+  prepareParams: ({ location: { search }, match: { params } }) => {
+    const q = parse(search);
+    const projectFilter = {
+      op: 'AND',
+      content: [
+        {
+          op: 'in',
+          content: {
+            field: 'project.project_id',
+            value: [params.id],
+          },
+        },
+      ],
+    };
+
+    const qq: Object = {
+      ...q,
+      clinicalFilters: replaceFilters(
+        replaceFilters(projectFilter, clinicalFilters),
+        parseFilterParam(q.filters, null),
+      ),
+      biospecimenFilters: replaceFilters(
+        replaceFilters(projectFilter, biospecimenFilters),
+        parseFilterParam(q.filters, null),
+      ),
+      mutatedFilters: replaceFilters(projectFilter, {
+        op: 'AND',
+        content: [caseHasMutation],
+      }),
+      annotationsFilters: projectFilter,
+    };
+
+    return {
+      id: btoa(`Project:${params.id}`),
+      ...qq,
+    };
+  },
+});

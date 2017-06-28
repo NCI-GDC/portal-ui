81d372be7429c6b69e4c7598734f9ebe25ef5753
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-06-28 14:53:42 -0400

    🐛 fix spark meter position in IE

--- src/packages/@ncigdc/uikit/SparkMeter.js
+++ src/packages/@ncigdc/uikit/SparkMeter.js
@@ -2,32 +2,20 @@ import React from 'react';
 export default ({ value, width = 30, max = 30 }) =>
   <div
     style={{
-      display: 'inline-block',
-      position: 'relative',
+      width,
       height: 8,
-      width: width + 10,
+      backgroundColor: `rgb(209, 209, 209)`,
+      margin: '0 5px',
+      borderRadius: 2,
+      display: 'inline-block',
       pointerEvents: 'none',
     }}
   >
-    <span
+    <div
       style={{
-        position: 'absolute',
-        display: 'inline-block',
-        width,
-        height: 8,
-        backgroundColor: `rgb(209, 209, 209)`,
-        margin: '0 5px',
-        borderRadius: 2,
-      }}
-    />
-    <span
-      style={{
-        position: 'absolute',
-        display: 'inline-block',
         width: Math.max(value * max, 2),
         height: 8,
         backgroundColor: `rgb(39, 156, 75)`,
-        margin: '0 5px',
         borderRadius: 2,
       }}
     />

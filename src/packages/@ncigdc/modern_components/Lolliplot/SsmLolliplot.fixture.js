35e4970b30ec3405d755ee7fe2c87d0678a42322
Alex Wilmer <djazium@gmail.com>
2017-06-30 14:35:11 -0400

    ✨ PRTL-1348 reset zoom / tooltip domain 🐛 PRTL-1367 add missing y axis label in lolliplot

---
+++ src/packages/@ncigdc/modern_components/Lolliplot/SsmLolliplot.fixture.js
@@ -0,0 +1,5 @@
+import { defaultProps } from 'recompose';
+import { SsmLolliplot } from './index';
+export default defaultProps({ ssmId: 'a0fe6696-dd49-555d-bb9e-cdbec315dbb3' })(
+  SsmLolliplot,
+);

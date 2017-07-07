6ab2d06d022203c3d2c4c8bf0a225ea28628d324
Chang Wang <garnwraly@gmail.com>
2017-07-07 11:13:02 -0400

    ✨ PRTL-1398 ConsequencesTable display 1 transcript per row, sorted by aa_change, add export buttons

--- src/packages/@ncigdc/containers/ConsequencesTable.js
+++ src/packages/@ncigdc/containers/ConsequencesTable.js
@@ -2,18 +2,24 @@
 import React from 'react';
 import Relay from 'react-relay/classic';
 import { compose, withPropsOnChange } from 'recompose';
-import { sortBy, groupBy, get, find } from 'lodash';
+import { orderBy, groupBy, get, find } from 'lodash';
 import externalReferenceLinks from '@ncigdc/utils/externalReferenceLinks';
 import EntityPageHorizontalTable from '@ncigdc/components/EntityPageHorizontalTable';
+import LocalPaginationTable from '@ncigdc/components/LocalPaginationTable';
+import DownloadTableToTsvButton from '@ncigdc/components/DownloadTableToTsvButton';
+import { Row } from '@ncigdc/uikit/Flex';
 import { Tooltip } from '@ncigdc/uikit/Tooltip';
-import { tableToolTipHint } from '@ncigdc/theme/mixins';
-import CollapsibleList from '@ncigdc/uikit/CollapsibleList';
+import saveFile from '@ncigdc/utils/filesaver';
+import { tableToolTipHint, visualizingButton } from '@ncigdc/theme/mixins';
 import GeneLink from '@ncigdc/components/Links/GeneLink';
 import BubbleIcon from '@ncigdc/theme/icons/BubbleIcon';
 import MinusIcon from '@ncigdc/theme/icons/Minus';
 import PlusIcon from '@ncigdc/theme/icons/Plus';
 import { ExternalLink } from '@ncigdc/uikit/Links';
+import Button from '@ncigdc/uikit/Button';
 import { withTheme } from '@ncigdc/theme';
+
+const paginationPrefix = 'consequencesTable';
 
 const strandIconMap = {
   '-1': <MinusIcon />,
@@ -26,11 +32,13 @@ type TProps = {
   theme: Object,
   functionalImpactTranscript: Object,
   canonicalTranscriptId: string,
+  dataRows: Array<Object>,
+  consequences: Array<Object>,
 };
 
 const ConsequencesTableComponent = compose(
   withTheme,
-  withPropsOnChange(['node'], ({ node }) => {
+  withPropsOnChange(['node'], ({ node, theme }) => {
     const consequenceOfInterest = node.consequence.hits.edges.find(
       consequence => get(consequence, 'node.transcript.annotation.impact'),
       {},
@@ -53,94 +61,133 @@ const ConsequencesTableComponent = compose(
         .hgvsc}${t.gene.gene_strand}`;
     });
 
+    const consequences = node.consequence.hits.edges.map(
+      x => x.node.transcript,
+    );
+
+    const dataRows = orderBy(
+      consequences.map(transcript => {
+        return {
+          symbol: (
+            <GeneLink uuid={transcript.gene.gene_id}>
+              {transcript.gene.symbol}
+            </GeneLink>
+          ),
+          aa_change: transcript.aa_change,
+          consequence: transcript.consequence_type,
+          coding_dna_change: transcript.annotation.hgvsc,
+          strand: transcript.gene.gene_strand
+            ? strandIconMap[transcript.gene.gene_strand.toString(10)]
+            : '--',
+          transcripts: (
+            <span>
+              <ExternalLink
+                key={transcript.transcript_id}
+                style={{
+                  paddingRight: '0.5em',
+                  fontWeight: transcript.transcript_id ===
+                    functionalImpactTranscript.transcript_id
+                    ? 'bold'
+                    : 'normal',
+                }}
+                href={externalReferenceLinks.ensembl(transcript.transcript_id)}
+              >
+                {transcript.transcript_id}
+              </ExternalLink>
+              {transcript.transcript_id === canonicalTranscriptId &&
+                <BubbleIcon
+                  text="C"
+                  toolTipText="Canonical"
+                  backgroundColor={theme.primary}
+                />}
+            </span>
+          ),
+        };
+      }),
+      ['aa_change'],
+      ['asc'],
+    );
+
     return {
       functionalImpactTranscript,
       canonicalTranscriptId,
       consequenceDataGrouped,
+      consequences,
+      dataRows,
     };
   }),
 )(
   (
     {
+      dataRows,
+      consequences,
       consequenceDataGrouped,
       canonicalTranscriptId,
       functionalImpactTranscript,
       theme,
     }: TProps = {},
   ) =>
-    <EntityPageHorizontalTable
-      style={{ width: '100%', minWidth: '450px' }}
-      headings={[
-        { key: 'symbol', title: 'Gene' },
-        {
-          key: 'aa_change',
-          title: 'AA Change',
-          tdStyle: { wordBreak: 'break-all', whiteSpace: 'pre-line' },
-        },
-        {
-          key: 'consequence',
-          title: (
-            <Tooltip
-              Component={'SO Term: consequence type'}
-              style={tableToolTipHint()}
+    <LocalPaginationTable
+      style={{ width: '100%', minWidth: 450 }}
+      data={dataRows}
+      prefix={paginationPrefix}
+      buttons={
+        <Row style={{ alignItems: 'flex-end' }}>
+          <Tooltip
+            Component={<span>Export All</span>}
+            style={{ marginLeft: '2rem' }}
+          >
+            <Button
+              style={{ ...visualizingButton }}
+              onClick={() =>
+                saveFile(
+                  JSON.stringify(consequences, null, 2),
+                  'JSON',
+                  'consequences-data.json',
+                )}
             >
-              Consequence
-            </Tooltip>
-          ),
-        },
-        {
-          key: 'coding_dna_change',
-          title: 'Coding DNA Change',
-          tdStyle: { wordBreak: 'break-all', whiteSpace: 'pre-line' },
-        },
-        { key: 'strand', title: 'Strand' },
-        { key: 'transcripts', title: 'Transcript(s)' },
-      ]}
-      data={Object.values(consequenceDataGrouped).map(d => {
-        const first = d[0].node.transcript;
-        const transcripts = d.map(n => n.node.transcript.transcript_id);
-        return {
-          symbol: (
-            <GeneLink uuid={first.gene.gene_id}>{first.gene.symbol}</GeneLink>
-          ),
-          aa_change: first.aa_change,
-          consequence: first.consequence_type,
-          coding_dna_change: first.annotation.hgvsc,
-          strand: first.gene.gene_strand
-            ? strandIconMap[first.gene.gene_strand.toString(10)]
-            : '--',
-          transcripts: (
-            <CollapsibleList
-              data={sortBy(
-                transcripts,
-                t => t !== canonicalTranscriptId,
-              ).map(t =>
-                <span>
-                  <ExternalLink
-                    key={t}
-                    style={{
-                      paddingRight: '0.5em',
-                      fontWeight: t === functionalImpactTranscript.transcript_id
-                        ? 'bold'
-                        : 'normal',
-                    }}
-                    href={externalReferenceLinks.ensembl(t)}
-                  >
-                    {t}
-                  </ExternalLink>
-                  {t === canonicalTranscriptId &&
-                    <BubbleIcon
-                      text="C"
-                      toolTipText="Canonical"
-                      backgroundColor={theme.primary}
-                    />}
-                </span>,
-              )}
-            />
-          ),
-        };
-      })}
-    />,
+              JSON
+            </Button>
+          </Tooltip>
+          <DownloadTableToTsvButton
+            selector="#consequences-table"
+            filename="consequences-table.tsv"
+            style={{ marginLeft: '0.5rem' }}
+          />
+        </Row>
+      }
+    >
+      <EntityPageHorizontalTable
+        tableId="consequences-table"
+        style={{ width: '100%', minWidth: '450px' }}
+        headings={[
+          { key: 'symbol', title: 'Gene' },
+          {
+            key: 'aa_change',
+            title: 'AA Change',
+            tdStyle: { wordBreak: 'break-all', whiteSpace: 'pre-line' },
+          },
+          {
+            key: 'consequence',
+            title: (
+              <Tooltip
+                Component={'SO Term: consequence type'}
+                style={tableToolTipHint()}
+              >
+                Consequence
+              </Tooltip>
+            ),
+          },
+          {
+            key: 'coding_dna_change',
+            title: 'Coding DNA Change',
+            tdStyle: { wordBreak: 'break-all', whiteSpace: 'pre-line' },
+          },
+          { key: 'strand', title: 'Strand' },
+          { key: 'transcripts', title: 'Transcript(s)' },
+        ]}
+      />
+    </LocalPaginationTable>,
 );
 
 export const ConsequencesTableQuery = {

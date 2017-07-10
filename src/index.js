938dd301fe01bc202da2aacd0976c47385e60f27
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-07-10 16:44:15 -0400

    combine rows for same gene

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { setTheme } from '@ncigdc/theme';
-
 import Root from './Root';
-
-setTheme('active');
 
 ReactDOM.render(<Root />, document.getElementById('root'));
--- src/packages/@ncigdc/components/Modals/UploadGeneSet/MappingTable.js
+++ src/packages/@ncigdc/components/Modals/UploadGeneSet/MappingTable.js
@@ -1,34 +1,58 @@
 import React from 'react';
-import { uniq } from 'lodash';
+import { uniq, get } from 'lodash';
 
 import EntityPageHorizontalTable from '@ncigdc/components/EntityPageHorizontalTable';
-import { geneMap } from '@ncigdc/utils/validateIds';
+import { geneMap, GENE_ID_FIELDS } from '@ncigdc/utils/validateIds';
 import LocalPaginationTable from '@ncigdc/components/LocalPaginationTable';
 import Button from '@ncigdc/uikit/Button';
 import Row from '@ncigdc/uikit/Flex/Row';
 import saveFile from '@ncigdc/utils/filesaver';
 import toTsvString from '@ncigdc/utils/toTsvString';
 import { visualizingButton } from '@ncigdc/theme/mixins';
+import { theme } from '@ncigdc/theme/index';
 
-const HEADINGS = [
-  {
-    key: 'submitted',
-    title: 'Submitted Gene Identifier',
-    thStyle: { textAlign: 'center' },
-  },
-  {
-    key: 'mapped',
-    title: 'Mapped',
-    subheadings: ['GDC Gene ID', 'Symbol'],
-    thStyle: { textAlign: 'center' },
-  },
-];
+const ID_FIELD_DISPLAY = {
+  symbol: 'Symbol',
+  gene_id: 'Ensembl',
+  'external_db_ids.entrez_gene': 'Entrez',
+  'external_db_ids.hgnc': 'HGNC',
+  'external_db_ids.uniprotkb_swissprot': 'UniProtKB/Swiss-Prot',
+  'external_db_ids.omim_gene': 'OMIM',
+};
 
 export default ({ genes, ...props }) => {
-  const data = genes.map(gene => ({
-    submitted: gene,
-    mapped: [geneMap[gene].gene_id, geneMap[gene].symbol],
-  }));
+  const submittedHeaders = [];
+  const data = Object.entries(
+    genes.reduce((acc, gene) => {
+      acc[geneMap[gene].gene_id] = (acc[geneMap[gene].gene_id] || [])
+        .concat(gene);
+      return acc;
+    }, {}),
+  )
+    .map(([geneId, submitted]) => {
+      const geneData = geneMap[geneId];
+
+      return {
+        submitted: GENE_ID_FIELDS.map((idField, i) => {
+          const value = []
+            .concat(get(geneData, idField, []))
+            .filter(v => submitted.includes(v))[0];
+
+          if (value) {
+            submittedHeaders[i] = idField;
+            return value;
+          } else {
+            return '';
+          }
+        }),
+        mapped: [geneId, geneMap[geneId].symbol],
+      };
+    })
+    .map(row => ({
+      ...row,
+      submitted: row.submitted.filter((s, i) => submittedHeaders[i]),
+    }));
+
   const from = genes.length;
   const to = uniq(data.map(g => g.mapped[0])).length;
   return (
@@ -61,7 +85,27 @@ export default ({ genes, ...props }) => {
           </Row>
         }
       >
-        <EntityPageHorizontalTable headings={HEADINGS} />
+        <EntityPageHorizontalTable
+          dividerStyle={{
+            borderLeft: `1px solid ${theme.greyScale3}`,
+          }}
+          headings={[
+            {
+              key: 'submitted',
+              title: 'Submitted Gene Identifier',
+              subheadings: submittedHeaders
+                .filter(Boolean)
+                .map(h => ID_FIELD_DISPLAY[h]),
+              thStyle: { textAlign: 'center' },
+            },
+            {
+              key: 'mapped',
+              title: 'Mapped',
+              subheadings: ['GDC Gene ID', ID_FIELD_DISPLAY.symbol],
+              thStyle: { textAlign: 'center' },
+            },
+          ]}
+        />
       </LocalPaginationTable>
     </div>
   );

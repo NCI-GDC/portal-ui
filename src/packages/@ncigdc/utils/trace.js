6ba69ac952609cc934775700ba6b02de685d22a4
Alex Wilmer <djazium@gmail.com>
2017-07-11 16:30:22 -0400

    comment

--- src/packages/@ncigdc/utils/trace.js
+++ src/packages/@ncigdc/utils/trace.js
@@ -2,6 +2,28 @@
 
 import React from 'react';
 
+/**
+ *  Utility HOC designed to easily log received props.
+*/
+
+/* Recommended to be put on the global namespace in development mode:
+
+global.trace = process.env.NODE_ENV === 'production'
+  ? C => p => <C {...p} />
+  : require(<path_to_trace>).default;
+
+*/
+
+/* usage:
+
+  global.trace('Trying to fix issue #12')(Button)
+
+  compose(
+    global.trace() // string argument is optional.
+  )
+
+*/
+
 export default (additionalMessage = '') => Component => props => (
   additionalMessage && console.log(additionalMessage),
   console.info(

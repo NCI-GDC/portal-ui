7452084d5df96627c87c805393438ea16052fa81
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-07-31 11:29:33 -0400

    ✨ setup for save sets
    add sets to redux
    
    update schema
    
    delete old files
    
    move createSetButton

--- src/packages/@ncigdc/dux/index.js
+++ src/packages/@ncigdc/dux/index.js
@@ -16,7 +16,7 @@ const setupStore: TSetupStore = ({ persistConfig = {} } = {}) => {
   );
 
   persistStore(store, {
-    whitelist: ['cart', 'tableColumns', 'customFacets'],
+    whitelist: ['cart', 'tableColumns', 'customFacets', 'sets'],
     ...persistConfig,
   });
 

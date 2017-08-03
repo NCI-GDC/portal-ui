5632bbbbdbed8e6b7b071874ebefa3f2b7e72d5b
Alex Wilmer <djazium@gmail.com>
2017-08-03 13:20:23 -0400

    💄 PRTL-1497hideSurvival in generoute: ssmtable

--- src/packages/@ncigdc/routes/GeneRoute.js
+++ src/packages/@ncigdc/routes/GeneRoute.js
@@ -116,6 +116,7 @@ export default (
                   defaultFilters={geneFilter}
                   shouldShowGeneSymbol={false}
                   context={<GeneSymbol geneId={geneId} />}
+                  hideSurvival
                 />
               </Column>
             </Column>

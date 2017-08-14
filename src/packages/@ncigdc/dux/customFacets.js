42c666954f2cf5164b9251164cafd37aad637b37
Christine <christine.tc.yu@gmail.com>
2017-08-14 14:44:12 -0400

    PRTL-1456 use facets for custom facets and aggregations for preset (#1973)
    * PRTL-1456 use facets for custom facets and aggregations for preset
    
    🔥 to generated relay parts
    
    * use redux
    - use connect instead of localstorage everywhere
    - eslint error
    - remove stale test
    
    * parse json in withPropsOnChange

---
+++ src/packages/@ncigdc/dux/customFacets.js
@@ -0,0 +1,92 @@
+// @flow
+import { REHYDRATE } from 'redux-persist/constants';
+import { uniqBy } from 'lodash';
+
+// Custom
+import { namespaceActions } from './utils';
+
+/*----------------------------------------------------------------------------*/
+
+const customFacets = namespaceActions('customFacets', [
+  'ADD_FACET',
+  'REMOVE_FACET',
+  'RESET_FACETS',
+]);
+
+const add = ({ entityType, facet }: { entityType: string, facet: Object }) => ({
+  type: customFacets.ADD_FACET,
+  payload: { entityType, facet },
+});
+
+const remove = ({
+  entityType,
+  field,
+}: {
+  entityType: string,
+  field: string,
+}) => ({
+  type: customFacets.REMOVE_FACET,
+  payload: { entityType, field },
+});
+
+const reset = ({ entityType }: { entityType: string }) => ({
+  type: customFacets.RESET_FACETS,
+  payload: { entityType },
+});
+
+const entityTypesWithCustomFacets = [
+  'RepositoryCases',
+  'ExploreCases',
+  'Files',
+];
+
+const initialState = entityTypesWithCustomFacets.reduce(
+  (acc, type) => ({
+    ...acc,
+    [type]: [],
+  }),
+  {},
+);
+
+const reducer = (
+  state: Object = initialState,
+  action: {
+    type: string,
+    payload: { entityType: string, field?: string, facet?: Object },
+  },
+) => {
+  switch (action.type) {
+    case REHYDRATE: {
+      const { customFacets } = action.payload;
+      if (customFacets) return { ...state, ...customFacets };
+      return state;
+    }
+
+    case customFacets.ADD_FACET: {
+      const { entityType, facet } = action.payload;
+      return {
+        ...state,
+        [entityType]: uniqBy([...state[entityType], facet], ({ full }) => full),
+      };
+    }
+
+    case customFacets.REMOVE_FACET: {
+      const { entityType, field } = action.payload;
+      return {
+        ...state,
+        [entityType]: state[entityType].filter(facet => facet.field !== field),
+      };
+    }
+
+    case customFacets.RESET_FACETS: {
+      const { entityType } = action.payload;
+      return { ...state, [entityType]: [] };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export { add, remove, reset };
+export default reducer;

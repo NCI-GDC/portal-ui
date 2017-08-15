3715b22d21aba6bd165cf97581f786665c3bad76
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-08-15 13:32:12 -0400

    ✨ set table instead of dropdown
    key on id instead of label

--- src/packages/@ncigdc/components/Modals/SaveSetModal.js
+++ src/packages/@ncigdc/components/Modals/SaveSetModal.js
@@ -8,6 +8,7 @@ import { addSet } from '@ncigdc/dux/sets';
 import { setModal } from '@ncigdc/dux/modal';
 import { ExclamationTriangleIcon } from '@ncigdc/theme/icons';
 import filtersToName from '@ncigdc/utils/filtersToName';
+import { updateSet } from '../../dux/sets';
 
 const enhance = compose(
   withState(
@@ -28,47 +29,57 @@ const SaveSetModal = ({
   dispatch,
   type,
   sets,
-}) =>
-  <BaseModal
-    title={title}
-    extraButtons={
-      <CreateSetButton
-        disabled={!input}
-        filters={filters}
-        onComplete={setId => {
-          dispatch(setModal(null));
-          dispatch(addSet({ type, label: input, id: setId }));
-        }}
-      >
-        Save
-      </CreateSetButton>
-    }
-    closeText="Cancel"
-  >
-    <label htmlFor="save-set-modal-name">Name: </label>
-    <input
-      autoFocus
-      value={input}
-      onChange={e => setInput(e.target.value)}
-      id="save-set-modal-name"
-      type="text"
-    />
-    {sets[input] &&
-      <div
-        style={{
-          marginTop: 10,
-          paddingRight: '5px',
-          backgroundColor: '#fcf8e3',
-          borderColor: '#faebcc',
-          color: '#8a6d3b',
-          padding: '15px',
-          border: '1px solid transparent',
-          borderRadius: '4px',
-        }}
-      >
-        <ExclamationTriangleIcon /> Warning: A set with the same name exists,
-        this will overwrite it.
-      </div>}
-  </BaseModal>;
+}) => {
+  const existingSet = Object.entries(sets).find(([, label]) => label === input);
+  return (
+    <BaseModal
+      title={title}
+      extraButtons={
+        <CreateSetButton
+          disabled={!input}
+          filters={filters}
+          onComplete={setId => {
+            dispatch(setModal(null));
+            if (existingSet) {
+              dispatch(
+                updateSet({ type, oldId: existingSet[0], newId: setId }),
+              );
+            } else {
+              dispatch(addSet({ type, label: input, id: setId }));
+            }
+          }}
+        >
+          Save
+        </CreateSetButton>
+      }
+      closeText="Cancel"
+    >
+      <label htmlFor="save-set-modal-name">Name: </label>
+      <input
+        autoFocus
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        id="save-set-modal-name"
+        type="text"
+      />
+      {existingSet &&
+        <div
+          style={{
+            marginTop: 10,
+            paddingRight: '5px',
+            backgroundColor: '#fcf8e3',
+            borderColor: '#faebcc',
+            color: '#8a6d3b',
+            padding: '15px',
+            border: '1px solid transparent',
+            borderRadius: '4px',
+          }}
+        >
+          <ExclamationTriangleIcon /> Warning: A set with the same name exists,
+          this will overwrite it.
+        </div>}
+    </BaseModal>
+  );
+};
 
 export default enhance(SaveSetModal);
---
+++ src/packages/@ncigdc/modern_components/Counts/exploreCase.relay.js
@@ -0,0 +1,30 @@
+// @flow
+import React from 'react';
+import { BaseQuery } from '@ncigdc/modern_components/Query';
+import { graphql } from 'react-relay';
+
+export default (Component: ReactClass<*>) => (props: Object) => {
+  return (
+    <BaseQuery
+      parentProps={{ path: 'explore.cases.hits.total', ...props }}
+      name="ExploreCaseCount"
+      variables={{ filters: props.filters }}
+      Component={Component}
+      query={graphql`
+        query exploreCase_relayQuery(
+          $filters: FiltersArgument
+        ) {
+          viewer {
+            explore {
+              cases {
+                hits(filters: $filters first: 0) {
+                  total
+                }
+              }
+            }
+          }
+        }
+      `}
+    />
+  );
+};

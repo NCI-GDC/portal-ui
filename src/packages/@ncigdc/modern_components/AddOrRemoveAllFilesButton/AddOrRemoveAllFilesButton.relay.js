123ca351fa346df5f5d23adfbda4a3faba24deaf
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-08-16 14:08:35 -0400

    ♻️  update query loaders to not need redux state

--- src/packages/@ncigdc/modern_components/AddOrRemoveAllFilesButton/AddOrRemoveAllFilesButton.relay.js
+++ src/packages/@ncigdc/modern_components/AddOrRemoveAllFilesButton/AddOrRemoveAllFilesButton.relay.js
@@ -4,8 +4,7 @@ import React from 'react';
 import { graphql } from 'react-relay';
 import { makeFilter } from '@ncigdc/utils/filters';
 import { compose, withPropsOnChange, branch, renderComponent } from 'recompose';
-import Query from '@ncigdc/modern_components/Query';
-import Button from '@ncigdc/uikit/Button';
+import { BaseQuery } from '@ncigdc/modern_components/Query';
 
 export default (Component: ReactClass<*>) =>
   compose(
@@ -32,22 +31,11 @@ export default (Component: ReactClass<*>) =>
     ),
   )((props: Object) => {
     return (
-      <Query
+      <BaseQuery
         parentProps={props}
         name="AddOrRemoveAllFilesButton"
-        minHeight={50}
         variables={props.variables}
         Component={Component}
-        customLoader={({ loading }) =>
-          !loading
-            ? null
-            : <Button>
-                <i className="fa fa-spinner fa-spin" />&nbsp;<span
-                  style={{ color: 'rgb(154, 176, 189)' }}
-                >
-                  Add all files to the cart
-                </span>
-              </Button>}
         query={graphql`
           query AddOrRemoveAllFilesButton_relayQuery(
             $filters: FiltersArgument

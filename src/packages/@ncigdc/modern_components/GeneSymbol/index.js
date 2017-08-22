e46c1ee2d334a9ec618b24cacd0d143459831746
Christine <christine.tc.yu@gmail.com>
2017-08-22 12:27:17 -0400

    code split (#2024)
    * :sparkles: PRTL-1457 code split by route
    
    * split on common components

--- src/packages/@ncigdc/modern_components/GeneSymbol/index.js
+++ src/packages/@ncigdc/modern_components/GeneSymbol/index.js
@@ -1,6 +1,11 @@
+import LoadableWithLoading from '@ncigdc/components/LoadableWithLoading';
 import PropTypes from 'prop-types';
-import Component from './GeneSymbol';
 import createRenderer from './GeneSymbol.relay';
+
+const Component = LoadableWithLoading({
+  loader: () => import('./GeneSymbol'),
+});
+
 const GeneSymbol = createRenderer(Component);
 GeneSymbol.propTypes = {
   geneId: PropTypes.string,

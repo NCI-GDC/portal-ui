84e2f109840af88327560f2bdd01d9c34416926f
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-08-22 16:36:29 -0400

    ♻️ cleanup header

--- src/packages/@ncigdc/uikit/Links/ExternalLink.js
+++ src/packages/@ncigdc/uikit/Links/ExternalLink.js
@@ -2,21 +2,17 @@ import React from 'react';
 import ExternalLinkIcon from 'react-icons/lib/fa/external-link';
 
 export default function ExternalLink({
-  href,
   children,
-  style,
-  title,
   hasExternalIcon = true,
+  className = '',
   ...props
 }): React.Element {
   return (
     <a
-      href={href}
+      {...props}
       target="_blank"
       rel="noopener noreferrer"
-      style={style}
-      title={title}
-      className={props.className + ' test-external-link'}
+      className={className + ' test-external-link'}
     >
       {hasExternalIcon &&
         <ExternalLinkIcon style={{ marginRight: '0.5rem' }} />}

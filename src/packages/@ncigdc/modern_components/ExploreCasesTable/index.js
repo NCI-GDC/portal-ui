a233c12bbdae1e7e0040310aee7da9a81fa7ff70
Alex Wilmer <djazium@gmail.com>
2017-08-29 14:31:05 -0400

    ♻️ modern ExploreCasesTable (#2065)

--- src/packages/@ncigdc/modern_components/ExploreCasesTable/index.js
+++ src/packages/@ncigdc/modern_components/ExploreCasesTable/index.js
@@ -1,2 +1,9 @@
-import ExploreCasesTable from './ExploreCasesTable';
-export default ExploreCasesTable;
+import LoadableWithLoading from '@ncigdc/components/LoadableWithLoading';
+import withCaseData from './ExploreCasesTable.relay';
+import withSsmData from './ExploreCasesSsmsAggregations.relay';
+
+const Component = LoadableWithLoading({
+  loader: () => import('./ExploreCasesTable'),
+});
+
+export default withCaseData(withSsmData(Component));

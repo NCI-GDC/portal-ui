0069ffb4024a2cb138b00a4536d95bf9909957e8
Alex Wilmer <djazium@gmail.com>
2017-08-29 12:05:38 -0400

    check localstorage for react envs first

--- src/packages/@ncigdc/utils/constants.js
+++ src/packages/@ncigdc/utils/constants.js
@@ -1,8 +1,9 @@
 // @flow
 
-export const AUTH = process.env.REACT_APP_GDC_AUTH || '';
+export const AUTH =
+  localStorage.REACT_APP_GDC_AUTH || process.env.REACT_APP_GDC_AUTH || '';
 export const AUTH_API = `${AUTH}/api`;
-export const API = process.env.REACT_APP_API;
+export const API = localStorage.REACT_APP_API || process.env.REACT_APP_API;
 
 export const DATA_CATEGORIES = {
   SEQ: { full: 'Raw Sequencing Data', abbr: 'Seq' },

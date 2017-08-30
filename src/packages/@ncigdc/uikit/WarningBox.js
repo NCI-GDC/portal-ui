ab051a15d238a8cd8a2cddae6f992b8410974fae
Christine <christine.tc.yu@gmail.com>
2017-08-30 16:08:46 -0400

    :sparkles: PRTL-1597 add Manage Sets page (#2042)

---
+++ src/packages/@ncigdc/uikit/WarningBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ExclamationTriangleIcon } from '@ncigdc/theme/icons';
+
+export default ({ children, style, ...props }) =>
+  <div
+    style={{
+      marginTop: 10,
+      paddingRight: '5px',
+      backgroundColor: '#fcf8e3',
+      borderColor: '#faebcc',
+      color: '#8a6d3b',
+      padding: '15px',
+      border: '1px solid transparent',
+      borderRadius: '4px',
+      ...style,
+    }}
+    {...props}
+  >
+    <ExclamationTriangleIcon /> {children}
+  </div>;

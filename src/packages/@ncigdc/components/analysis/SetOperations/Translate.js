f24e643e3d902e3962c385f3cfe1f80f2c1fcae4
Alex Wilmer <djazium@gmail.com>
2017-08-31 11:42:24 -0400

    ✨ [PRTL-1600] set operations

---
+++ src/packages/@ncigdc/components/analysis/SetOperations/Translate.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default ({ x = 0, y = 0, children }) =>
+  <div style={{ position: 'absolute', transform: `translate(${x}px, ${y}px)` }}>
+    {children}
+  </div>;

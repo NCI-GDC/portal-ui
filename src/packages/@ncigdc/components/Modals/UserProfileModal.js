bc8407a4a9bf82bf525ecfdaa55a0ab8c1a14821
Christine <christine.tc.yu@gmail.com>
2017-09-01 18:54:52 -0400

    1458 user profile page (#2052)

---
+++ src/packages/@ncigdc/components/Modals/UserProfileModal.js
@@ -0,0 +1,80 @@
+// @flow
+import React from 'react';
+import BaseModal from '@ncigdc/components/Modals/BaseModal';
+import { compose } from 'recompose';
+import { connect } from 'react-redux';
+
+import Hidden from '@ncigdc/components/Hidden';
+import { withTheme } from '@ncigdc/theme';
+import Check from '@ncigdc/theme/icons/Check';
+import { Row } from '@ncigdc/uikit/Flex';
+import EntityPageHorizontalTable from '@ncigdc/components/EntityPageHorizontalTable';
+import withRouter from '@ncigdc/utils/withRouter';
+
+const enhance = compose(
+  connect(({ auth: { user } }) => ({ user })),
+  withTheme,
+  withRouter,
+);
+
+const UserProfileModal = ({
+  user: { username, projects },
+  theme,
+  push,
+  query,
+}) => {
+  const flattenedProjects = Object.keys(projects).reduce(
+    (acc, k) => ({ ...acc, ...projects[k] }),
+    {},
+  );
+  const allValues = [
+    ...new Set(
+      Object.keys(flattenedProjects).reduce(
+        (acc, projectId) => [...acc, ...flattenedProjects[projectId]],
+        [],
+      ),
+    ),
+  ];
+
+  const data = Object.keys(flattenedProjects).map((projectId, i) => ({
+    projectId,
+    ...allValues.reduce(
+      (acc, v) => ({
+        ...acc,
+        [v]: flattenedProjects[projectId].includes(v)
+          ? <span><Check /><Hidden>True</Hidden></span>
+          : <Hidden>False</Hidden>,
+      }),
+      {},
+    ),
+  }));
+  return (
+    <BaseModal
+      title={`Username: ${username}`}
+      closeText="Done"
+      contentStyle={{ padding: 0 }}
+    >
+      <div
+        style={{
+          overflow: 'auto',
+          // calc instead of using flex because IE11 doesn't handle flex + max-height properly
+          maxHeight: 'calc(100vh - 200px)',
+        }}
+      >
+        <Row style={{ justifyContent: 'center' }}>
+          <EntityPageHorizontalTable
+            emptyMessage={'User has no projects'}
+            headings={[
+              { key: 'projectId', title: 'Project ID' },
+              ...allValues.map(v => ({ key: v, title: v })),
+            ]}
+            style={{ width: '100%' }}
+            data={data}
+          />
+        </Row>
+      </div>
+    </BaseModal>
+  );
+};
+
+export default enhance(UserProfileModal);

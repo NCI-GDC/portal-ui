87b2651ef87e6c5de170e4b6dc44a658a806dce2
Alex Wilmer <djazium@gmail.com>
2017-09-05 17:26:15 -0400

    ♻️ modern FilesTable (#2056)

--- src/packages/@ncigdc/tableModels/index.js
+++ src/packages/@ncigdc/tableModels/index.js
@@ -1,13 +1,12 @@
 // @flow
 
 import cases from './casesTableModel';
-import files from './filesTableModel';
 import annotations from './annotationsTableModel';
-
 import exploreCases from '@ncigdc/modern_components/ExploreCasesTable/ExploreCasesTable.model';
 import projects from '@ncigdc/modern_components/ProjectsTable/ProjectsTable.model';
 import genes from '@ncigdc/modern_components/GenesTable/GenesTable.model';
 import ssms from '@ncigdc/modern_components/SsmsTable/SsmsTable.model';
+import files from '@ncigdc/modern_components/FilesTable/FilesTable.model';
 
 export default {
   projects,

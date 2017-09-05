74cc6ca250d4c7bb6fb8ba27c8824c0af0391064
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-09-05 12:19:03 -0400

    load d3v3

---
+++ src/packages/@ncigdc/utils/loadScript.js
@@ -0,0 +1,19 @@
+import loadScript from 'load-script';
+
+export default function(src) {
+  return new Promise(function(resolve, reject) {
+    const existingScript = document.querySelector(`script[src="${src}"]`);
+
+    if (existingScript) {
+      resolve();
+    } else {
+      loadScript(src, err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    }
+  });
+}

9ab5ceee4b1746e7f9a6175cfbf3ef4d2caf86fb
Alex Wilmer <djazium@gmail.com>
2017-09-06 12:03:21 -0400

    style cohort page

--- src/packages/@ncigdc/modern_components/CohortComparison/CohortComparison.js
+++ src/packages/@ncigdc/modern_components/CohortComparison/CohortComparison.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose, withState, withProps } from 'recompose';
-import { union, find } from 'lodash';
-
+import Table, { Tr, Td, Th } from '@ncigdc/uikit/Table';
 import { withTheme } from '@ncigdc/theme';
+import { Row } from '@ncigdc/uikit/Flex';
 import SurvivalPlotWrapper from '@ncigdc/components/SurvivalPlotWrapper';
 import withPropsOnChange from '@ncigdc/utils/withPropsOnChange';
 import { getDefaultCurve } from '@ncigdc/utils/survivalplot';
+import FacetTable from './FacetTable';
 
 const mapping = {
   'demographic.gender': 'Gender',
@@ -18,69 +19,9 @@ const initialState = {
   loading: true,
 };
 
-const section = (
-  field,
-  data1,
-  data2,
-  result1,
-  result2,
-  set1_name,
-  set1_colour,
-  set2_name,
-  set2_colour,
-) => {
-  return (
-    <div>
-      <h2>{mapping[field]}</h2>
-      <table style={{ width: '80%' }}>
-        <thead>
-          <tr>
-            <th />
-            <th style={{ textAlign: 'center', color: set1_colour }} colSpan={2}>
-              {set1_name}
-            </th>
-            <th style={{ textAlign: 'center', color: set2_colour }} colSpan={2}>
-              {set2_name}
-            </th>
-          </tr>
-          <tr>
-            <th>{mapping[field]}</th>
-            <th style={{ textAlign: 'right' }}># of items</th>
-            <th style={{ textAlign: 'right' }}>%</th>
-            <th style={{ textAlign: 'right' }}># of items</th>
-            <th style={{ textAlign: 'right' }}>%</th>
-          </tr>
-        </thead>
-        <tbody>
-          {union(
-            data1[field].buckets.map(b => b.key),
-            data2[field].buckets.map(b => b.key),
-          ).map(k => {
-            const set1_bucket =
-              (find(data1[field].buckets, b => b.key === k) || {}).doc_count ||
-              0;
-            const set2_bucket =
-              (find(data2[field].buckets, b => b.key === k) || {}).doc_count ||
-              0;
-            return (
-              <tr key={k}>
-                <td width={250}>{k}</td>
-                <td style={{ textAlign: 'right' }}>{set1_bucket}</td>
-                <td style={{ textAlign: 'right' }}>
-                  {(set1_bucket / result1.hits.total * 100).toFixed(0)}%
-                </td>
-                <td style={{ textAlign: 'right' }}>{set2_bucket}</td>
-                <td style={{ textAlign: 'right' }}>
-                  {(set2_bucket / result2.hits.total * 100).toFixed(0)}%
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+const Alias = ({ i, style = { fontWeight: 'bold' } }) =>
+  <span style={style}><em>S</em><sub>{i}</sub></span>;
+
 export default compose(
   connect(({ sets }) => ({ sets })),
   withState('defaultSurvivalData', 'setDefaultSurvivalData', {}),
@@ -142,104 +83,104 @@ export default compose(
     const data2 = JSON.parse(result2.facets);
 
     const set1_name = sets.case[set1];
-    const set1_colour = 'orange';
+    const set1_colour = 'rgb(158, 124, 36)';
     const set2_name = sets.case[set2];
-    const set2_colour = 'skyblue';
-    console.log(survivalData);
+    const set2_colour = 'rgb(29, 97, 135)';
+
     return (
-      <div style={{ margin: '0 3rem' }}>
-        <h1>Cohort Comparison</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Selected Cohorts</th>
-              <th style={{ textAlign: 'right' }}># of cases</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td style={{ width: '150px', color: set1_colour }}>
-                {set1_name}
-              </td>
-              <td style={{ textAlign: 'right' }}>{result1.hits.total}</td>
-            </tr>
-            <tr>
-              <td style={{ width: '150px', color: set2_colour }}>
-                {set2_name}
-              </td>
-              <td style={{ textAlign: 'right' }}>{result2.hits.total}</td>
-            </tr>
-          </tbody>
-        </table>
-        <h2>Survival Analysis</h2>
-        <div style={{ width: '50%' }}>
-          <SurvivalPlotWrapper {...survivalData} height={240} />
-          {survivalData.rawData &&
-            <table style={{ width: '80%' }}>
-              <thead>
-                <tr>
-                  <th />
-                  <th
-                    style={{ textAlign: 'center', color: set1_colour }}
-                    colSpan={2}
-                  >
-                    {set1_name}
-                  </th>
-                  <th
-                    style={{ textAlign: 'center', color: set2_colour }}
-                    colSpan={2}
-                  >
-                    {set2_name}
-                  </th>
-                </tr>
-                <tr>
-                  <th>Cases included in Analysis</th>
-                  <th style={{ textAlign: 'right' }}># of items</th>
-                  <th style={{ textAlign: 'right' }}>%</th>
-                  <th style={{ textAlign: 'right' }}># of items</th>
-                  <th style={{ textAlign: 'right' }}>%</th>
-                </tr>
-              </thead>
+      <Row style={{ margin: '0 3rem' }}>
+        <div style={{ flex: 8 }}>
+          <h1>Cohort Comparison</h1>
+          <Table
+            headings={[
+              <Th key="1" style={{ backgroundColor: 'white' }}>
+                Selected Cohorts
+              </Th>,
+              <Th
+                key="2"
+                style={{ textAlign: 'right', backgroundColor: 'white' }}
+              >
+                # of cases
+              </Th>,
+            ]}
+            body={
               <tbody>
-                <tr>
-                  <td width={250}>Overall Survival Analysis</td>
-                  <td style={{ textAlign: 'right' }}>
-                    {survivalData.rawData.results[0].donors.length}
-                  </td>
-                  <td style={{ textAlign: 'right' }}>
-                    {(survivalData.rawData.results[0].donors.length /
-                      result1.hits.total *
-                      100).toFixed(0)}%
-                  </td>
-                  <td style={{ textAlign: 'right' }}>
-                    {survivalData.rawData.results[1].donors.length}
-                  </td>
-                  <td style={{ textAlign: 'right' }}>
-                    {(survivalData.rawData.results[1].donors.length /
-                      result2.hits.total *
-                      100).toFixed(0)}%
-                  </td>
-                </tr>
-
+                <Tr>
+                  <Td style={{ width: '150px', color: set1_colour }}>
+                    <Alias i={1} /> : {set1_name}
+                  </Td>
+                  <Td style={{ textAlign: 'right' }}>{result1.hits.total}</Td>
+                </Tr>
+                <Tr>
+                  <Td style={{ width: '150px', color: set2_colour }}>
+                    <Alias i={2} /> : {set2_name}
+                  </Td>
+                  <Td style={{ textAlign: 'right' }}>{result2.hits.total}</Td>
+                </Tr>
               </tbody>
-            </table>}
+            }
+          />
+
+          <h2>Survival Analysis</h2>
+          <div style={{ width: '50%' }}>
+            <SurvivalPlotWrapper {...survivalData} height={240} />
+            {survivalData.rawData &&
+              <Table
+                headings={[
+                  <Th>Cases included in Analysis</Th>,
+                  <Th style={{ textAlign: 'right' }}>
+                    # of items in <Alias i={1} />
+                  </Th>,
+                  <Th style={{ textAlign: 'right' }}>%</Th>,
+                  <Th style={{ textAlign: 'right' }}>
+                    # of items in <Alias i={2} />
+                  </Th>,
+                  <Th style={{ textAlign: 'right' }}>%</Th>,
+                ]}
+                body={
+                  <tbody>
+                    <Tr>
+                      <Td width={250}>Overall Survival Analysis</Td>
+                      <Td style={{ textAlign: 'right' }}>
+                        {survivalData.rawData.results[0] &&
+                          survivalData.rawData.results[0].donors.length}
+                      </Td>
+                      <Td style={{ textAlign: 'right' }}>
+                        {survivalData.rawData.results[0] &&
+                          (survivalData.rawData.results[0].donors.length /
+                            result1.hits.total *
+                            100).toFixed(0)}%
+                      </Td>
+                      <Td style={{ textAlign: 'right' }}>
+                        {survivalData.rawData.results[1] &&
+                          survivalData.rawData.results[1].donors.length}
+                      </Td>
+                      <Td style={{ textAlign: 'right' }}>
+                        {survivalData.rawData.results[1] &&
+                          (survivalData.rawData.results[1].donors.length /
+                            result2.hits.total *
+                            100).toFixed(0)}%
+                      </Td>
+                    </Tr>
+                  </tbody>
+                }
+              />}
+          </div>
+
+          {facets.map(field =>
+            FacetTable({
+              mapping,
+              field,
+              data1,
+              data2,
+              result1,
+              result2,
+              Alias,
+            }),
+          )}
         </div>
-
-        {facets.map(f =>
-          section(
-            f,
-            data1,
-            data2,
-            result1,
-            result2,
-            set1_name,
-            set1_colour,
-            set2_name,
-            set2_colour,
-          ),
-        )}
-
-      </div>
+        <div style={{ flex: 4 }} />
+      </Row>
     );
   },
 );

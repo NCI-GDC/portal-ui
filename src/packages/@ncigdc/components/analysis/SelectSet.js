75368d90ce83f8e20c45d88b86d508646a1d4295
Alex Wilmer <djazium@gmail.com>
2017-09-06 11:03:30 -0400

    style select set page

--- src/packages/@ncigdc/components/analysis/SelectSet.js
+++ src/packages/@ncigdc/components/analysis/SelectSet.js
@@ -1,9 +1,10 @@
 // @flow
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { compose, withState } from 'recompose';
 import { xor, omit } from 'lodash';
-import { Row } from '@ncigdc/uikit/Flex';
+import { Row, Column } from '@ncigdc/uikit/Flex';
 import EntityPageHorizontalTable from '@ncigdc/components/EntityPageHorizontalTable';
 import countComponents from '@ncigdc/modern_components/Counts';
 import { Tooltip } from '@ncigdc/uikit/Tooltip';
@@ -16,12 +17,14 @@ type TProps = {
   selectedSets: {},
   setSelectedSets: Function,
   setDisabledMessage: Function,
-  title: string,
+  label: string,
   setInstructions: string,
   onCancel: Function,
   onDemo: Function,
   onRun: Function,
   validateSets: Function,
+  description: string,
+  Icon: Function,
 };
 
 const enhance = compose(
@@ -34,12 +37,14 @@ const SetTable = ({
   selectedSets,
   setSelectedSets,
   setDisabledMessage,
-  title,
+  label,
   setInstructions,
   onCancel,
   onDemo,
   onRun,
   validateSets,
+  Icon,
+  description,
 }: TProps) => {
   const headings = [
     { key: 'select', title: ' ' },
@@ -90,7 +95,7 @@ const SetTable = ({
             </Tooltip>
           ),
           type: type.replace(/^./, m => m.toUpperCase()),
-          name: <label htmlFor={id}>{label}</label>,
+          name: <label htmlFor={id}>{_.truncate(label, { length: 70 })}</label>,
           count: (
             <CountComponent
               filters={{
@@ -109,45 +114,73 @@ const SetTable = ({
 
   return (
     <div>
-      <div style={{ padding: '2rem 2.5rem 0' }}>
-        You can create and save case and gene sets of interest from the{' '}
-        <ExploreLink>Exploration Page</ExploreLink>
-        <h1 style={{ fontSize: '2rem' }}>{title}</h1>
-        <div>{setInstructions}</div>
-      </div>
+      <Row>
+        <Column style={{ padding: '2rem 2.5rem 0' }}>
+          <Row>
+            <Row
+              style={{
+                width: 120,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Icon style={{ fontSize: 40, marginRight: 25 }} />
+            </Row>
+            <div>
+              <h1 style={{ fontSize: '2rem' }}>{label}</h1>
+              <div style={{ marginBottom: 10 }}>{description}</div>
+            </div>
+          </Row>
+          <Row
+            spacing={5}
+            style={{
+              marginTop: 'auto',
+              justifyContent: 'flex-end',
+              padding: '1rem 2.5rem 1rem',
+              borderTop: `1px solid ${theme.greyScale5}`,
+            }}
+          >
+            <Button onClick={onCancel}>Cancel</Button>
+            <Button onClick={onDemo}>Demo</Button>
+            <Button
+              disabled={!validateSets(selectedSets)}
+              onClick={() => onRun(selectedSets)}
+            >
+              Run
+            </Button>
+          </Row>
+        </Column>
 
-      {setData.length
-        ? <EntityPageHorizontalTable data={setData} headings={headings} />
-        : <EntityPageHorizontalTable
-            data={[{ select: 'No saved set' }]}
-            headings={[
-              {
-                ...headings[0],
-                tdProps: { colSpan: 4 },
-                tdStyle: { textAlign: 'center' },
-              },
-              ...headings.slice(1).map(h => ({
-                ...h,
-                tdStyle: { display: 'none' },
-              })),
-            ]}
-          />}
-      <Row
-        spacing={5}
-        style={{
-          justifyContent: 'flex-end',
-          padding: '1rem 2.5rem 1rem',
-          borderTop: `1px solid ${theme.greyScale5}`,
-        }}
-      >
-        <Button onClick={onCancel}>Cancel</Button>
-        <Button onClick={onDemo}>Demo</Button>
-        <Button
-          disabled={!validateSets(selectedSets)}
-          onClick={() => onRun(selectedSets)}
-        >
-          Run
-        </Button>
+        {setData.length
+          ? <div style={{ flex: 1 }}>
+              <div style={{ margin: '2rem 0' }}>
+                <div>
+                  <strong>{setInstructions}</strong>
+                </div>
+                <div style={{ fontStyle: 'italic' }}>
+                  You can create and save case, gene and mutation sets of
+                  interest
+                  from
+                  the{' '}
+                  <ExploreLink>Exploration Page</ExploreLink>
+                </div>
+              </div>
+              <EntityPageHorizontalTable data={setData} headings={headings} />
+            </div>
+          : <EntityPageHorizontalTable
+              data={[{ select: 'No saved set' }]}
+              headings={[
+                {
+                  ...headings[0],
+                  tdProps: { colSpan: 4 },
+                  tdStyle: { textAlign: 'center' },
+                },
+                ...headings.slice(1).map(h => ({
+                  ...h,
+                  tdStyle: { display: 'none' },
+                })),
+              ]}
+            />}
       </Row>
     </div>
   );

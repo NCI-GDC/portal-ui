28afdd8a8b2169de1a834d31d2a66434d81082c9
Alex Wilmer <djazium@gmail.com>
2017-09-12 12:45:04 -0400

    🐛 mapState shouldn't return `null`

--- src/packages/@ncigdc/components/ModalContainer.js
+++ src/packages/@ncigdc/components/ModalContainer.js
@@ -5,9 +5,9 @@ import { connect } from 'react-redux';
 import { setModal } from '@ncigdc/dux/modal';
 import Modal from '@ncigdc/uikit/Modal';
 
-const ModalContainer = connect(
-  state => state.modal,
-)(({ component, autoClose, dispatch }) =>
+const ModalContainer = connect(state => ({
+  ...state.modal,
+}))(({ component, autoClose, dispatch }) =>
   <Modal
     isOpen={!!component}
     onRequestClose={() => autoClose && dispatch(setModal(null))}

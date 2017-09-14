97d554a084d1cfb92ac187a79a8bf30a769adfc0
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-09-14 14:20:44 -0400

    ✨ [PRTL-1666] add tsv button

--- src/packages/@ncigdc/modern_components/CohortComparison/FacetTable.js
+++ src/packages/@ncigdc/modern_components/CohortComparison/FacetTable.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { compose } from 'recompose';
 import { union, find, truncate } from 'lodash';
-import Table, { Tr, Td, Th } from '@ncigdc/uikit/Table';
-import { Row } from '@ncigdc/uikit/Flex';
 import ExploreLink from '@ncigdc/components/Links/ExploreLink';
 import BarChart from '@ncigdc/components/Charts/TwoBarChart';
 import { withTheme } from '@ncigdc/theme';
 import Pvalue from '@ncigdc/modern_components/Pvalue';
 import Alias from '@ncigdc/components/Alias';
+import { visualizingButton } from '@ncigdc/theme/mixins';
+import Button from '@ncigdc/uikit/Button';
+import toTsvString from '@ncigdc/utils/toTsvString';
+import saveFile from '@ncigdc/utils/filesaver';
+import EntityPageHorizontalTable from '@ncigdc/components/EntityPageHorizontalTable';
 
 export default compose(
   withTheme,
@@ -20,191 +23,204 @@ export default compose(
     data2,
     result1,
     result2,
-    Set1,
-    Set2,
     set1,
     set2,
     palette,
-    buckets1 = data1[field].buckets.filter(x => x.key !== '_missing'),
-    buckets2 = data2[field].buckets.filter(x => x.key !== '_missing'),
-    data = union(buckets1.map(b => b.key), buckets2.map(b => b.key)),
-  }) => (
-    <div>
-      <Row>
+  }) => {
+    const buckets1 = data1[field].buckets.filter(x => x.key !== '_missing');
+    const buckets2 = data2[field].buckets.filter(x => x.key !== '_missing');
+    const tableData = union(
+      buckets1.map(b => b.key),
+      buckets2.map(b => b.key),
+    ).map(k => {
+      const bucket1 = find(buckets1, b => b.key === k) || {};
+      const bucket2 = find(buckets2, b => b.key === k) || {};
+
+      const casesS1 = bucket1.doc_count || 0;
+      const casesS2 = bucket2.doc_count || 0;
+
+      return {
+        value: k,
+        casesS1,
+        percentS1: (casesS1 / result1.hits.total * 100).toFixed(0),
+        filters1: bucket1.filters,
+        casesS2,
+        percentS2: (casesS2 / result2.hits.total * 100).toFixed(0),
+        filters2: bucket2.filters,
+      };
+    });
+
+    return (
+      <div>
         <h2>{mapping[field]}</h2>
-        {/* <Row style={{ marginLeft: 'auto', alignItems: 'center' }}>
-        <div>{Set1}</div>
-        <div style={{ margin: '0 10px' }}>{Set2}</div>
-      </Row> */}
-      </Row>
-      <div
-        style={{
-          maxWidth: data.length * 140 + 150, // TODO: use same logic used in TwoBarCharts
-        }}
-      >
-        <BarChart
-          data1={data.map(k => {
-            const bucket = find(buckets1, b => b.key === k) || {};
 
-            return {
-              label: truncate(k, { length: 15 }),
-              value: bucket.doc_count || 0,
-            };
-          })}
-          data2={data.map(k => {
-            const bucket = find(buckets2, b => b.key === k) || {};
-
-            return {
-              label: truncate(k, { length: 15 }),
-              value: bucket.doc_count || 0,
-            };
-          })}
-          yAxis={{ title: '# Cases' }}
-          height={200}
-          styles={{
-            xAxis: {
-              stroke: theme.greyScale4,
-              textFill: theme.greyScale3,
-            },
-            yAxis: {
-              stroke: theme.greyScale4,
-              textFill: theme.greyScale3,
-            },
-            bars1: { fill: palette[0] },
-            bars2: { fill: palette[1] },
-            tooltips: {
-              fill: '#fff',
-              stroke: theme.greyScale4,
-              textFill: theme.greyScale3,
-            },
+        <div
+          style={{
+            maxWidth: tableData.length * 140 + 150, // TODO: use same logic used in TwoBarCharts
           }}
-        />
-      </div>
-      <Table
-        headings={[
-          <Th key="1">{mapping[field]}</Th>,
-          <Th key="2" style={{ textAlign: 'right' }}>
-            # Cases <Alias i={1} />
-          </Th>,
-          <Th key="3" style={{ textAlign: 'right' }}>
-            %
-          </Th>,
-          <Th key="4" style={{ textAlign: 'right' }}>
-            # Cases <Alias i={2} />
-          </Th>,
-          <Th key="5" style={{ textAlign: 'right' }}>
-            %
-          </Th>,
-        ]}
-        body={
-          <tbody>
-            {data.map((k, i) => {
-              const set1_bucket = find(buckets1, b => b.key === k) || {};
-              const set2_bucket = find(buckets2, b => b.key === k) || {};
-
-              return (
-                <Tr key={k} index={i}>
-                  <Td width={250}>{k}</Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {(set1_bucket.doc_count || 0) === 0 ? (
-                      0
-                    ) : (
-                      <ExploreLink
-                        query={{
-                          searchTableTab: 'cases',
-                          filters: {
-                            op: 'and',
-                            content: [
-                              {
-                                op: 'in',
-                                content: {
-                                  field: 'cases.case_id',
-                                  value: [`set_id:${set1}`],
-                                },
-                              },
-                              ...(set1_bucket.filters
-                                ? set1_bucket.filters
-                                : [
-                                    {
-                                      op: 'in',
-                                      content: {
-                                        field: `cases.${field}`,
-                                        value: [set1_bucket.key],
-                                      },
-                                    },
-                                  ]),
-                            ],
-                          },
-                        }}
-                      >
-                        {(set1_bucket.doc_count || 0).toLocaleString()}
-                      </ExploreLink>
-                    )}
-                  </Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {((set1_bucket.doc_count || 0) /
-                      result1.hits.total *
-                      100
-                    ).toFixed(0)}%
-                  </Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {(set2_bucket.doc_count || 0) === 0 ? (
-                      0
-                    ) : (
-                      <ExploreLink
-                        query={{
-                          searchTableTab: 'cases',
-                          filters: {
-                            op: 'and',
-                            content: [
-                              {
-                                op: 'in',
-                                content: {
-                                  field: 'cases.case_id',
-                                  value: [`set_id:${set2}`],
-                                },
-                              },
-                              ...(set2_bucket.filters
-                                ? set2_bucket.filters
-                                : [
-                                    {
-                                      op: 'in',
-                                      content: {
-                                        field: `cases.${field}`,
-                                        value: [set2_bucket.key],
-                                      },
-                                    },
-                                  ]),
-                            ],
-                          },
-                        }}
-                      >
-                        {(set2_bucket.doc_count || 0).toLocaleString()}
-                      </ExploreLink>
-                    )}
-                  </Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {((set2_bucket.doc_count || 0) /
-                      result2.hits.total *
-                      100
-                    ).toFixed(0)}%
-                  </Td>
-                </Tr>
-              );
-            })}
-          </tbody>
-        }
-      />
-      <div style={{ textAlign: 'right' }}>
-        {[buckets1.length, buckets2.length].every(l => l === 2) && (
-          <Pvalue
-            data={[
-              buckets1.map(x => x.doc_count),
-              buckets2.map(x => x.doc_count),
-            ]}
+        >
+          <BarChart
+            data1={tableData.map(({ value, casesS1 }) => ({
+              label: truncate(value, { length: 15 }),
+              value: casesS1,
+            }))}
+            data2={tableData.map(({ value, casesS2 }) => ({
+              label: truncate(value, { length: 15 }),
+              value: casesS2,
+            }))}
+            yAxis={{ title: '# Cases' }}
+            height={200}
+            styles={{
+              xAxis: {
+                stroke: theme.greyScale4,
+                textFill: theme.greyScale3,
+              },
+              yAxis: {
+                stroke: theme.greyScale4,
+                textFill: theme.greyScale3,
+              },
+              bars1: { fill: palette[0] },
+              bars2: { fill: palette[1] },
+              tooltips: {
+                fill: '#fff',
+                stroke: theme.greyScale4,
+                textFill: theme.greyScale3,
+              },
+            }}
           />
-        )}
+        </div>
+        <EntityPageHorizontalTable
+          rightComponent={
+            <Button
+              style={{ ...visualizingButton }}
+              onClick={() =>
+                saveFile(
+                  toTsvString(tableData),
+                  'TSV',
+                  `${mapping[field]}-comparison.tsv`,
+                )}
+            >
+              TSV
+            </Button>
+          }
+          headings={[
+            { key: 'value', title: mapping[field], tdStyle: { maxWidth: 250 } },
+            {
+              key: 'casesS1',
+              title: (
+                <span>
+                  # Cases <Alias i={1} />
+                </span>
+              ),
+              style: { textAlign: 'right' },
+            },
+            {
+              key: 'percentS1',
+              title: '%',
+              style: { textAlign: 'right' },
+            },
+            {
+              key: 'casesS2',
+              title: (
+                <span>
+                  # Cases <Alias i={2} />
+                </span>
+              ),
+              style: { textAlign: 'right' },
+            },
+            {
+              key: 'percentS2',
+              title: '%',
+              style: { textAlign: 'right' },
+            },
+          ]}
+          data={tableData.map(row => ({
+            ...row,
+            casesS1:
+              row.casesS1 === 0 ? (
+                0
+              ) : (
+                <ExploreLink
+                  query={{
+                    searchTableTab: 'cases',
+                    filters: {
+                      op: 'and',
+                      content: [
+                        {
+                          op: 'in',
+                          content: {
+                            field: 'cases.case_id',
+                            value: [`set_id:${set1}`],
+                          },
+                        },
+                        ...(row.filters
+                          ? row.filters
+                          : [
+                              {
+                                op: 'in',
+                                content: {
+                                  field: `cases.${field}`,
+                                  value: [row.value],
+                                },
+                              },
+                            ]),
+                      ],
+                    },
+                  }}
+                >
+                  {row.casesS1.toLocaleString()}
+                </ExploreLink>
+              ),
+            percentS1: `${row.percentS1}%`,
+            casesS2:
+              row.casesS2 === 0 ? (
+                0
+              ) : (
+                <ExploreLink
+                  query={{
+                    searchTableTab: 'cases',
+                    filters: {
+                      op: 'and',
+                      content: [
+                        {
+                          op: 'in',
+                          content: {
+                            field: 'cases.case_id',
+                            value: [`set_id:${set2}`],
+                          },
+                        },
+                        ...(row.filters
+                          ? row.filters
+                          : [
+                              {
+                                op: 'in',
+                                content: {
+                                  field: `cases.${field}`,
+                                  value: [row.value],
+                                },
+                              },
+                            ]),
+                      ],
+                    },
+                  }}
+                >
+                  {row.casesS2}
+                </ExploreLink>
+              ),
+            percentS2: `${row.percentS2}%`,
+          }))}
+        />
+        <div style={{ textAlign: 'right' }}>
+          {[buckets1.length, buckets2.length].every(l => l === 2) && (
+            <Pvalue
+              data={[
+                buckets1.map(x => x.doc_count),
+                buckets2.map(x => x.doc_count),
+              ]}
+            />
+          )}
+        </div>
       </div>
-    </div>
-  ),
+    );
+  },
 );

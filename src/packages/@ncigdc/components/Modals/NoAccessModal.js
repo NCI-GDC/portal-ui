976ad4e03ab22245c1c9b6d28dc1454a633a8db2
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-09-25 16:43:09 -0400

    🐛 [PRTL-1683] close access error on login

--- src/packages/@ncigdc/components/Modals/NoAccessModal.js
+++ src/packages/@ncigdc/components/Modals/NoAccessModal.js
@@ -1,29 +1,39 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
+
 import BaseModal from '@ncigdc/components/Modals/BaseModal';
 import LoginButton from '@ncigdc/components/LoginButton';
+import { setModal } from '@ncigdc/dux/modal';
 
-const LoginModal = (
-  {
-    message = "You don't have access",
-    primaryButton,
-    closeText = 'Close',
-  }: {
-    message?: string,
-    primaryButton?: any,
-    closeText?: string,
-  } = {},
-) => (
-  <BaseModal
-    title="Access Error"
-    extraButtons={primaryButton}
-    closeText={closeText}
-  >
-    {message}
-    <p>
-      Please <LoginButton />
-    </p>
-  </BaseModal>
-);
+class LoginModal extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.user && !this.props.user) {
+      nextProps.dispatch(setModal(null));
+    }
+  }
+  render() {
+    const {
+      message = "You don't have access",
+      primaryButton,
+      closeText = 'Close',
+    } = this.props;
 
-export default LoginModal;
+    return (
+      <BaseModal
+        title="Access Error"
+        extraButtons={primaryButton}
+        closeText={closeText}
+      >
+        {message}
+        <p>
+          Please <LoginButton />
+        </p>
+      </BaseModal>
+    );
+  }
+}
+
+export default connect(state => ({
+  user: state.auth.user,
+}))(LoginModal);

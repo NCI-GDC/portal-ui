702e01619b5cd19c3f610bea89513df9ecb55fe6
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-09-28 12:52:36 -0400

    ✨ [PRTL-1668] use simple project pie chart

--- src/packages/@ncigdc/components/Charts/PieChart.js
+++ src/packages/@ncigdc/components/Charts/PieChart.js
@@ -25,83 +25,93 @@ const getNestedValue = (item, path) => {
 const PieChart = compose(
   withTooltip,
   withTheme,
-)(({ data, path = 'file_count', height = 160, width = 160, setTooltip }) => {
-  const color = d3.scaleOrdinal(d3.schemeCategory20);
-  const outerRadius = height / 2 + 10;
+)(
+  ({
+    data,
+    path = 'file_count',
+    height = 160,
+    width = 160,
+    setTooltip,
+    marginTop = 0,
+  }) => {
+    const color = d3.scaleOrdinal(d3.schemeCategory20);
+    const outerRadius = height / 2 + 10;
 
-  const node = ReactFauxDOM.createElement('div');
-  node.style.setProperty('display', 'flex');
-  node.style.setProperty('justify-content', 'center');
-  node.setAttribute('class', 'test-pie-chart');
+    const node = ReactFauxDOM.createElement('div');
+    node.style.setProperty('margin-top', `${marginTop}px`);
+    node.style.setProperty('display', 'flex');
+    node.style.setProperty('justify-content', 'center');
+    node.setAttribute('class', 'test-pie-chart');
 
-  const pie = d3
-    .pie()
-    .value(d => getNestedValue(d, path.split('.')))
-    .padAngle(HALF_DEGREE_IN_RAD);
+    const pie = d3
+      .pie()
+      .value(d => getNestedValue(d, path.split('.')))
+      .padAngle(HALF_DEGREE_IN_RAD);
 
-  const arc = d3.arc().padRadius(outerRadius).innerRadius(0);
+    const arc = d3.arc().padRadius(outerRadius).innerRadius(0);
 
-  const svg = d3
-    .select(node)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .append('g')
-    .attr('transform', `translate(${width / 2}, ${height / 2})`);
+    const svg = d3
+      .select(node)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-  const g = svg
-    .selectAll('.arc')
-    .data(pie(data))
-    .enter()
-    .append('g')
-    .each(d => {
-      d.outerRadius = outerRadius - 20;
-    })
-    .attr('class', 'arc');
+    const g = svg
+      .selectAll('.arc')
+      .data(pie(data))
+      .enter()
+      .append('g')
+      .each(d => {
+        d.outerRadius = outerRadius - 20;
+      })
+      .attr('class', 'arc');
 
-  const gHover = svg
-    .selectAll('.arc-hover')
-    .data(pie(data))
-    .enter()
-    .append('g')
-    .each(d => {
-      d.outerRadius = outerRadius - 15;
-    })
-    .attr('class', 'arc-hover');
+    const gHover = svg
+      .selectAll('.arc-hover')
+      .data(pie(data))
+      .enter()
+      .append('g')
+      .each(d => {
+        d.outerRadius = outerRadius - 15;
+      })
+      .attr('class', 'arc-hover');
 
-  const gPath = g.append('path');
-  const gHoverPath = gHover.append('path');
+    const gPath = g.append('path');
+    const gHoverPath = gHover.append('path');
 
-  gPath.attr('d', arc).style('fill', (d, i) => color(i));
+    gPath.attr('d', arc).style('fill', (d, i) => color(i));
 
-  const fillHover = gHoverPath
-    .attr('d', arc)
-    .style('fill', (d, i) => color(i))
-    .style('opacity', 0);
+    const fillHover = gHoverPath
+      .attr('d', arc)
+      .style('fill', (d, i) => color(i))
+      .style('opacity', 0);
 
-  fillHover
-    .attr('class', d => `pointer arc-hover-${_.snakeCase(d.data.id)}`)
-    .on('mouseenter', d => {
-      document.querySelector(
-        `.arc-hover-${_.snakeCase(d.data.id)}`,
-      ).style.opacity = 0.5;
-      setTooltip(d.data.tooltip);
-    })
-    .on('mouseleave', d => {
-      document.querySelector(
-        `.arc-hover-${_.snakeCase(d.data.id)}`,
-      ).style.opacity = 0;
-      setTooltip();
-    })
-    .on('mousedown', d => {
-      if (d.data.clickHandler) {
-        d.data.clickHandler(d);
+    fillHover
+      .attr('class', d => `pointer arc-hover-${_.snakeCase(d.data.id)}`)
+      .on('mouseenter', d => {
+        document.querySelector(
+          `.arc-hover-${_.snakeCase(d.data.id)}`,
+        ).style.opacity = 0.5;
+        setTooltip(d.data.tooltip);
+      })
+      .on('mouseleave', d => {
+        document.querySelector(
+          `.arc-hover-${_.snakeCase(d.data.id)}`,
+        ).style.opacity = 0;
         setTooltip();
-      }
-    });
+      })
+      .on('mousedown', d => {
+        if (d.data.clickHandler) {
+          d.data.clickHandler(d);
+          setTooltip();
+        }
+      });
 
-  return node.toReact();
-});
+    return node.toReact();
+  },
+);
 
 PieChart.propTypes = {
   data: PropTypes.array,

422fa6d3f3fa900c5381c4502921f8541d86f3ce
Alex Wilmer <djazium@gmail.com>
2017-10-19 16:11:10 -0400

    fuss with google login

--- src/packages/@ncigdc/routes/Login.js
+++ src/packages/@ncigdc/routes/Login.js
@@ -1,20 +1,33 @@
 import React from 'react';
 
-export default () => (
-  <div
-    style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      backgroundColor: 'white',
-      width: '100vw',
-      height: '100vh',
-      zIndex: 1000,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <h1>AWG Login Screen</h1>
-  </div>
-);
+export default class extends React.Component {
+  state = { loaded: false };
+  componentDidMount() {
+    window.gapi.signin2.render('g-signin2', {
+      onsuccess: e => {
+        console.log(123, e);
+      },
+    });
+  }
+  render() {
+    return (
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          backgroundColor: 'white',
+          width: '100vw',
+          height: '100vh',
+          zIndex: 1000,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1>AWG Login Screen</h1>
+        <div id="g-signin2" />
+      </div>
+    );
+  }
+}

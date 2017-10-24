1a75ec0302153cdfc6e197056b9e3f021fbc2575
Alex Wilmer <djazium@gmail.com>
2017-10-24 15:52:38 -0400

    use get for case symbol

--- src/packages/@ncigdc/modern_components/CaseSymbol/CaseSymbol.js
+++ src/packages/@ncigdc/modern_components/CaseSymbol/CaseSymbol.js
@@ -1,12 +1,12 @@
 // @flow
 
 import React from 'react';
-import { head } from 'lodash';
+import { head, get } from 'lodash';
 import { compose, branch, renderComponent, mapProps } from 'recompose';
 
 export default compose(
   branch(
-    ({ viewer }) => !viewer.repository.cases.hits.edges[0],
+    ({ viewer }) => !get(viewer, 'viewer.repository.cases.hits.edges[0]'),
     renderComponent(() => <div>No case found.</div>),
   ),
   mapProps(p => ({

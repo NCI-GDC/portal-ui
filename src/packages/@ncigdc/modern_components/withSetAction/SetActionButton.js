409c4577998cbf4c51ba24ff14cfa206ebb4d9d7
Alex Wilmer <djazium@gmail.com>
2017-10-31 13:50:28 -0400

    ✨ [PRTL-1852] force btn click on prop change

--- src/packages/@ncigdc/modern_components/withSetAction/SetActionButton.js
+++ src/packages/@ncigdc/modern_components/withSetAction/SetActionButton.js
@@ -3,26 +3,38 @@ import Button from '@ncigdc/uikit/Button';
 import Overlay from '@ncigdc/uikit/Overlay';
 import Spinner from '@ncigdc/uikit/Loaders/Material';
 
-export default ({
-  style,
-  children,
-  disabled,
-  createSet,
-  Component = Button,
-  ...props
-}) => {
-  return (
-    <span>
-      <Overlay show={props.isCreating}>
-        <Spinner />
-      </Overlay>
-      <Component
-        disabled={disabled}
-        style={style}
-        onClick={() => createSet(props)}
-      >
-        {children}
-      </Component>
-    </span>
-  );
-};
+export default class extends React.Component {
+  clicked = false;
+  componentWillReceiveProps(next) {
+    if (next.forceClick && !this.clicked) {
+      this.clicked = true;
+      this.props.createSet(this.props);
+    }
+  }
+  render() {
+    const {
+      style,
+      children,
+      disabled,
+      createSet,
+      Component = Button,
+      id,
+      ...props
+    } = this.props;
+
+    return (
+      <span>
+        <Overlay show={props.isCreating}>
+          <Spinner />
+        </Overlay>
+        <Component
+          disabled={disabled}
+          style={style}
+          onClick={() => createSet(props)}
+        >
+          {children}
+        </Component>
+      </span>
+    );
+  }
+}

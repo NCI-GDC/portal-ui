7e0b9d2db26e4389a01c228986f3ec9f920e27a4
Alex Wilmer <djazium@gmail.com>
2017-11-06 14:46:04 -0500

    🐛 [PRTL-1860] weird unicode minus sign errored on server (#2200)

--- src/packages/@ncigdc/components/Charts/Venn/buildOps.js
+++ src/packages/@ncigdc/components/Charts/Venn/buildOps.js
@@ -23,35 +23,35 @@ export default ({ setIds, type }) => {
   return setIds.length === 2
     ? [
         mapOp('( S1 ∩ S2 )', { in: setIds }),
-        mapOp('( S1 ) − ( S2 )', {
+        mapOp('( S1 ) - ( S2 )', {
           in: setIds.slice(0, 1),
           exclude: setIds.slice(1),
         }),
-        mapOp('( S2 ) − ( S1 )', { in: [setIds[1]], exclude: [setIds[0]] }),
+        mapOp('( S2 ) - ( S1 )', { in: [setIds[1]], exclude: [setIds[0]] }),
       ]
     : [
         mapOp('( S1 ∩ S2 ∩ S3 )', { in: setIds }),
-        mapOp('( S1 ∩ S2 ) − ( S3 )', {
+        mapOp('( S1 ∩ S2 ) - ( S3 )', {
           in: setIds.slice(0, 2),
           exclude: [setIds[2]],
         }),
-        mapOp('( S2 ∩ S3 ) − ( S1 )', {
+        mapOp('( S2 ∩ S3 ) - ( S1 )', {
           in: [setIds[1], setIds[2]],
           exclude: [setIds[0]],
         }),
-        mapOp('( S1 ∩ S3 ) − ( S2 )', {
+        mapOp('( S1 ∩ S3 ) - ( S2 )', {
           in: [setIds[0], setIds[2]],
           exclude: [setIds[1]],
         }),
-        mapOp('( S1 ) − ( S2 ∪ S3 )', {
+        mapOp('( S1 ) - ( S2 ∪ S3 )', {
           in: [setIds[0]],
           exclude: [setIds[1], setIds[2]],
         }),
-        mapOp('( S2 ) − ( S1 ∪ S3 )', {
+        mapOp('( S2 ) - ( S1 ∪ S3 )', {
           in: [setIds[1]],
           exclude: [setIds[0], setIds[2]],
         }),
-        mapOp('( S3 ) − ( S1 ∪ S2 )', {
+        mapOp('( S3 ) - ( S1 ∪ S2 )', {
           in: [setIds[2]],
           exclude: [setIds[0], setIds[1]],
         }),

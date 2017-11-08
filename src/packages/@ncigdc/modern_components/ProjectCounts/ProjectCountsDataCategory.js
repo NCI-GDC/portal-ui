ec4e39c2bab8a89cb4303d1b63890ca0b1fdc208
Aishmit Khurana <aishmitsingh@gmail.com>
2017-11-08 15:44:56 -0500

    formatted file ProjectCountsDataCategory.js

--- src/packages/@ncigdc/modern_components/ProjectCounts/ProjectCountsDataCategory.js
+++ src/packages/@ncigdc/modern_components/ProjectCounts/ProjectCountsDataCategory.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { scaleOrdinal, schemeCategory20 } from "d3";
-import { compose, branch, renderComponent } from "recompose";
-import SummaryCard from "@ncigdc/components/SummaryCard";
-import { makeFilter, mergeQuery } from "@ncigdc/utils/filters";
-import Link from "@ncigdc/components/Links/Link";
-import removeEmptyKeys from "@ncigdc/utils/removeEmptyKeys";
-import { stringifyJSONParam } from "@ncigdc/utils/uri";
-import { DATA_CATEGORIES } from "@ncigdc/utils/constants";
+import React from 'react';
+import { scaleOrdinal, schemeCategory20 } from 'd3';
+import { compose, branch, renderComponent } from 'recompose';
+import SummaryCard from '@ncigdc/components/SummaryCard';
+import { makeFilter, mergeQuery } from '@ncigdc/utils/filters';
+import Link from '@ncigdc/components/Links/Link';
+import removeEmptyKeys from '@ncigdc/utils/removeEmptyKeys';
+import { stringifyJSONParam } from '@ncigdc/utils/uri';
+import { DATA_CATEGORIES } from '@ncigdc/utils/constants';
 import SparkMeterWithTooltip from '@ncigdc/components/SparkMeterWithTooltip';
 import SampleSize from '@ncigdc/components/SampleSize';
 
@@ -14,18 +14,18 @@ const colors20 = scaleOrdinal(schemeCategory20);
 
 const styles = {
   coloredSquare: {
-    display: "inline-block",
+    display: 'inline-block',
     width: 10,
     height: 10,
-    marginRight: 5
-  }
+    marginRight: 5,
+  },
 };
 
 export default compose(
   branch(
     ({ viewer }) => !viewer.projects.hits.edges[0],
-    renderComponent(() => <div>No project found.</div>)
-  )
+    renderComponent(() => <div>No project found.</div>),
+  ),
 )(({ viewer: { projects: { hits: { edges } } }, query, push }) => {
   const project = edges[0].node;
   const totalFiles = project.summary.file_count;
@@ -33,7 +33,7 @@ export default compose(
 
   const dataCategories = Object.keys(DATA_CATEGORIES).reduce((acc, key) => {
     const type = project.summary.data_categories.find(
-      item => item.data_category === DATA_CATEGORIES[key].full
+      item => item.data_category === DATA_CATEGORIES[key].full,
     );
     return acc.concat(
       type
@@ -47,16 +47,16 @@ export default compose(
             ),
             case_count_meter: (
               <SparkMeterWithTooltip
-              part={type.case_count}
-              whole={totalCases}
-            />
-            )
+                part={type.case_count}
+                whole={totalCases}
+              />
+            ),
           }
         : {
             data_category: DATA_CATEGORIES[key].full,
             file_count: 0,
-            case_count: 0
-          }
+            case_count: 0,
+          },
     );
   }, []);
   return (
@@ -66,10 +66,10 @@ export default compose(
       data={dataCategories.map((item, i) => {
         const filters = makeFilter([
           {
-            field: "cases.project.project_id",
-            value: [project.project_id]
+            field: 'cases.project.project_id',
+            value: [project.project_id],
           },
-          { field: "files.data_category", value: [item.data_category] }
+          { field: 'files.data_category', value: [item.data_category] },
         ]);
 
         return {
@@ -80,7 +80,7 @@ export default compose(
               <div
                 style={{
                   ...styles.coloredSquare,
-                  backgroundColor: colors20(i)
+                  backgroundColor: colors20(i),
                 }}
               />
               {item.data_category}
@@ -93,14 +93,14 @@ export default compose(
                 pathname="/repository"
                 query={{
                   filters,
-                  facetTab: "cases",
-                  searchTableTab: "cases"
+                  facetTab: 'cases',
+                  searchTableTab: 'cases',
                 }}
               >
                 {item.case_count.toLocaleString()}
               </Link>
             ) : (
-              "0"
+              '0'
             ),
           file_count: item.file_count ? (
             <Link
@@ -108,64 +108,64 @@ export default compose(
               pathname="/repository"
               query={{
                 filters,
-                facetTab: "files",
-                searchTableTab: "files"
+                facetTab: 'files',
+                searchTableTab: 'files',
               }}
             >
               {item.file_count.toLocaleString()}
             </Link>
           ) : (
-            "0"
+            '0'
           ),
           file_count_value: item.file_count,
           tooltip: (
             <span>
               <b>{item.data_category}</b>
               <br />
-              {item.file_count} file{item.file_count > 1 ? "s" : ""}
+              {item.file_count} file{item.file_count > 1 ? 's' : ''}
             </span>
           ),
           clickHandler: () => {
             const newQuery = mergeQuery(
               {
                 filters,
-                facetTab: "files",
-                searchTableTab: "files"
+                facetTab: 'files',
+                searchTableTab: 'files',
               },
               query,
-              "replace"
+              'replace',
             );
             const q = removeEmptyKeys({
               ...newQuery,
-              filters: newQuery.filters && stringifyJSONParam(newQuery.filters)
+              filters: newQuery.filters && stringifyJSONParam(newQuery.filters),
             });
-            push({ pathname: "/repository", query: q });
-          }
+            push({ pathname: '/repository', query: q });
+          },
         };
       })}
       footer={`${dataCategories.length} Data Categories`}
       path="file_count_value"
       headings={[
-        { key: "data_category", title: "Data Category", color: true },
+        { key: 'data_category', title: 'Data Category', color: true },
         {
-          key: "case_count",
-          title: "Cases",
-          style: { textAlign: "right" }
+          key: 'case_count',
+          title: 'Cases',
+          style: { textAlign: 'right' },
         },
         {
-          key: "case_count_meter",
+          key: 'case_count_meter',
           title: (
             <Link
               pathname="/repository"
               query={{
                 filters: makeFilter([
                   {
-                    field: "project.project_id",
-                    value: project.project_id
-                  }
+                    field: 'project.project_id',
+                    value: project.project_id,
+                  },
                 ]),
-                facetTab: "files",
-                searchTableTab: "files"
+                facetTab: 'files',
+                searchTableTab: 'files',
               }}
               title="Browse files"
             >
@@ -174,29 +174,29 @@ export default compose(
           ),
           thStyle: {
             width: 1,
-            textAlign: "center"
+            textAlign: 'center',
           },
-          style: { textAlign: "left" }
+          style: { textAlign: 'left' },
         },
         {
-          key: "file_count",
-          title: "Files",
-          style: { textAlign: "right" }
+          key: 'file_count',
+          title: 'Files',
+          style: { textAlign: 'right' },
         },
         {
-          key: "file_count_meter",
+          key: 'file_count_meter',
           title: (
             <Link
               pathname="/repository"
               query={{
                 filters: makeFilter([
                   {
-                    field: "project.project_id",
-                    value: project.project_id
-                  }
+                    field: 'project.project_id',
+                    value: project.project_id,
+                  },
                 ]),
-                facetTab: "files",
-                searchTableTab: "files"
+                facetTab: 'files',
+                searchTableTab: 'files',
               }}
               title="Browse files"
             >
@@ -205,10 +205,10 @@ export default compose(
           ),
           thStyle: {
             width: 1,
-            textAlign: "center"
+            textAlign: 'center',
           },
-          style: { textAlign: "left" }
-        }
+          style: { textAlign: 'left' },
+        },
       ]}
     />
   );

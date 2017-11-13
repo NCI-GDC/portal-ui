bdd545cd208faf4df697152eb336e71bd5266306
Alex Wilmer <djazium@gmail.com>
2017-11-13 18:37:36 -0500

    ✨ pass query name in apollo request

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { REACT_APP_GRAPHQL } from '@ncigdc/utils/constants';
 
@@ -14,8 +15,15 @@ global.trace = require('@ncigdc/utils/trace').default;
 
 const Root = require('./Root').default;
 
+const link = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    uri: REACT_APP_GRAPHQL + `/${operation.operationName}`,
+  });
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: REACT_APP_GRAPHQL }),
+  link: link.concat(createHttpLink()),
   cache: new InMemoryCache(),
 });
 

5c15bb26586f77e5fb490943fdb6a1c12d97449b
aishmitk <aishmitsingh@gmail.com>
2017-11-16 11:20:14 -0500

    ✨[PRTL-1842] More obvious that there is no data for one or both sets in survival plot (#2217)
    * ✨ [PTRL-1842] More obvious that survival plot does not contain survival data
    
    * capitalized venn
    
    * fixed bug where after loading the survival plot would disappear if there was no data
    
    * fixed loading screen for survival analysis

--- src/packages/@ncigdc/modern_components/CohortComparison/Survival.js
+++ src/packages/@ncigdc/modern_components/CohortComparison/Survival.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { compose, branch, renderNothing } from 'recompose';
+import { compose } from 'recompose';
 import SurvivalPlotWrapper from '@ncigdc/components/SurvivalPlotWrapper';
 import Table, { Tr, Td, Th } from '@ncigdc/uikit/Table';
 import { Row } from '@ncigdc/uikit/Flex';
@@ -62,14 +62,6 @@ export const makeSurvivalCurveFilter = (setId, otherSetId) => ({
 export default compose(
   withTheme,
   withRouter,
-  branch(
-    ({ survivalData, loading }) =>
-      !loading &&
-      (!survivalData ||
-        !survivalData.rawData ||
-        !survivalData.rawData.results.some(Boolean)),
-    renderNothing,
-  ),
 )(
   ({
     loading,
@@ -124,93 +116,105 @@ export default compose(
       <Row>
         <h2>Survival Analysis</h2>
       </Row>
-      <div>
-        <SurvivalPlotWrapper
-          survivalPlotloading={loading}
-          {...survivalData}
-          palette={palette}
-          height={240}
-        />
-        {survivalData.rawData && (
-          <Table
-            headings={[
-              <Th key="1">
-                <Tooltip
-                  style={tableToolTipHint()}
-                  Component={
-                    <span>
-                      Criteria to include Case from your sets in the survival
-                      analysis:<br />
-                      - Case does not overlap between your selected sets<br />
-                      - Case has complete data for the purpose of the analysis
-                      (event and time-to-event)
-                    </span>
-                  }
-                >
-                  Cases included in Analysis
-                </Tooltip>
-              </Th>,
-              <Th key="2" style={{ textAlign: 'right' }}>
-                # Cases <Alias i={1} />
-              </Th>,
-              <Th key="3" style={{ textAlign: 'right' }}>
-                %
-              </Th>,
-              <Th key="4" style={{ textAlign: 'right' }}>
-                # Cases <Alias i={2} />
-              </Th>,
-              <Th key="5" style={{ textAlign: 'right' }}>
-                %
-              </Th>,
-            ]}
-            body={
-              <tbody>
-                <Tr index={0}>
-                  <Td width={250}>Overall Survival Analysis</Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {get(survivalData, 'rawData.results[0].donors.length', 0) >
-                    0 ? (
-                      <CaseSetButton
-                        setId={set1id}
-                        otherSetId={set2id}
-                        count={survivalData.rawData.results[0].donors.length.toLocaleString()}
-                      />
-                    ) : (
-                      0
-                    )}
-                  </Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {survivalData.rawData.results[0] &&
-                      (survivalData.rawData.results[0].donors.length /
-                        result1.hits.total *
-                        100
-                      ).toFixed(0)}%
-                  </Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {get(survivalData, 'rawData.results[1].donors.length', 0) >
-                    0 ? (
-                      <CaseSetButton
-                        setId={set2id}
-                        otherSetId={set1id}
-                        count={survivalData.rawData.results[1].donors.length.toLocaleString()}
-                      />
-                    ) : (
-                      0
-                    )}
-                  </Td>
-                  <Td style={{ textAlign: 'right' }}>
-                    {survivalData.rawData.results[1] &&
-                      (survivalData.rawData.results[1].donors.length /
-                        result2.hits.total *
-                        100
-                      ).toFixed(0)}%
-                  </Td>
-                </Tr>
-              </tbody>
-            }
+      {loading ||
+      get(survivalData, 'rawData.results[0].donors.length', 0) ||
+      get(survivalData, 'rawData.results[1].donors.length', 0) ? (
+        <div>
+          <SurvivalPlotWrapper
+            survivalPlotloading={loading}
+            {...survivalData}
+            palette={palette}
+            height={240}
           />
-        )}
-      </div>
+          {survivalData.rawData && (
+            <Table
+              headings={[
+                <Th key="1">
+                  <Tooltip
+                    style={tableToolTipHint()}
+                    Component={
+                      <span>
+                        Criteria to include Case from your sets in the survival
+                        analysis:<br />
+                        - Case does not overlap between your selected sets<br />
+                        - Case has complete data for the purpose of the analysis
+                        (event and time-to-event)
+                      </span>
+                    }
+                  >
+                    Cases included in Analysis
+                  </Tooltip>
+                </Th>,
+                <Th key="2" style={{ textAlign: 'right' }}>
+                  # Cases <Alias i={1} />
+                </Th>,
+                <Th key="3" style={{ textAlign: 'right' }}>
+                  %
+                </Th>,
+                <Th key="4" style={{ textAlign: 'right' }}>
+                  # Cases <Alias i={2} />
+                </Th>,
+                <Th key="5" style={{ textAlign: 'right' }}>
+                  %
+                </Th>,
+              ]}
+              body={
+                <tbody>
+                  <Tr index={0}>
+                    <Td width={250}>Overall Survival Analysis</Td>
+                    <Td style={{ textAlign: 'right' }}>
+                      {get(
+                        survivalData,
+                        'rawData.results[0].donors.length',
+                        0,
+                      ) > 0 ? (
+                        <CaseSetButton
+                          setId={set1id}
+                          otherSetId={set2id}
+                          count={survivalData.rawData.results[0].donors.length.toLocaleString()}
+                        />
+                      ) : (
+                        'No Survival Data'
+                      )}
+                    </Td>
+                    <Td style={{ textAlign: 'right' }}>
+                      {survivalData.rawData.results[0] &&
+                        (survivalData.rawData.results[0].donors.length /
+                          result1.hits.total *
+                          100
+                        ).toFixed(0)}%
+                    </Td>
+                    <Td style={{ textAlign: 'right' }}>
+                      {get(
+                        survivalData,
+                        'rawData.results[1].donors.length',
+                        0,
+                      ) > 0 ? (
+                        <CaseSetButton
+                          setId={set2id}
+                          otherSetId={set1id}
+                          count={survivalData.rawData.results[1].donors.length.toLocaleString()}
+                        />
+                      ) : (
+                        'No Survival Data'
+                      )}
+                    </Td>
+                    <Td style={{ textAlign: 'right' }}>
+                      {survivalData.rawData.results[1] &&
+                        (survivalData.rawData.results[1].donors.length /
+                          result2.hits.total *
+                          100
+                        ).toFixed(0)}%
+                    </Td>
+                  </Tr>
+                </tbody>
+              }
+            />
+          )}
+        </div>
+      ) : (
+        <div>No Survival data available for this Cohort Comparison</div>
+      )}
     </div>
   ),
 );

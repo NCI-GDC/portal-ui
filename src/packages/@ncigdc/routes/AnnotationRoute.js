7010a25e0ae8970ece919f46a3c11ce4bac185e5
Ann Catton <anncatton@gmail.com>
2018-04-11 13:19:48 -0400

    Export auth route (#2308)
    * Export AuthRoute separately. Hide Case, Project and Annotation routes behind auth.

--- src/packages/@ncigdc/routes/AnnotationRoute.js
+++ src/packages/@ncigdc/routes/AnnotationRoute.js
@@ -1,11 +1,12 @@
 // @flow
 
 import React from 'react';
-import { Route } from 'react-router-dom';
 import AnnotationSummary from '@ncigdc/modern_components/AnnotationSummary';
+import AuthRoute from '@ncigdc/routes/AuthRoute';
 
 export default (
-  <Route
+  <AuthRoute
+    exact
     path="/annotations/:id"
     component={({ match, annotationId = match.params.id }) => (
       <AnnotationSummary annotationId={annotationId} />

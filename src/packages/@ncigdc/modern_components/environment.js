3f1d230e1e6879a2daba2e8abee022ba4f882171
Ann Catton <anncatton@gmail.com>
2018-05-17 11:34:33 -0400

    and logging and check for user missing all projects in env query fetch

--- src/packages/@ncigdc/modern_components/environment.js
+++ src/packages/@ncigdc/modern_components/environment.js
@@ -75,8 +75,18 @@ function fetchQuery(operation, variables, cacheConfig) {
       let tries = 20;
       let id = setInterval(() => {
         let { user } = window.store.getState().auth;
-
+        console.log('env json: ', json);
+        console.log('env user: ', user);
         if (user) {
+          if (
+            !json.fence_projects.length &&
+            !json.nih_projects.length &&
+            !json.intersection.length
+          ) {
+            clear();
+            window.location.href = '/login?error=timeout';
+            return;
+          }
           if (!json.fence_projects.length) {
             clear();
             window.location.href = '/login?error=no_fence_projects';

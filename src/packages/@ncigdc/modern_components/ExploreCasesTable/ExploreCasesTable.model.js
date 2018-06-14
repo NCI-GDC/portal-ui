dd1df910a3008ef74d5e0bc268bed86a9ac08ed3
Christine <christine.tc.yu@gmail.com>
2018-06-14 14:32:41 -0400

    :bug: fix explore tsv slide count (#2322)
    * :bug: fix explore tsv slide count

--- src/packages/@ncigdc/modern_components/ExploreCasesTable/ExploreCasesTable.model.js
+++ src/packages/@ncigdc/modern_components/ExploreCasesTable/ExploreCasesTable.model.js
@@ -263,7 +263,7 @@ const casesTableModel = [
           <Td style={{ textAlign: 'center' }}>
             {[
               <ForTsvExport key={`tsv-export-${node.case_id}`}>
-                {!!slideCount ? slideCount.file_count : 0}
+                {slideCount}
               </ForTsvExport>,
               !!slideCount ? (
                 <Tooltip

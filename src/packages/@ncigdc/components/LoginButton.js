2028d560913d187be2033a63d12d090087e3257b
Ann Catton <anncatton@gmail.com>
2018-06-22 14:30:52 -0400

    cleanup

--- src/packages/@ncigdc/components/LoginButton.js
+++ src/packages/@ncigdc/components/LoginButton.js
@@ -25,6 +25,7 @@ const openAuthWindow = ({
   if (navigator.cookieEnabled) {
     const win = open(winUrl, 'Auth', winStyle);
     window.loginPopup = win;
+
     const interval = setInterval(() => {
       try {
         // Because the login window redirects to a different domain, checking

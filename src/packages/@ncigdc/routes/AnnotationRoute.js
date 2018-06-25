6ec3de8f8b93ede57682ced5af805dc9ec177422
Ann Catton <anncatton@gmail.com>
2018-06-25 13:40:07 -0400

    test if new route component breaks annotation page

--- src/packages/@ncigdc/routes/AnnotationRoute.js
+++ src/packages/@ncigdc/routes/AnnotationRoute.js
@@ -1,11 +1,13 @@
 // @flow
 
 import React from 'react';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
+import AuthRoute from '@ncigdc/routes/AuthRoute';
 import AnnotationSummary from '@ncigdc/modern_components/AnnotationSummary';
 
 export default (
-  <Route
+  <AuthRoute
+    exact
     path="/annotations/:id"
     component={({ match, annotationId = match.params.id }) => (
       <AnnotationSummary annotationId={annotationId} />
--- src/packages/@ncigdc/routes/AwgRoutes.js
+++ src/packages/@ncigdc/routes/AwgRoutes.js
@@ -5,6 +5,7 @@ import NotFound from '@ncigdc/components/NotFound';
 import LoadableWithLoading from '@ncigdc/components/LoadableWithLoading';
 import FileRoute from '@ncigdc/routes/FileRoute';
 import AuthRoute from '@ncigdc/routes/AuthRoute';
+import AnnotationRoute from '@ncigdc/routes/AnnotationRoute';
 
 const CartRoute = LoadableWithLoading({
   loader: () => import('@ncigdc/routes/CartRoute'),
@@ -30,9 +31,9 @@ const CaseRoute = LoadableWithLoading({
   loader: () => import('@ncigdc/routes/CaseRoute/CaseRoute'),
 });
 
-const AnnotationRoute = LoadableWithLoading({
-  loader: () => import('@ncigdc/routes/AnnotationRoute'),
-});
+// const AnnotationRoute = LoadableWithLoading({
+//   loader: () => import('@ncigdc/routes/AnnotationRoute'),
+// });
 
 export default () => (
   <span>
@@ -50,7 +51,7 @@ export default () => (
       <AuthRoute path="/projects/:id" component={ProjectRoute} />
       <AuthRoute path="/files/:id" component={FileRoute} />
       <AuthRoute path="/cases/:id" component={CaseRoute} />
-      <AuthRoute path="/annotations/:id" component={AnnotationRoute} />
+      {AnnotationRoute}
       <Route component={NotFound} />
     </Switch>
   </span>

488c2ca2141a2260df77c3d7fdbf3aa6c7b7d4e6
Ann Catton <anncatton@gmail.com>
2018-11-01 12:19:23 -0400

    use replaceFilters

--- src/packages/@ncigdc/modern_components/CancerDistributionTable/CancerDistributionTable.relay.js
+++ src/packages/@ncigdc/modern_components/CancerDistributionTable/CancerDistributionTable.relay.js
@@ -3,11 +3,7 @@
 import React from 'react';
 import { graphql } from 'react-relay';
 import { compose, withPropsOnChange } from 'recompose';
-import {
-  makeFilter,
-  replaceFilters,
-  addInFilters,
-} from '@ncigdc/utils/filters';
+import { makeFilter, replaceFilters } from '@ncigdc/utils/filters';
 import Query from '@ncigdc/modern_components/Query';
 
 export default (Component: ReactClass<*>) =>
@@ -45,7 +41,7 @@ export default (Component: ReactClass<*>) =>
             ]),
             filters,
           ),
-          caseAggsFilter: addInFilters(
+          caseAggsFilter: replaceFilters(
             {
               op: 'and',
               content: [

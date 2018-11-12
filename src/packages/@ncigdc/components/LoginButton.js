ae4dc79e979cb0653acbd0ad1c60315dc834c75d
Ann Catton <anncatton@gmail.com>
2018-11-12 14:01:40 -0500

    remove login loop

--- src/packages/@ncigdc/components/LoginButton.js
+++ src/packages/@ncigdc/components/LoginButton.js
@@ -6,7 +6,7 @@ import LoginIcon from 'react-icons/lib/fa/sign-in';
 import { fetchUser } from '@ncigdc/dux/auth';
 import LocationSubscriber from '@ncigdc/components/LocationSubscriber';
 import styled from '@ncigdc/theme/styled';
-import { AUTH, FENCE } from '@ncigdc/utils/constants';
+import { AUTH, FENCE, AWG } from '@ncigdc/utils/constants';
 
 /*----------------------------------------------------------------------------*/
 
@@ -18,16 +18,14 @@ const openAuthWindow = ({
   push,
   user,
   pollInterval = 600,
-  winUrl = `${AUTH}?next=${location.origin}`,
+  winUrl = AWG
+    ? `${FENCE}/login/fence?redirect=${location.origin}`
+    : `${AUTH}?next=${location.origin}`,
   winStyle = 'width=800, height=600',
 }) => {
   if (navigator.cookieEnabled) {
-    const win = open(winUrl, 'Auth', winStyle);
-    window.loginPopup = win;
-    console.log('location origin: ', location.origin);
-    console.log('is first window open? ', first);
-    console.log('win url: ', winUrl);
-    console.log('user? ', user);
+    const win = open(FENCE, 'Auth', 'width=800, height=600');
+
     const interval = setInterval(() => {
       try {
         // Because the login window redirects to a different domain, checking
@@ -35,44 +33,75 @@ const openAuthWindow = ({
         // #clearInterval from ever getting called in this block.
         // Must check this block (if the login window has been closed) first!
         if (win.closed) {
-          console.log('window closed? ', win.closed);
           clearInterval(interval);
         } else if (
           win.document.URL.includes(location.origin) &&
-          !win.document.URL.includes('auth')
+          !win.document.URL.includes(location.origin + '/auth')
         ) {
           win.close();
-          console.log('location is not auth: ', location.origin);
+
           setTimeout(() => {
             clearInterval(interval);
             setTimeout(() => {
-              // fetch authpublic user
-              if (first) {
-                first = false;
-                dispatch(fetchUser());
-                // login with fence
-                console.log('log in with fence');
-                openAuthWindow({
-                  pathname,
-                  dispatch,
-                  user,
-                  push,
-                  pollInterval: 500,
-                  winUrl: `${FENCE}/login/fence?redirect=${location.origin}`,
-                  winStyle:
-                    'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=50000, top=50000, width=1, height=1, visible=none',
-                });
-              } else {
-                console.log('redirecting to repository page');
-                push('/repository');
-              }
-            }, pollInterval);
-          }, pollInterval);
+              dispatch(fetchUser());
+            }, 1000);
+          }, 1000);
         }
       } catch (err) {
         console.log('Error while monitoring the Login window: ', err);
       }
     }, pollInterval);
+    // const win = open(winUrl, 'Auth', winStyle);
+    // window.loginPopup = win;
+    // console.log('location origin: ', location.origin);
+    // console.log('is first window open? ', first);
+    // console.log('win url: ', winUrl);
+    // console.log('user? ', user);
+    // const interval = setInterval(() => {
+    //   try {
+    //     // Because the login window redirects to a different domain, checking
+    //     // win.document in IE11 throws exceptions right away, which prevents
+    //     // #clearInterval from ever getting called in this block.
+    //     // Must check this block (if the login window has been closed) first!
+    //     if (win.closed) {
+    //       console.log('window closed? ', win.closed);
+    //       clearInterval(interval);
+    //     } else if (
+    //       win.document.URL.includes(location.origin) &&
+    //       !win.document.URL.includes('auth')
+    //     ) {
+    //       win.close();
+    //       console.log('location is not auth: ', location.origin);
+    //       setTimeout(() => {
+    //         clearInterval(interval);
+    //         setTimeout(() => {
+    //           // fetch authpublic user
+    //           if (first) {
+    //             first = false;
+    //             dispatch(fetchUser());
+    //             // login with fence
+    //             console.log('log in with fence');
+    //             openAuthWindow({
+    //               pathname,
+    //               dispatch,
+    //               user,
+    //               push,
+    //               pollInterval: 500,
+    //               winUrl: `${FENCE}/login/fence?redirect=${location.origin}`,
+    //               winStyle:
+    //                 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=50000, top=50000, width=1, height=1, visible=none',
+    //             });
+    //           } else {
+    //             console.log('redirecting to repository page');
+    //             push('/repository');
+    //           }
+    //         }, pollInterval);
+    //       }, pollInterval);
+    //     }
+    //   } catch (err) {
+    //     console.log('Error while monitoring the Login window: ', err);
+    //   }
+    // }, pollInterval);
   } else {
     // show cookie needs to be enabled message
   }

570da41a038ee0b2f899128d4e74ee6dd92f749b
Ann Catton <anncatton@gmail.com>
2018-11-20 12:49:03 -0500

    parse error

--- src/Root.js
+++ src/Root.js
@@ -107,19 +107,15 @@ Relay.injectNetworkLayer(
           } else {
             console.log('Something went wrong in Root', err);
             // not able to pass the response status from throw so need to exclude by error message
-            console.log('err.message: ', err.message);
-            console.log('to string: ', err.toString());
-            console.log(
-              'string includes: ',
-              err
-                .toString()
-                .includes(
-                  'Your token is invalid or expired. Please get a new token from GDC Data Portal.',
-                ),
-            );
-            console.log('parse err.message: ', JSON.parse(err.message));
-            console.log('parse to string: ', JSON.parse(err.toString()));
-            if (IS_AUTH_PORTAL && user) {
+            let errorMessage = err.message
+              ? JSON.parse(err.message).message
+              : null;
+            if (
+              IS_AUTH_PORTAL &&
+              user &&
+              errorMessage ===
+                'Your token is invalid or expired. Please get a new token from GDC Data Portal.'
+            ) {
               return redirectToLogin('timeout');
             }
           }

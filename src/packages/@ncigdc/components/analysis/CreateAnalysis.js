d7b68e397afd536f063d514e2dea4000e176ea96
Ann Catton <anncatton@gmail.com>
2019-01-25 15:10:28 -0500

    ✨ PRTL-2415 Add cdave to analysis, restyle

--- src/packages/@ncigdc/components/analysis/CreateAnalysis.js
+++ src/packages/@ncigdc/components/analysis/CreateAnalysis.js
@@ -6,6 +6,7 @@ import { addAnalysis } from '@ncigdc/dux/analysis';
 import withRouter from '@ncigdc/utils/withRouter';
 import Button from '@ncigdc/uikit/Button';
 import { Row } from '@ncigdc/uikit/Flex';
+import { zDepth1 } from '@ncigdc/theme/mixins';
 
 import availableAnalysis from './availableAnalysis';
 import SelectSet from './SelectSet';
@@ -53,33 +54,51 @@ const CreateAnalysis = ({ analysis, setAnalysis, dispatch, push }) => {
       }}
     />
   ) : (
-    <Row>
-      <div style={{ padding: '2rem 2.5rem', flex: 1 }}>
-        {availableAnalysis.map(analysis => {
-          return (
-            <Row key={analysis.type}>
-              <div style={{ margin: 20 }}>
-                <analysis.Icon />
-              </div>
-              <div>
-                <h1 style={{ fontSize: '2rem' }}>{analysis.label}</h1>
-                <div style={{ marginBottom: 10 }}>{analysis.description}</div>
-                <Row spacing={5}>
-                  <Button onClick={() => setAnalysis(analysis)}>Select</Button>
-                  <DemoButton
-                    demoData={analysis.demoData}
-                    type={analysis.type}
-                  />
-                </Row>
-              </div>
-            </Row>
-          );
-        })}
-      </div>
-      <div
+    <Row
+      style={{
+        padding: '2rem 2.5rem',
+        flexFlow: 'row wrap',
+        margin: '0 2rem',
+        justifyContent: 'space-between',
+      }}
+    >
+      {/* <div
+        style={{
+
+          flex: 1,
+          flexWrap: 'wrap',
+        }}
+      > */}
+      {availableAnalysis.map(analysis => {
+        return (
+          <Row
+            key={analysis.type}
+            style={{
+              ...zDepth1,
+              margin: '2rem',
+              padding: '2rem',
+              width: '45%',
+            }}
+          >
+            <div style={{ margin: 20 }}>
+              <analysis.Icon />
+            </div>
+            <div>
+              <h1 style={{ fontSize: '2rem' }}>{analysis.label}</h1>
+              <div style={{ marginBottom: 10 }}>{analysis.description}</div>
+              <Row spacing={5}>
+                <Button onClick={() => setAnalysis(analysis)}>Select</Button>
+                <DemoButton demoData={analysis.demoData} type={analysis.type} />
+              </Row>
+            </div>
+          </Row>
+        );
+      })}
+      {/* </div> */}
+      {/* <div
         // for a second column of analysis when the time comes
         style={{ flex: 1 }}
-      />
+      /> */}
     </Row>
   );
 };

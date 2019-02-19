fccd0091ff181f66c03db4a07b3ace336113fd8a
Ann Catton <anncatton@gmail.com>
2019-02-19 12:56:58 -0500

    demo button comment

--- src/packages/@ncigdc/components/analysis/CreateAnalysis.js
+++ src/packages/@ncigdc/components/analysis/CreateAnalysis.js
@@ -141,6 +141,7 @@ const CreateAnalysis = ({ analysis, setAnalysis, dispatch, push }) => {
               <div style={{ marginBottom: 10 }}>{analysis.description}</div>
               <Row spacing={5}>
                 <Button onClick={() => setAnalysis(analysis)}>Select</Button>
+                {/* disabling demo button until demo data is ready */}
                 <DemoButton demoData={analysis.demoData} type={analysis.type} disabled={analysis.type === 'clinical_data'} />
               </Row>
             </div>

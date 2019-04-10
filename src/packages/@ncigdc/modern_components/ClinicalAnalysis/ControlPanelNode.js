4b56d041939ed53c67b9f059fef0e1c572ed3a14
Sam Rich <samrichca@users.noreply.github.com>
2019-04-10 13:17:33 -0400

    Prtl 2494 facet toggles (#2503)
    * install react toggle
    
    * make facet title clickable
    
    * add pretty toggles
    
    * cleanup
    
    * make toggles smaller
    
    * remove facet toggle component

--- src/packages/@ncigdc/modern_components/ClinicalAnalysis/ControlPanelNode.js
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/ControlPanelNode.js
@@ -9,6 +9,8 @@ import {
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { singular } from 'pluralize';
+import Toggle from 'react-toggle';
+import './reactToggle.css';
 
 import { humanify } from '@ncigdc/utils/string';
 import { Row, Column } from '@ncigdc/uikit/Flex';
@@ -103,15 +105,16 @@ const ClinicalGrouping = compose(
               padding: '0 10px',
             }}
           >
-            {fields.length > MAX_FIELDS_LENGTH && showingMore && (
-              <Row>
-                <StyledToggleMoreLink
-                  onClick={() => setShowingMore(!showingMore)}
-                >
-                  {'Less...'}
-                </StyledToggleMoreLink>
-              </Row>
-            )}
+            {fields.length > MAX_FIELDS_LENGTH &&
+              showingMore && (
+                <Row>
+                  <StyledToggleMoreLink
+                    onClick={() => setShowingMore(!showingMore)}
+                  >
+                    {'Less...'}
+                  </StyledToggleMoreLink>
+                </Row>
+              )}
             {_.orderBy(fields, 'name', 'asc')
               .slice(0, showingMore ? Infinity : MAX_VISIBLE_FACETS)
               .map(field => ({
@@ -137,28 +140,52 @@ const ClinicalGrouping = compose(
                       borderBottom: `1px solid ${theme.greyScale5}`,
                     }}
                   >
-                    <Row style={{ alignItems: 'center' }}>
-                      <Tooltip
-                        Component={
-                          <div style={{ maxWidth: '24em' }}>
-                            {fieldDescription}
-                          </div>
-                        }
+                    <div style={{ display: 'flex', width: '100%' }}>
+                      <label
+                        htmlFor={fieldName}
+                        style={{
+                          width: '100%',
+                          display: 'block',
+                          cursor: 'pointer',
+                        }}
                       >
-                        <h4 style={{ fontSize: '1.4rem' }}>
-                          {humanify({ term: _.last(fieldName.split('.')) })}
-                        </h4>
-                      </Tooltip>
-                    </Row>
-                    <FacetCheckbox
-                      fieldName={fieldName}
-                      analysis_id={analysis_id}
-                      fieldType={name}
+                        <Tooltip
+                          Component={
+                            <div style={{ maxWidth: '24em' }}>
+                              {fieldDescription}
+                            </div>
+                          }
+                        >
+                          <h4
+                            style={{
+                              fontSize: '1.4rem',
+                              display: 'inline-block',
+                            }}
+                          >
+                            {humanify({ term: _.last(fieldName.split('.')) })}
+                          </h4>
+                        </Tooltip>
+                      </label>
+                    </div>
+                    <Toggle
+                      icons={false}
+                      id={fieldName}
                       disabled={!type.name}
-                      plotTypes={plotTypes}
+                      name={fieldName}
                       checked={checked}
-                      toggleAction={toggleAction}
-                      dispatch={dispatch}
+                      onChange={() => {
+                        if (!type.name) {
+                          return null;
+                        }
+                        dispatch(
+                          toggleAction({
+                            fieldName,
+                            id: analysis_id,
+                            fieldType: name,
+                            plotTypes,
+                          })
+                        );
+                      }}
                     />
                   </Row>
                 );
@@ -183,46 +210,9 @@ const ClinicalGrouping = compose(
   }
 );
 
-const FacetCheckbox = ({
-  fieldName,
-  dispatch,
-  analysis_id,
-  fieldType,
-  disabled,
-  plotTypes,
-  checked,
-  toggleAction,
-}) => (
-  <div
-    onClick={() => {
-      if (disabled) {
-        return null;
-      }
-      dispatch(
-        toggleAction({ fieldName, id: analysis_id, fieldType, plotTypes })
-      );
-    }}
-  >
-    <label htmlFor={fieldName}>
-      <Hidden>{fieldName}</Hidden>
-    </label>
-    <input
-      readOnly
-      type="checkbox"
-      style={{
-        pointerEvents: 'none',
-        flexShrink: 0,
-        verticalAlign: 'middle',
-      }}
-      disabled={disabled}
-      name={fieldName}
-      aria-label={fieldName}
-      checked={checked}
-    />
-  </div>
-);
-
-export default compose(withTheme)(
+export default compose(
+  withTheme
+)(
   ({
     theme,
     currentAnalysis,

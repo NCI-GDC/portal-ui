0f3094e7acf441b872ec753678c64f087a95c46a
Sam Rich <samrichca@users.noreply.github.com>
2019-04-10 12:55:11 -0400

    moved facets code out of the component

--- src/packages/@ncigdc/modern_components/ClinicalAnalysis/ControlPanelNode.js
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/ControlPanelNode.js
@@ -5,6 +5,7 @@ import {
   renderComponent,
   withState,
   withProps,
+  withPropsOnChange,
 } from 'recompose';
 import _ from 'lodash';
 import { connect } from 'react-redux';
@@ -43,6 +44,15 @@ const getPlotType = field => {
     return 'categorical';
   }
 };
+
+// will need to add other types as they become available
+const clinicalTypeOrder = [
+  'demographic',
+  'diagnoses',
+  'treatments',
+  'exposures',
+  // 'follow_ups',
+];
 
 const parseFieldName = field => field.replace(/__/g, '.');
 
@@ -103,66 +113,71 @@ const ClinicalGrouping = compose(
               padding: '0 10px',
             }}
           >
-            {fields.length > MAX_FIELDS_LENGTH && showingMore && (
-              <Row>
-                <StyledToggleMoreLink
-                  onClick={() => setShowingMore(!showingMore)}
-                >
-                  {'Less...'}
-                </StyledToggleMoreLink>
-              </Row>
-            )}
+            {fields.length > MAX_FIELDS_LENGTH &&
+              showingMore && (
+                <Row>
+                  <StyledToggleMoreLink
+                    onClick={() => setShowingMore(!showingMore)}
+                  >
+                    {'Less...'}
+                  </StyledToggleMoreLink>
+                </Row>
+              )}
             {_.orderBy(fields, 'name', 'asc')
               .slice(0, showingMore ? Infinity : MAX_VISIBLE_FACETS)
               .map(field => ({
                 fieldDescription:
                   field.description || 'No description available',
                 fieldName: parseFieldName(field.name),
+                fieldTitle: field.title,
                 type: field.type,
                 plotTypes: getPlotType(field),
               }))
-              .map(({ fieldDescription, fieldName, type, plotTypes }, i) => {
-                const checked = Object.keys(
-                  currentAnalysis.displayVariables
-                ).includes(fieldName);
-                const toggleAction = checked
-                  ? removeClinicalAnalysisVariable
-                  : addClinicalAnalysisVariable;
-                return (
-                  <Row
-                    key={i}
-                    style={{
-                      justifyContent: 'space-between',
-                      alignItems: 'center',
-                      borderBottom: `1px solid ${theme.greyScale5}`,
-                    }}
-                  >
-                    <Row style={{ alignItems: 'center' }}>
-                      <Tooltip
-                        Component={
-                          <div style={{ maxWidth: '24em' }}>
-                            {fieldDescription}
-                          </div>
-                        }
-                      >
-                        <h4 style={{ fontSize: '1.4rem' }}>
-                          {humanify({ term: _.last(fieldName.split('.')) })}
-                        </h4>
-                      </Tooltip>
+              .map(
+                (
+                  { fieldDescription, fieldName, type, fieldTitle, plotTypes },
+                  i
+                ) => {
+                  const checked = Object.keys(
+                    currentAnalysis.displayVariables
+                  ).includes(fieldName);
+                  const toggleAction = checked
+                    ? removeClinicalAnalysisVariable
+                    : addClinicalAnalysisVariable;
+                  return (
+                    <Row
+                      key={i}
+                      style={{
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        borderBottom: `1px solid ${theme.greyScale5}`,
+                      }}
+                    >
+                      <Row style={{ alignItems: 'center' }}>
+                        <Tooltip
+                          Component={
+                            <div style={{ maxWidth: '24em' }}>
+                              {fieldDescription}
+                            </div>
+                          }
+                        >
+                          <h4 style={{ fontSize: '1.4rem' }}>{fieldTitle}</h4>
+                        </Tooltip>
+                      </Row>
+                      <FacetCheckbox
+                        fieldName={fieldName}
+                        analysis_id={analysis_id}
+                        fieldType={name}
+                        disabled={!type.name}
+                        plotTypes={plotTypes}
+                        checked={checked}
+                        toggleAction={toggleAction}
+                        dispatch={dispatch}
+                      />
                     </Row>
-                    <FacetCheckbox
-                      fieldName={fieldName}
-                      analysis_id={analysis_id}
-                      fieldType={name}
-                      disabled={!type.name}
-                      plotTypes={plotTypes}
-                      checked={checked}
-                      toggleAction={toggleAction}
-                      dispatch={dispatch}
-                    />
-                  </Row>
-                );
-              })}
+                  );
+                }
+              )}
             {fields.length > MAX_VISIBLE_FACETS && (
               <Row>
                 <StyledToggleMoreLink
@@ -222,7 +237,28 @@ const FacetCheckbox = ({
   </div>
 );
 
-export default compose(withTheme)(
+export default compose(
+  withTheme,
+  withPropsOnChange(
+    (props, nextProps) => props.searchValue !== nextProps.searchValue,
+    ({ searchValue, clinicalAnalysisFields }) => {
+      const humanifyFields = clinicalAnalysisFields.map(field => ({
+        ...field,
+        title: humanify({ term: _.last(field.name.split('__')) }),
+      }));
+      const groupedByClinicalType = _.groupBy(humanifyFields, field => {
+        const sections = field.name.split('__');
+        if (sections.includes('treatments')) {
+          return sections[1];
+        } else {
+          return sections[0];
+        }
+      });
+
+      return { groupedByClinicalType };
+    }
+  )
+)(
   ({
     theme,
     currentAnalysis,
@@ -230,25 +266,9 @@ export default compose(withTheme)(
     clinicalAnalysisFields,
     usefulFacets,
     analysis_id,
+    searchValue,
+    groupedByClinicalType,
   }) => {
-    let groupedByClinicalType = _.groupBy(clinicalAnalysisFields, field => {
-      const sections = field.name.split('__');
-      if (sections.includes('treatments')) {
-        return sections[1];
-      } else {
-        return sections[0];
-      }
-    });
-
-    // will need to add other types as they become available
-    const clinicalTypeOrder = [
-      'demographic',
-      'diagnoses',
-      'treatments',
-      'exposures',
-      // 'follow_ups',
-    ];
-
     return (
       <div>
         <Row

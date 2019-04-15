55eafc46e1d9463e2ad31fde17750c73e4e9e741
Ann Catton <anncatton@gmail.com>
2019-04-15 16:43:53 -0400

    cleanup

--- src/packages/@ncigdc/components/analysis/availableAnalysis.js
+++ src/packages/@ncigdc/components/analysis/availableAnalysis.js
@@ -11,59 +11,6 @@ import { DISPLAY_CDAVE } from '@ncigdc/utils/constants';
 import ClinicalAnalysisContainer from '@ncigdc/modern_components/IntrospectiveType';
 import defaultVariables from './defaultCDAVEvariables';
 
-// const defaultVariables = {
-//   'demographic.ethnicity': {
-//     type: 'Demographic',
-//     active_chart: 'histogram',
-//     active_calculation: 'number',
-//     active_survival: 'overall',
-//     plotTypes: 'categorical',
-//     bins: [],
-//   },
-//
-//   'demographic.gender': {
-//     type: 'Demographic',
-//     active_chart: 'histogram',
-//     active_calculation: 'number',
-//     active_survival: 'overall',
-//     plotTypes: 'categorical',
-//     bins: [],
-//   },
-//
-//   'demographic.race': {
-//     type: 'Demographic',
-//     active_chart: 'histogram',
-//     active_calculation: 'number',
-//     active_survival: 'overall',
-//     plotTypes: 'categorical',
-//     bins: [],
-//   },
-//   'diagnoses.age_at_diagnosis': {
-//     type: 'Diagnosis',
-//     active_chart: 'histogram',
-//     active_calculation: 'number',
-//     active_survival: 'overall',
-//     plotTypes: 'continuous',
-//     bins: [],
-//   },
-//
-//   'diagnoses.age_at_diagnosis': {
-//     type: 'Diagnosis',
-//     active_chart: 'histogram',
-//     active_calculation: 'number',
-//     active_survival: 'overall',
-//     plotTypes: 'continuous',
-//     bins: [],
-//   },
-//   'diagnoses.cause_of_death': {
-//     type: 'Diagnosis',
-//     active_chart: 'histogram',
-//     active_calculation: 'number',
-//     active_survival: 'overall',
-//     plotTypes: 'categorical',
-//     bins: [],
-//   },
-// };
 export type TSelectedSets = {
   [TSetTypes]: any,
 };

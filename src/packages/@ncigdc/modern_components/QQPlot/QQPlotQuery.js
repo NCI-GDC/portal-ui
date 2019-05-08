b38c03c995923135487a8e0fa62b97ad0b11544e
Ann Catton <anncatton@gmail.com>
2019-05-08 16:08:00 -0400

    collect qq data points for all clinical types

--- src/packages/@ncigdc/modern_components/QQPlot/QQPlotQuery.js
+++ src/packages/@ncigdc/modern_components/QQPlot/QQPlotQuery.js
@@ -2,46 +2,23 @@ import React from 'react';
 import {
   compose, withPropsOnChange, withProps, withState, // branch, renderComponent,
 } from 'recompose';
+import _ from 'lodash';
 
 import { addInFilters } from '@ncigdc/utils/filters';
-import customQuery from '@ncigdc/modern_components/QQPlot/customQuery';
 import Loader from '@ncigdc/uikit/Loaders/Loader';
 import { fetchApi } from '@ncigdc/utils/ajax';
 import QQPlot from './QQPlot';
 
-const myFieldName = 'diagnoses.treatments.days_to_treatment_start';
-
 export default compose(
-    // branch(
-    //   ({ typeName }) => !typeName,
-    //   renderComponent(() => (
-    //     <div>
-    //       <pre>Type name</pre> must be provided
-    //     </div>
-    //   ))
-    // ),
   withState('data', 'setData', null),
   withState('isLoading', 'setIsLoading', true),
   withPropsOnChange(['filters', 'fieldName'], ({ fieldName, filters, first }) => {
-    const fooFilters = {
-      op: 'and',
-      content: [
-        {
-          op: '=',
-          content: {
-            field: 'cases.case_id',
-            value: 'set_id:AWp1PRe5MiBtcCotu6Tn',
-          },
-        },
-      ],
-    };
     const missingFilter = {
       op: 'and',
       content: [
         {
           op: 'NOT',
           content: {
-            // field: `cases.${myFieldName}`,
             field: `cases.${fieldName}`,
             value: ['MISSING'],
           },
@@ -52,21 +29,6 @@ export default compose(
       newFilters: addInFilters(filters, missingFilter),
       first,
     };
-
-  //   const facetName = fieldName.replace(/\./g, '__');
-  //
-  //   return {
-  //     // query: `query QQPlotQuery(\n $filters_1: FiltersArgument \n) {\n viewer {\n explore {\n cases {\n hits(first: ${first}, filters: $filters_1) {\n total \n edges {\n node {\n ${clinicalType} {\n hits(first: 99) {\n edges {\n node {\n ${queryField}\n }\n }\n }\n }\n }\n }\n }\n }\n }\n }\n }`,
-  //     query: `query QQPlotQuery(\n $filters_1: FiltersArgument \n) {\n viewer {\n explore {\n cases {\n hits(filters: $filters_1) {\n total \n }\n aggregations(filters: $filters_1) {\n ${facetName} {\n histogram(interval:1) {\n buckets {\n key \n doc_count \n }\n }\n }\n }\n }\n }\n }\n }`,
-  //     variables: {
-  //       // filters_1: filters
-  //       filters_1: addInFilters(filters, missingFilter),
-  //       first
-  //     },
-  //     clinicalType,
-  //     queryField,
-  //     fieldName,
-  //   };
   }),
   withProps({
     updateData: async ({
@@ -74,45 +36,47 @@ export default compose(
       first,
       setData,
       setIsLoading,
-      // query,
-      // queryName = 'QQPlot',
       fieldName,
     }) => {
       const res = await fetchApi('case_ssms', {
         headers: { 'Content-Type': 'application/json' },
         body: {
+
           filters: JSON.stringify(newFilters),
           size: first,
           fields: fieldName,
         },
       });
       const data = res && res.data ? res.data.hits : [];
-      // const parsedFieldName = field => {
-        // return field.split('.');
-          // return {
-          //   clinicalType: parsed[0],
-          //   queryField: parsed[1],
-          // };
-      // };
-      // const { clinicalType, queryField } = parsedFieldName(fieldName);
+      const parsedFieldName = field => {
+        const parsed = field.split('.');
+        return {
+          clinicalType: parsed[0],
+          clinicalField: parsed[1],
+          clinicalNestedField: parsed[2],
+        };
+      };
+      const { clinicalType, clinicalField, clinicalNestedField } = parsedFieldName(fieldName);
 
-      const splitName = fieldName.split('.');
 
       const continuousValues = [];
-      data.map(d => {
-        return d[splitName[0]].map(firstLevel => {
-          if (_.isArray(firstLevel[splitName[1]])) {
-            return firstLevel[splitName[1]].map(secondLevel => {
-              continuousValues.push(secondLevel[splitName[2]]);
-            });
-          }
-          continuousValues.push(firstLevel[splitName[1]]);
-        });
-      });
 
-      console.log('values: ', continuousValues.filter(b => _.isNumber(b)));
+      data.forEach(hit => {
+        if (_.isArray(hit[clinicalType])) {
+          return hit[clinicalType].map(subType => {
+            if (clinicalNestedField) {
+              return subType[clinicalField].map(sub => {
+                return continuousValues.push(sub[clinicalNestedField]);
+              });
+            }
+            return continuousValues.push(subType[clinicalField]);
+          });
+        } if (_.isPlainObject(hit[clinicalType])) {
+          return continuousValues.push(hit[clinicalType][clinicalField]);
+        }
+        return continuousValues.push(hit[clinicalType]);
+      });
       setData(continuousValues.filter(b => _.isNumber(b)), () => setIsLoading(false));
-      // setData(res && res.data.viewer, () => setIsLoading(false));
     },
   }),
   withPropsOnChange(['fieldName'], ({ updateData, ...props }) => updateData(props))

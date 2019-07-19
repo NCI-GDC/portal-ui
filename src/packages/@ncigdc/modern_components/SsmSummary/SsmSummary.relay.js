ebfb6144903aecc22e3665290943bb562a1c1a83
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2019-07-19 13:54:16 -0400

    Revert "Merge pull request #2571 from NCI-GDC/feature/PRTL-2534"
    This reverts commit 79dfb87caa1a552dc1f9957d4f36d7edfe14844b, reversing
    changes made to 83a17596e9770a9ac87cfd8f626710d0a8a5dde0.

--- src/packages/@ncigdc/modern_components/SsmSummary/SsmSummary.relay.js
+++ src/packages/@ncigdc/modern_components/SsmSummary/SsmSummary.relay.js
@@ -1,77 +1,77 @@
+// @flow
+
 import React from 'react';
 import { graphql } from 'react-relay';
 import { makeFilter } from '@ncigdc/utils/filters';
-import {
-  branch,
-  compose,
-  renderComponent,
-  setDisplayName,
-  withPropsOnChange,
-} from 'recompose';
+import { compose, withPropsOnChange, branch, renderComponent } from 'recompose';
 import Query from '@ncigdc/modern_components/Query';
 
-export default (Component: ReactClass<*>) => compose(
-  setDisplayName('EnhancedSSMSummary_Relay'),
-  branch(
-    ({ ssmId }) => !ssmId,
-    renderComponent(() => (
-      <div>
-        <pre>ssmId</pre>
-        {' '}
-        must be provided
-      </div>
-    )),
-  ),
-  withPropsOnChange(['ssmId'], ({ ssmId }) => ({
-    variables: {
-      consequenceFilters: makeFilter([
-        {
-          field: 'consequence.transcript.is_canonical',
-          value: 'true',
+export default (Component: ReactClass<*>) =>
+  compose(
+    branch(
+      ({ ssmId }) => !ssmId,
+      renderComponent(() => (
+        <div>
+          <pre>ssmId</pre> must be provided
+        </div>
+      )),
+    ),
+    withPropsOnChange(['ssmId'], ({ ssmId }) => {
+      return {
+        variables: {
+          filters: makeFilter([
+            {
+              field: 'ssms.ssm_id',
+              value: [ssmId],
+            },
+          ]),
+          consequenceFilters: makeFilter([
+            {
+              field: 'consequence.transcript.is_canonical',
+              value: 'true',
+            },
+          ]),
         },
-      ]),
-      filters: makeFilter([
-        {
-          field: 'ssms.ssm_id',
-          value: [ssmId],
-        },
-      ]),
-    },
-  })),
-)((props: Object) => (
-  <Query
-    Component={Component}
-    minHeight={props.minHeight || 200}
-    parentProps={props}
-    query={graphql`
-      query SsmSummary_relayQuery(
-        $filters: FiltersArgument
-        $consequenceFilters: FiltersArgument
-      ) {
-        viewer {
-          explore {
-            ssms {
-              hits(first: 1, filters: $filters) {
-                edges {
-                  node {
-                    ssm_id
-                    reference_allele
-                    mutation_subtype
-                    ncbi_build
-                    genomic_dna_change
-                    consequence {
-                      hits(first: 1, filters: $consequenceFilters) {
-                        edges {
-                          node {
-                            transcript {
-                              is_canonical
-                              transcript_id
-                              annotation {
-                                polyphen_impact
-                                polyphen_score
-                                sift_score
-                                sift_impact
-                                vep_impact
+      };
+    }),
+  )((props: Object) => {
+    return (
+      <Query
+        parentProps={props}
+        minHeight={props.minHeight || 200}
+        variables={props.variables}
+        Component={Component}
+        query={graphql`
+          query SsmSummary_relayQuery(
+            $filters: FiltersArgument
+            $consequenceFilters: FiltersArgument
+          ) {
+            viewer {
+              explore {
+                ssms {
+                  hits(first: 1, filters: $filters) {
+                    edges {
+                      node {
+                        ssm_id
+                        reference_allele
+                        mutation_subtype
+                        ncbi_build
+                        genomic_dna_change
+                        consequence {
+                          hits(first: 1, filters: $consequenceFilters) {
+                            edges {
+                              node {
+                                transcript {
+                                  is_canonical
+                                  transcript_id
+                                  annotation {
+                                    polyphen_impact
+                                    polyphen_score
+                                    sift_score
+                                    sift_impact
+                                    vep_impact
+                                  }
+                                }
                               }
                             }
                           }
@@ -83,9 +83,7 @@ export default (Component: ReactClass<*>) => compose(
               }
             }
           }
-        }
-      }
-    `}
-    variables={props.variables}
-    />
-));
+        `}
+      />
+    );
+  });

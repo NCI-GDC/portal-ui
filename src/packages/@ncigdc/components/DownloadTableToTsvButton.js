55a1509fe8bc7194e5f817e102fba028aab3c0da
Sam Rich <samrichca@users.noreply.github.com>
2019-07-22 16:13:13 -0400

    [PRTL-2688] remove select from histogram TSV (#2627)

--- src/packages/@ncigdc/components/DownloadTableToTsvButton.js
+++ src/packages/@ncigdc/components/DownloadTableToTsvButton.js
@@ -16,15 +16,17 @@ type TProps = {
   style: Object,
 };
 
-const getSingleHeader = (headThs: Array<NodeList>) =>
-  reduce(
-    headThs[0],
-    (acc, th) =>
-      th.rowSpan === 2 ? [...acc, th] : [...acc, ...map(headThs[1], t => t)],
-    []
-  );
+const getSingleHeader = (headThs: Array<NodeList>) => reduce(
+  headThs[0],
+  (acc, th) => (th.rowSpan === 2 ? [...acc, th] : [...acc, ...map(headThs[1], t => t)]),
+  []
+);
 
-export const downloadToTSV = ({ selector, filename }) => {
+export const downloadToTSV = ({ excludedColumns = [
+  'Add all files to cart',
+  'Remove all files from cart',
+  'Select column',
+], filename, selector }) => {
   const tableEl = document.querySelector(selector);
   const headTrs = tableEl.querySelector('thead').querySelectorAll('tr');
   const headThs = map(headTrs, h => h.querySelectorAll('th'));
@@ -32,9 +34,7 @@ export const downloadToTSV = ({ selector, filename }) => {
     headThs.length === 2
       ? getSingleHeader(headThs)
       : tableEl.querySelectorAll('th');
-  const thText = map(thEls, el => el.innerText).map(t =>
-    t.replace(/\s+/g, ' ')
-  );
+  const thText = map(thEls, el => el.innerText).map(t => t.replace(/\s+/g, ' '));
   const trs = tableEl.querySelector('tbody').querySelectorAll('tr');
   const tdText = map(trs, t => {
     const tds = t.querySelectorAll('td');
@@ -42,7 +42,9 @@ export const downloadToTSV = ({ selector, filename }) => {
       tds,
       (acc, td) => {
         const markedForTsv = td.querySelector('.for-tsv-export');
-        const exportText = markedForTsv ? markedForTsv.innerText : td.innerText;
+        const exportText = markedForTsv
+          ? markedForTsv.innerText
+          : td.innerText;
         const joinedText = exportText
           .trim()
           .split(/\s*\n\s*/)
@@ -57,12 +59,28 @@ export const downloadToTSV = ({ selector, filename }) => {
       []
     );
   });
-  saveFile(mapStringArrayToTsvString(thText, tdText), 'TSV', filename);
-  track('download-table', { type: 'tsv', filename, selector });
+  const excludedIndices = excludedColumns.reduce((acc, curr) => {
+    const normalizedCurr = curr.toLowerCase().trim();
+    const normalizedThText = thText.map(th => th.toLowerCase().trim());
+    const index = normalizedThText.indexOf(normalizedCurr);
+    return [...acc, ...(index >= 0 ? [index] : [])];
+  }, []);
+  const thFiltered = thText
+    .filter((th, idx) => excludedIndices.indexOf(idx) === -1);
+  const tdFiltered = tdText.map(tr => tr
+    .filter((td, idx) => excludedIndices.indexOf(idx) === -1));
+
+  saveFile(mapStringArrayToTsvString(thFiltered, tdFiltered), 'TSV', filename);
+  track('download-table', {
+    filename,
+    selector,
+    type: 'tsv',
+  });
 };
 
 const DownloadTableToTsvButton = compose(withTooltip)(
   ({
+    excludedColumns,
     filename,
     selector,
     leftIcon,
@@ -72,13 +90,19 @@ const DownloadTableToTsvButton = compose(withTooltip)(
   }: TProps) => (
     <Button
       leftIcon={leftIcon}
-      style={{ ...visualizingButton, ...style }}
-      onMouseEnter={() =>
-        displayTooltip && setTooltip(<span>Export current view</span>)
+      onClick={() => downloadToTSV({
+        excludedColumns,
+        filename,
+        selector,
+      })}
+      onMouseEnter={() => displayTooltip && setTooltip(<span>Export current view</span>)
       }
       onMouseLeave={() => displayTooltip && setTooltip('')}
-      onClick={() => downloadToTSV({ selector, filename })}
-    >
+      style={{
+        ...visualizingButton,
+        ...style,
+      }}
+      >
       TSV
     </Button>
   )

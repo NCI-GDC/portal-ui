b447504af01d043aba7dd33701f4c792bd3ac914
Ann Catton <anncatton@gmail.com>
2019-07-29 15:57:39 -0400

    🐛 PRTL-2606 Calculate qq line start/end points instead of clipping plot edges

--- src/packages/@ncigdc/modern_components/QQPlot/QQPlot.js
+++ src/packages/@ncigdc/modern_components/QQPlot/QQPlot.js
@@ -79,28 +79,21 @@ const QQPlot = ({
   el.style.width = '100%';
   el.setAttribute('class', 'qq-plot');
 
+
+  const xMin = d3.min(zScores, d => Math.floor(d.x));
+  const xMax = d3.max(zScores, d => Math.ceil(d.x));
+
+  const yMin = d3.min(zScores, d => Math.floor(d.y));
+  const yMax = d3.max(zScores, d => Math.ceil(d.y));
+
   const xScale = d3
     .scaleLinear()
-    .domain([
-      d3.min(zScores, (d) => {
-        return Math.floor(d.x);
-      }),
-      d3.max(zScores, (d) => {
-        return Math.ceil(d.x);
-      }),
-    ])
+    .domain([xMin, xMax])
     .range([padding, chartWidth - padding * 2]);
 
   const yScale = d3
     .scaleLinear()
-    .domain([
-      d3.min(zScores, (d) => {
-        return Math.floor(d.y);
-      }),
-      d3.max(zScores, (d) => {
-        return Math.ceil(d.y);
-      }),
-    ])
+    .domain([yMin, yMax])
     .range([chartHeight, padding]);
 
   const xAxis = d3
@@ -115,12 +108,21 @@ const QQPlot = ({
 
   // get slope from first and third quantile to match qqline from R
   const slope = (quantile3Coords.y - quantile1Coords.y) / (quantile3Coords.x - quantile1Coords.x);
-  const yMin = zScores[0].y;
-  const yMax = last(zScores).y;
+  // const yMin = zScores[0].y;
+  // const yMax = last(zScores).y;
 
-  // calculate x values for start and end of line
-  const xAtYMin = quantile1Coords.x - ((quantile1Coords.y - yMin) / slope);
-  const xAtYMax = quantile3Coords.x + ((yMax - quantile3Coords.y) / slope);
+  // calculate coords for start and end of line with y = mx + b
+  // start and end points will equal the y/x min and max OR
+  // intercepts, whichever is within plot limits
+  const b = quantile1Coords.y - (slope * quantile1Coords.x);
+
+  // const xAtYMin = quantile1Coords.x - ((quantile1Coords.y - yMin) / slope);
+  // const xAtYMax = quantile3Coords.x + ((yMax - quantile3Coords.y) / slope);
+
+  const xAtYMin = (yMin - b) / slope;
+  const xAtYMax = (yMax - b) / slope;
+  const yAtXMax = (slope * xMax) + b;
+  const yAtXMin = (slope * xMin) + b;
 
   const svg = d3
     .select(el)
@@ -153,14 +155,18 @@ const QQPlot = ({
     .attr('class', 'coords')
     .datum([
       {
-        x: xAtYMin,
-        y: yMin,
+        // x: xAtYMin,
+        // y: yMin,
+        x: Math.max(xAtYMin, xMin),
+        y: Math.max(yAtXMin, yMin),
       },
       ...quantile1Coords,
       ...quantile3Coords,
       {
-        x: xAtYMax,
-        y: yMax,
+        x: Math.min(xAtYMax, xMax),
+        y: Math.min(yMax, yAtXMax),
+        // x: xAtYMax,
+        // y: yMax,
       },
     ])
     .attr('d', line)
@@ -169,26 +175,26 @@ const QQPlot = ({
     .attr('transform', `translate(${padding},${-(padding / 2)})`);
 
 // clip path to prevent qq line extending beyond y-axis
-  svg
-    .append('rect')
-    .attr('x', -(padding))
-    .attr('y', padding / 2)
-    .attr('clip-path', 'url(#regression-clip-left)')
-    .style('fill', 'white')
-    .attr('height', chartHeight - padding)
-    .attr('width', padding * 2)
-    .attr('transform', `translate(${padding},${padding / 2})`);
+  // svg
+  //   .append('rect')
+  //   .attr('x', -(padding))
+  //   .attr('y', padding / 2)
+  //   .attr('clip-path', 'url(#regression-clip-left)')
+  //   .style('fill', 'white')
+  //   .attr('height', chartHeight - padding)
+  //   .attr('width', padding * 2)
+  //   .attr('transform', `translate(${padding},${padding / 2})`);
 
   // position slightly higher to account for qqline end
-  svg
-    .append('rect')
-    .attr('x', -(padding))
-    .attr('y', padding / 2)
-    .attr('clip-path', 'url(#regression-clip-right)')
-    .style('fill', 'white')
-    .attr('height', chartHeight)
-    .attr('width', padding * 2)
-    .attr('transform', `translate(${chartWidth},${-padding})`);
+  // svg
+  //   .append('rect')
+  //   .attr('x', -(padding))
+  //   .attr('y', padding / 2)
+  //   .attr('clip-path', 'url(#regression-clip-right)')
+  //   .style('fill', 'white')
+  //   .attr('height', chartHeight)
+  //   .attr('width', padding * 2)
+  //   .attr('transform', `translate(${chartWidth},${-padding})`);
 
   svg
     .append('text')

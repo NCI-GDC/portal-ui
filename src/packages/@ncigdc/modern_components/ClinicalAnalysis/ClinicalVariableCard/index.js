4107e635f3f0c063e6e49b9ac9cdc5cc4cfe97e4
Henry Zhao <yuanheng.zhao@gmail.com>
2019-08-01 11:00:06 -0400

    [PRTL-2611] Merge qq/box download button. (#2644)
    * Merge qq/box download button.
    
    * Update package.
    
    * Update the color of boxplot.

--- src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/index.js
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/index.js
@@ -925,25 +925,6 @@ const ClinicalVariableCard: React.ComponentType<IVariableCardProps> = ({
                         Box Plot
                       </span>
                     </Row>
-                    <Row>
-                      <DownloadVisualizationButton
-                        buttonStyle={{
-                          fontSize: '1.2rem',
-                          lineHeight: 0,
-                          minHeight: 20,
-                          minWidth: 22,
-                          padding: 0,
-                        }}
-                        noText
-                        slug={`boxplot-${fieldName}`}
-                        svg={() => wrapSvg({
-                          className: 'boxplot',
-                          selector: `#${wrapperId}-boxplot-container figure svg`,
-                          title: `${humanify({ term: fieldName })} Box Plot`,
-                        })}
-                        tooltipHTML="Download SVG or PNG"
-                        />
-                    </Row>
                     <Row
                       style={{
                         alignItems: 'center',
@@ -963,21 +944,26 @@ const ClinicalVariableCard: React.ComponentType<IVariableCardProps> = ({
                     </Row>
                     <Row>
                       <DownloadVisualizationButton
-                        buttonStyle={{
-                          fontSize: '1.2rem',
-                          lineHeight: 0,
-                          minHeight: 20,
-                          minWidth: 22,
-                          padding: 0,
-                        }}
                         data={qqData}
                         noText
-                        slug={`qq-plot-${fieldName}`}
-                        svg={() => wrapSvg({
-                          className: 'qq-plot',
-                          selector: `#${wrapperId}-qqplot-container .qq-plot svg`,
-                          title: `${humanify({ term: fieldName })} QQ Plot`,
-                        })}
+                        slug={[`qq-plot-${fieldName}`, `boxplot-${fieldName}`]}
+                        style={{
+                          float: 'right',
+                          marginRight: 2,
+                        }}
+                        svg={
+                          [
+                            () => wrapSvg({
+                              className: 'qq-plot',
+                              selector: `#${wrapperId}-qqplot-container .qq-plot svg`,
+                              title: `${humanify({ term: fieldName })} QQ Plot`,
+                            }),
+                            () => wrapSvg({
+                              className: `${variable.type.toLowerCase()}-boxplot`,
+                              selector: `#${wrapperId}-boxplot-container figure svg`,
+                              title: `${humanify({ term: fieldName })} Box Plot`,
+                            }),
+                        ]}
                         tooltipHTML="Download plot data"
                         tsvData={qqData}
                         />

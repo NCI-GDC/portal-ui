1f5b4482cf36f8a0d7291988db9626f490b61174
Ann Catton <anncatton@gmail.com>
2019-08-29 14:29:07 -0400

    update to nivo/sunburst package. Move fetch to external util. Add custom colour key to donut data

--- src/packages/@ncigdc/modern_components/PrimarySiteAndDiseaseType/ExploreSummaryPrimarySite.js
+++ src/packages/@ncigdc/modern_components/PrimarySiteAndDiseaseType/ExploreSummaryPrimarySite.js
@@ -1,92 +1,44 @@
 import React from 'react';
 import { compose, withPropsOnChange, withState } from 'recompose';
-import { Sunburst } from 'nivo';
+import { Sunburst } from '@nivo/sunburst';
 import { parse } from 'query-string';
-import { fetchQuery, graphql } from 'relay-runtime';
+import Color from 'color';
 
-import { API, IS_AUTH_PORTAL } from '@ncigdc/utils/constants';
-import md5 from 'blueimp-md5';
-import urlJoin from 'url-join';
-import _ from 'lodash';
-
-import consoleDebug from '@ncigdc/utils/consoleDebug';
-import { redirectToLogin } from '@ncigdc/utils/auth';
-
-import environment from '@ncigdc/modern_components/environment';
 import { parseFilterParam } from '@ncigdc/utils/uri';
 import withRouter from '@ncigdc/utils/withRouter';
-import { withTheme } from '@ncigdc/theme';
 import { replaceFilters } from '@ncigdc/utils/filters';
-// import withData from './DiseaseTypeByPrimarySite.relay';
-
-const arrghData = {
-  name: 'Primary Sites',
-  children: [
-    {
-      name: 'Breast',
-      children: [
-        {
-          name: 'Disease type 1',
-          size: 3,
-        },
-        {
-          name: 'Disease type 2',
-          size: 10,
-        },
-        {
-          name: 'Disease type 3',
-          size: 55,
-        },
-      ],
-    },
-    {
-      name: 'Bronchus and lung',
-      children: [
-        {
-          name: 'Disease type 1',
-          size: 500,
-        },
-        {
-          name: 'Disease type 2',
-          size: 25,
-        },
-      ],
-    },
-    {
-      name: 'Stomach',
-      children: [
-        {
-          name: 'Disease type 1',
-          size: 112,
-        },
-        {
-          name: 'Disease type 2',
-          size: 5,
-        },
-      ],
-    },
-  ],
-};
+import { theme } from '@ncigdc/theme';
+import customGraphQL from '@ncigdc/utils/customGraphQL';
 
 const getStringName = (name = '') => `${name.replace(/\s|,|-/g, '_')}`;
 
-const getDiseaseTypeBySite = (({
+const primarySiteColors = {
+  Breast: '#ED2891',
+  Colon: '#9467bd',
+  Kidney: '#2ca02c',
+  'Lymph Nodes': '#3953A4',
+  Pancreas: '#e17ac1',
+  Skin: '#BBD642',
+  Unknown: '#ff7f0e',
+};
+
+const getDiseaseTypeBySite = (async ({
   filters,
   primarySites,
 }) => {
   const variables = primarySites.reduce((acc, site) => {
     const siteFilter = {
       [`${getStringName(site)}Filters`]: replaceFilters({
-        op: 'and',
         content: [
           {
-            op: 'in',
             content: {
               field: 'cases.primary_site',
               value: [site],
             },
+            op: 'in',
           },
         ],
+        op: 'and',
       }, filters),
     };
     return Object.assign(
@@ -122,59 +74,14 @@ const getDiseaseTypeBySite = (({
     variables,
   });
 
-  const hash = md5(body);
-
-  return fetch(
-    urlJoin(API, `graphql/DiseaseTypeByPrimarySiteQuery?hash=${hash}`),
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body,
-    }
-  ).then(response => response
-    .json()
-    .then(json => {
-      if (!response.ok) {
-        consoleDebug('throwing error in Environment');
-        throw response;
-      }
-
-      // if (response.status === 200) {
-      //   simpleAggCache[hash] = json;
-      //   delete pendingAggCache[hash];
-      // }
-
-      return json;
-    })
-    .catch(err => {
-      if (err.status) {
-        switch (err.status) {
-          case 401:
-          case 403:
-            consoleDebug(err.statusText);
-            if (IS_AUTH_PORTAL) {
-              return redirectToLogin('timeout');
-            }
-            break;
-          case 400:
-          case 404:
-            consoleDebug(err.statusText);
-            break;
-          default:
-            return consoleDebug(`Default error case: ${err.statusText}`);
-        }
-      } else {
-        consoleDebug(
-          `Something went wrong in the environment, but no error status: ${err}`
-        );
-      }
-    }));
+  const data = await customGraphQL({
+    body,
+    component: 'DiseaseTypeByPrimarySite',
+  });
+  return data;
 });
 
 const enhance = compose(
-  withTheme,
   withRouter,
   withState('arcData', 'setArcData', {}),
   withPropsOnChange(
@@ -195,82 +102,53 @@ const enhance = compose(
       const filters = parseFilterParam(q.filters, defaultFilters);
 
       const primarySites = (aggregations && aggregations.primary_site.buckets);
-      // const variables = {
-      //   variables: {
-      //     filters: replaceFilters({
-      //       op: 'and',
-      //       content: [
-      //         {
-      //           op: 'in',
-      //           content: {
-      //             field: 'cases.primary_site',
-      //             value: primarySites,
-      //           },
-      //         },
-      //       ],
-      //     }, filters),
-      //   },
-      // };
 
       const data = await getDiseaseTypeBySite({
         filters,
         primarySites: primarySites.map(p => p.key),
       });
 
-      // const data = await fetchQuery(environment, diseaseTypeQuery, variables);
-      // console.log(data);
-
-      const colors = {
-        Breast: '#1f77b4',
-        Unknown: '#ff7f0e',
-        Colon: '#9467bd',
-        Kidney: '#2ca02c',
-        Pancreas: 'pink',
-      };
       const { data: { viewer: { explore: { cases } } } } = data;
-      const foo = {
-        name: 'Primary Sites',
+      const parsedData = {
         children: primarySites.map(primarySite => {
           return {
-            name: primarySite.key,
-            children: cases ? cases[`${getStringName(primarySite.key)}Aggs`].disease_type.buckets.map(bucket => {
-              return {
-                name: bucket.key,
-                // color: 'hsl(176,100%,33%)',
-                size: bucket.doc_count,
-              };
-            }) : [],
-            color: colors[primarySite.key],
+            children: cases
+              ? cases[`${getStringName(primarySite.key)}Aggs`].disease_type.buckets.map(bucket => {
+                return {
+                  color: Color(primarySiteColors[primarySite.key] ||
+                  theme.primaryLight1).lighten(0.3).rgbString(),
+                  id: bucket.key,
+                  size: bucket.doc_count,
+                // cases: `${bucket.doc_count} Cases`,
+                };
+              })
+            : [],
+            color: primarySiteColors[primarySite.key] || theme.primaryLight1,
+            id: primarySite.key,
             // size: primarySite.doc_count,
           };
         }),
+        id: 'Primary Sites',
       };
-      setArcData(foo);
+      setArcData(parsedData);
     }
   )
 );
 
 const ExploreSummaryPrimarySite = ({
   arcData,
-  ...props
 }) => {
-  console.log(arcData);
-
   return (
     <div>
       <Sunburst
         borderColor="white"
-        borderWidth={1}
-        // childColor={{ from: 'hsl(176,100%,33%)' }}
-        // colors={[
-        //   '#1f77b4',
-        //   '#ff7f0e',
-        //   '#9467bd',
-        //   '#2ca02c',
-        // ]}
+        borderWidth={0.5}
+        childColor="noinherit"
+        colorBy={data => data.color}
+        colors={Object.values(primarySiteColors)}
         data={arcData}
         height={400}
-        identity="name"
+        identity="id"
         margin={{
           bottom: 20,
           left: 20,

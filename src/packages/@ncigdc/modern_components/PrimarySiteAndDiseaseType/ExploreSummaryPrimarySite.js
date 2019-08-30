8088c6d4dffdeaf410ab23a8fe4bde8d515df606
Ann Catton <anncatton@gmail.com>
2019-08-30 12:37:23 -0400

    donut data refreshes. added loader

--- src/packages/@ncigdc/modern_components/PrimarySiteAndDiseaseType/ExploreSummaryPrimarySite.js
+++ src/packages/@ncigdc/modern_components/PrimarySiteAndDiseaseType/ExploreSummaryPrimarySite.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { compose, withPropsOnChange, withState } from 'recompose';
-import { Sunburst } from '@nivo/sunburst';
-import { parse } from 'query-string';
+import {
+  compose, withProps, withPropsOnChange, withState,
+} from 'recompose';
 import Color from 'color';
+import { isEmpty } from 'lodash';
 
-import { parseFilterParam } from '@ncigdc/utils/uri';
 import withRouter from '@ncigdc/utils/withRouter';
 import { replaceFilters } from '@ncigdc/utils/filters';
-import { theme } from '@ncigdc/theme';
 import customGraphQL from '@ncigdc/utils/customGraphQL';
-
-const getStringName = (name = '') => `${name.replace(/\s|,|-/g, '_')}`;
+import { withTheme } from '@ncigdc/theme';
+import DoubleDonut from '@ncigdc/components/Charts/DoubleDonut';
 
 const primarySiteColors = {
   Breast: '#ED2891',
@@ -22,7 +21,9 @@ const primarySiteColors = {
   Unknown: '#ff7f0e',
 };
 
-const getDiseaseTypeBySite = (async ({
+const getStringName = (name = '') => `${name.replace(/\s|,|-/g, '_')}`;
+
+const buildDiseaseTypeRequestBody = (({
   filters,
   primarySites,
 }) => {
@@ -48,48 +49,62 @@ const getDiseaseTypeBySite = (async ({
     );
   }, {});
 
-  const body = JSON.stringify({
+  return JSON.stringify({
     query: `
-    query DiseaseTypeByPrimarySite_relayQuery(${
-      primarySites.map(site => `$${getStringName(site)}Filters: FiltersArgument`).join(' ')
-    }) {
-      viewer {
-        explore {
-          cases {
-            ${primarySites.map(site => {
-      return `${getStringName(site)}Aggs: aggregations(filters: $${getStringName(site)}Filters) {
-                disease_type {
-                  buckets {
-                    key
-                    doc_count
-                  }
-                }
-              }`;
-    })}
+      query DiseaseTypeByPrimarySite_relayQuery(${
+        primarySites.map(site => `$${getStringName(site)}Filters: FiltersArgument`).join(' ')
+      }) {
+        viewer {
+          explore {
+            cases {
+              ${primarySites.map(site => `${getStringName(site)}Aggs: aggregations(filters: $${getStringName(site)}Filters) {
+                    disease_type {
+                      buckets {
+                        key
+                        doc_count
+                      }
+                    }
+                  }`)}
+            }
           }
         }
       }
-    }
     `,
     variables,
   });
-
-  const data = await customGraphQL({
-    body,
-    component: 'DiseaseTypeByPrimarySite',
-  });
-  return data;
 });
 
 const enhance = compose(
   withRouter,
+  withTheme,
+  withState('isLoading', 'setIsLoading', true),
   withState('arcData', 'setArcData', {}),
+  withProps({
+    updateData: async ({
+      primarySites,
+      setIsLoading,
+      variables,
+    }) => {
+      setIsLoading(true);
+      const body = buildDiseaseTypeRequestBody({
+        filters: variables.filters,
+        primarySites,
+      });
+      const data = await customGraphQL({
+        body,
+        component: 'DiseaseTypeByPrimarySite',
+      });
+      return data;
+    },
+  }),
   withPropsOnChange(
-    ['location'],
+    ['viewer'],
     async ({
-      defaultFilters = null,
-      location: { search },
       setArcData,
+      setIsLoading,
+      theme,
+      updateData,
+      variables,
       viewer: {
         explore: {
           cases: {
@@ -98,17 +113,15 @@ const enhance = compose(
         },
       },
     }) => {
-      const q = parse(search);
-      const filters = parseFilterParam(q.filters, defaultFilters);
-
       const primarySites = (aggregations && aggregations.primary_site.buckets);
-
-      const data = await getDiseaseTypeBySite({
-        filters,
+      const data = await updateData({
         primarySites: primarySites.map(p => p.key),
+        setIsLoading,
+        variables,
       });
 
       const { data: { viewer: { explore: { cases } } } } = data;
+
       const parsedData = {
         children: primarySites.map(primarySite => {
           return {
@@ -117,49 +130,38 @@ const enhance = compose(
                 return {
                   color: Color(primarySiteColors[primarySite.key] ||
                   theme.primaryLight1).lighten(0.3).rgbString(),
+                  count: bucket.doc_count,
                   id: bucket.key,
-                  size: bucket.doc_count,
                 // cases: `${bucket.doc_count} Cases`,
                 };
               })
             : [],
             color: primarySiteColors[primarySite.key] || theme.primaryLight1,
             id: primarySite.key,
-            // size: primarySite.doc_count,
+            // count: primarySite.doc_count,
           };
         }),
         id: 'Primary Sites',
       };
-      setArcData(parsedData);
+
+      setArcData(parsedData, () => setIsLoading(false));
     }
-  )
+  ),
 );
 
 const ExploreSummaryPrimarySite = ({
   arcData,
-}) => {
-  return (
-    <div>
-      <Sunburst
-        borderColor="white"
-        borderWidth={0.5}
-        childColor="noinherit"
-        colorBy={data => data.color}
-        colors={Object.values(primarySiteColors)}
-        data={arcData}
-        height={400}
-        identity="id"
-        margin={{
-          bottom: 20,
-          left: 20,
-          right: 20,
-          top: 20,
-        }}
-        value="size"
-        width={400}
-        />
-    </div>
-  );
-};
+  isLoading,
+}) => (
+    isEmpty(arcData)
+      ? <div>No Data</div>
+      : (
+        <DoubleDonut
+          arcData={arcData}
+          colors={Object.values(primarySiteColors)}
+          loading={isLoading}
+          />
+      )
+);
 
 export default enhance(ExploreSummaryPrimarySite);

6cc82f05798656789d9e36e9dc356095ead8ecd1
Samantha Rich <samrichca@users.noreply.github.com>
2019-09-16 09:46:31 -0400

    fix defects

--- src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/components/ClinicalHistogram.js
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/components/ClinicalHistogram.js
@@ -11,54 +11,56 @@ const ClinicalHistogram = ({
   theme,
   type,
 }) => {
-  const showXAxisLabels = histogramData.length < 20;
+  const showXAxisLabels = histogramData.length <= 20;
   return (
-  <React.Fragment>
-    <BarChart
-      data={histogramData}
-      height={CHART_HEIGHT}
-      margin={{
-        bottom: 50,
-        left: 55,
-        right: 50,
-        top: 20,
-      }}
-      showXAxisLabels={showXAxisLabels}
-      styles={{
-        bars: { fill: analysisColors[type] || theme.secondary },
-        tooltips: {
-          fill: '#fff',
-          stroke: theme.greyScale4,
-          textFill: theme.greyScale3,
-        },
-        xAxis: {
-          textFill: theme.greyScaleD3,
-        },
-        yAxis: {
-          stroke: theme.greyScale4,
-          textFill: theme.greyScale3,
-        },
-      }}
-      xAxis={{
-        style: histogramStyles(theme).axis,
-      }}
-      yAxis={{
-        style: histogramStyles(theme).axis,
-        title: `${
-          active_calculation === 'number' ? '#' : '%'
-        } of Cases`,
-      }}
-      />
-      {showXAxisLabels ||
-        <div style={{ 
-          color: theme.greyScale3, 
-          fontSize: '1.2rem',
-          marginBottom: 20, 
-          marginTop: -40,
-          textAlign: 'center',
+    <React.Fragment>
+      <BarChart
+        data={histogramData}
+        height={CHART_HEIGHT}
+        margin={{
+          bottom: 50,
+          left: 55,
+          right: 50,
+          top: 20,
         }}
+        showXAxisLabels={showXAxisLabels}
+        styles={{
+          bars: { fill: analysisColors[type] || theme.secondary },
+          tooltips: {
+            fill: '#fff',
+            stroke: theme.greyScale4,
+            textFill: theme.greyScale3,
+          },
+          xAxis: {
+            textFill: theme.greyScaleD3,
+          },
+          yAxis: {
+            stroke: theme.greyScale4,
+            textFill: theme.greyScale3,
+          },
+        }}
+        xAxis={{
+          style: histogramStyles(theme).axis,
+        }}
+        yAxis={{
+          style: histogramStyles(theme).axis,
+          title: `${
+            active_calculation === 'number' ? '#' : '%'
+          } of Cases`,
+        }}
+        />
+      {showXAxisLabels ||
+        <div
+          className="no-print"
+          style={{ 
+            color: theme.greyScale3, 
+            fontSize: '1.2rem',
+            marginBottom: 20, 
+            marginTop: -40,
+            textAlign: 'center',
+          }}
           >
-          Roll over the graph to see X axis labels.
+          Roll over the graph to see X axis labels
         </div> 
       }
     </React.Fragment>

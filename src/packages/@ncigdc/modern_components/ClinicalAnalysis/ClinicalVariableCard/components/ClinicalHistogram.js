06ac4e1e1ecdbf5788866de57d5b66a9c192339f
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2019-09-16 12:06:17 -0400

    [PRTL-2770] make x-axis font bold at clinical analysis histogram (#2701)

--- src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/components/ClinicalHistogram.js
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/components/ClinicalHistogram.js
@@ -35,7 +35,10 @@ const ClinicalHistogram = ({
       },
     }}
     xAxis={{
-      style: histogramStyles(theme).axis,
+      style: {
+        ...histogramStyles(theme).axis,
+        fontWeight: '700',
+      },
     }}
     yAxis={{
       style: histogramStyles(theme).axis,

6d033a88ea486121fd5b7351966328df50558fa4
Samantha Rich <samrichca@users.noreply.github.com>
2019-10-02 10:47:09 -0400

    make a helpers file

---
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/helpers.js
@@ -0,0 +1,59 @@
+import {
+  isEqual,
+  map,
+  mapKeys,
+  maxBy,
+  trim,
+} from 'lodash';
+import { humanify } from '@ncigdc/utils/string';
+
+export const PLOT_TYPES = {
+  categorical: ['histogram', 'survival'],
+  continuous: [
+    'histogram',
+    'survival',
+    'box',
+  ],
+};
+
+// helpers for downloading SVGs/PNGs
+
+export const OVERALL_SURVIVAL_SLUG = 'clinical-overall-survival-plot';
+export const getMaxKeyNameLength = bins => (
+  maxBy(bins, item => item.length) || ''
+).length;
+
+export const makeDownloadSlugs = displayVariables => Object.keys(displayVariables)
+.map(dVar => {
+  const fieldName = dVar.split('.')[1];
+  const chart = displayVariables[dVar].active_chart;
+  return chart === 'histogram'
+    ? `${fieldName}-bar-chart`
+    : chart === 'survival'
+      ? 'survival-plot'
+      : [`qq-plot-${fieldName}`, `boxplot-${fieldName}`];
+}).reduce((acc, curr) => acc.concat(curr), [OVERALL_SURVIVAL_SLUG]);
+
+export const makeDownloadSvgs = displayVariables => Object.keys(displayVariables)
+.reduce((acc, curr) => {
+  const fieldName = curr.split('.')[1];
+  const chart = displayVariables[curr].active_chart;
+
+  if (chart === 'histogram') {
+    const bins = Object.keys(displayVariables[curr].bins);
+    const maxKeyNameLength = getMaxKeyNameLength(bins);
+    return acc.concat({
+      bottomBuffer: maxKeyNameLength * 3,
+      rightBuffer: maxKeyNameLength * 2, 
+      selector: `#${fieldName}-chart-container .test-bar-chart svg`,
+      title: humanify({ term: fieldName }),
+    });
+  } else if (chart === 'survival') {
+    return acc.concat('survival');
+  } else if (chart === 'box') {
+    return acc.concat('box');
+  }
+}, [{
+  selector: `.${OVERALL_SURVIVAL_SLUG} .survival-plot svg`,
+  slug: OVERALL_SURVIVAL_SLUG,
+}]);

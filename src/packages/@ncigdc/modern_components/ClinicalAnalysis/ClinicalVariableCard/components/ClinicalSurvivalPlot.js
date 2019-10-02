4552a37f16cb23a45e26d248ec2b1589ab62cff0
Samantha Rich <samrichca@users.noreply.github.com>
2019-10-02 10:52:15 -0400

    add custom slug to survival plot

--- src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/components/ClinicalSurvivalPlot.js
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalVariableCard/components/ClinicalSurvivalPlot.js
@@ -3,34 +3,36 @@ import React from 'react';
 import SurvivalPlotWrapper from '@ncigdc/components/SurvivalPlotWrapper';
 
 import '../../survivalPlot.css';
-import { OVERALL_SURVIVAL_SLUG } from '../../helpers';
 
 const ClinicalSurvivalPlot = ({
   plotType,
   survivalData: { id, legend, rawData },
   survivalPlotLoading,
-}) => (
-  <div
-    style={{
-      display: 'flex',
-      flex: '0 0 auto',
-      flexDirection: 'column',
-      height: '265px',
-      justifyContent: 'center',
-      margin: '5px 2px 10px',
-    }}
-    >
-    <SurvivalPlotWrapper
-      height={202}
-      id={id}
-      legend={legend}
-      plotType={plotType}
-      rawData={rawData}
-      slug={OVERALL_SURVIVAL_SLUG}
-      survivalPlotLoading={survivalPlotLoading}
-      uniqueClass={OVERALL_SURVIVAL_SLUG}
-      />
-  </div>
-);
+}) => {
+  const survivalPlotSlug = id && `${id.split('.')[1]}-survival-plot`;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flex: '0 0 auto',
+        flexDirection: 'column',
+        height: '265px',
+        justifyContent: 'center',
+        margin: '5px 2px 10px',
+      }}
+      >
+      <SurvivalPlotWrapper
+        height={202}
+        id={id}
+        legend={legend}
+        plotType={plotType}
+        rawData={rawData}
+        slug={survivalPlotSlug}
+        survivalPlotLoading={survivalPlotLoading}
+        uniqueClass={survivalPlotSlug}
+        />
+    </div>
+  )
+};
 
 export default ClinicalSurvivalPlot;

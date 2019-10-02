74b5583080dd0f11557baaf71472dcfdf973d40d
Samantha Rich <samrichca@users.noreply.github.com>
2019-10-02 10:53:13 -0400

    move things to helpers, add overall survival slug

--- src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalAnalysisResult.js
+++ src/packages/@ncigdc/modern_components/ClinicalAnalysis/ClinicalAnalysisResult.js
@@ -24,7 +24,6 @@ import DownloadVisualizationButton from '@ncigdc/components/DownloadVisualizatio
 import CopyIcon from '@ncigdc/theme/icons/Copy';
 import Hidden from '@ncigdc/components/Hidden';
 import { visualizingButton, zDepth1 } from '@ncigdc/theme/mixins';
-import { humanify } from '@ncigdc/utils/string';
 
 import Input from '@ncigdc/uikit/Form/Input';
 import {
@@ -39,6 +38,13 @@ import tryParseJSON from '@ncigdc/utils/tryParseJSON';
 import { getDefaultCurve } from '@ncigdc/utils/survivalplot';
 import SurvivalPlotWrapper from '@ncigdc/components/SurvivalPlotWrapper';
 import DeprecatedSetResult from './DeprecatedSetResult';
+import { 
+  getMaxKeyNameLength,
+  makeDownloadSlugs,
+  makeDownloadSvgs,
+  OVERALL_SURVIVAL_SLUG,
+  PLOT_TYPES,
+} from './helpers';
 import './print.css';
 import './survivalPlot.css';
 
@@ -54,75 +60,6 @@ interface IAnalysisResultProps {
   Icon: () => React.Component<any>;
   analysis: any;
 }
-
-const plotTypes = {
-  categorical: ['histogram', 'survival'],
-  continuous: [
-    'histogram',
-    'survival',
-    'box',
-  ],
-};
-
-const makeDownloadSlugs = displayVariables => Object.keys(displayVariables)
-  .map(dVar => {
-    const fieldName = dVar.split('.')[1];
-    const chart = displayVariables[dVar].active_chart;
-    return chart === 'histogram'
-      ? `${fieldName}-bar-chart`
-      : chart === 'survival'
-        ? 'survival-plot'
-        : [`qq-plot-${fieldName}`, `boxplot-${fieldName}`];
-  }).reduce((acc, curr) => acc.concat(curr), []);
-
-const getMaxKeyNameLength = bins => (
-  maxBy(bins, item => item.length) || ''
-).length;
-
-const makeDownloadSvgs = displayVariables => Object.keys(displayVariables)
-  .reduce((acc, curr) => {
-    const fieldName = curr.split('.')[1];
-    const chart = displayVariables[curr].active_chart;
-
-    if (chart === 'histogram') {
-      const bins = Object.keys(displayVariables[curr].bins);
-      const maxKeyNameLength = getMaxKeyNameLength(bins);
-      return acc.concat({
-        bottomBuffer: maxKeyNameLength * 3,
-        rightBuffer: maxKeyNameLength * 2, 
-        selector: `#${fieldName}-chart-container .test-bar-chart svg`,
-        title: humanify({ term: fieldName }),
-      });
-    } else if (chart === 'survival') {
-      return acc.concat({
-        selector: `.survival-plot .survival-plot svg`,
-        title: '',
-        // embed: {
-        //   top: {
-        //     elements: legend
-        //       .map((l, i) => {
-        //         const legendItem = document.querySelector(
-        //           `.survivalPlot .legend-${i}`
-        //         ).cloneNode(true);
-        //         const legendTitle = legendItem.querySelector('span.print-only.inline');
-        //         if (legendTitle !== null) legendTitle.className = '';
-        //         return legendItem;
-        //       })
-        //       .concat(
-        //         pValue
-        //           ? document.querySelector(
-        //             `.${uniqueClass} .p-value`
-        //           )
-        //           : null
-        //       ),
-        //   },
-        // },
-      })
-    } else if (chart === 'box') {
-      // console.log('box');
-      return acc;
-    }
-  }, []);
 
 const CopyAnalysisModal = compose(
   setDisplayName('EnhancedCopyAnalysisModal'),
@@ -204,6 +141,7 @@ const ClinicalAnalysisResult = ({
   const downloadSlugs = makeDownloadSlugs(displayVariables);
   const downloadSvgs = makeDownloadSvgs(displayVariables);
   console.log('downloadSvgs', downloadSvgs);
+  console.log('downloadSlugs', downloadSlugs);
 
   return hits.total === 0
     ? (
@@ -378,8 +316,9 @@ const ClinicalAnalysisResult = ({
                     {...overallSurvivalData}
                     height={430}
                     plotType="clinicalOverall"
+                    slug={OVERALL_SURVIVAL_SLUG}
                     survivalPlotLoading={survivalPlotLoading}
-                    uniqueClass="clinical-survival-plot"
+                    uniqueClass={OVERALL_SURVIVAL_SLUG}
                     />
                 </div>
               </Column>
@@ -408,7 +347,7 @@ const ClinicalAnalysisResult = ({
                       id={id}
                       key={varFieldName}
                       overallSurvivalData={overallSurvivalData}
-                      plots={plotTypes[varProperties.plotTypes || 'categorical']}
+                      plots={PLOT_TYPES[varProperties.plotTypes || 'categorical']}
                       setId={setId}
                       stats={parsedFacets[varFieldName].stats}
                       style={{ minWidth: controlPanelExpanded ? 310 : 290 }}
@@ -428,7 +367,7 @@ const ClinicalAnalysisResult = ({
                       id={id}
                       key={varFieldName}
                       overallSurvivalData={overallSurvivalData}
-                      plots={plotTypes[varProperties.plotTypes || 'categorical']}
+                      plots={PLOT_TYPES[varProperties.plotTypes || 'categorical']}
                       setId={setId}
                       style={{ minWidth: controlPanelExpanded ? 310 : 290 }}
                       variable={varProperties}

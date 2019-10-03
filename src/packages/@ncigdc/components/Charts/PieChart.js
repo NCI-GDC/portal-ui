85af20704671b6ded812789feca1630980876ffd
Sam Rich <samrichca@users.noreply.github.com>
2019-10-03 10:33:25 -0400

    [PRTL-2842] repository query & top 5 legend (#2730)

--- src/packages/@ncigdc/components/Charts/PieChart.js
+++ src/packages/@ncigdc/components/Charts/PieChart.js
@@ -1,13 +1,13 @@
 // @flow
 
-import PropTypes from 'prop-types';
 import * as d3 from 'd3';
 import _ from 'lodash';
 import ReactFauxDOM from 'react-faux-dom';
-import { compose } from 'recompose';
+import { compose, pure } from 'recompose';
 
 import { withTooltip } from '@ncigdc/uikit/Tooltip';
 import { withTheme } from '@ncigdc/theme';
+import withSize from '@ncigdc/utils/withSize';
 
 const getNestedValue = (item, path) => {
   if (path.length === 1) {
@@ -20,103 +20,110 @@ const getNestedValue = (item, path) => {
   return getNestedValue(nextItem, path);
 };
 
-const PieChart = compose(
-  withTooltip,
-  withTheme
-)(
-  ({
-    data,
-    enableInnerRadius = false,
-    height = 160,
-    mappingId = 'id',
-    marginTop = 0,
-    path = 'file_count',
-    setTooltip,
-    width = 160,
-  }) => {
-    const color = d3.scaleOrdinal(d3.schemeCategory20);
-    const outerRadius = height / 2 + 10;
-    const innerRadius = enableInnerRadius ? outerRadius / 2 : 0;
-    const node = ReactFauxDOM.createElement('div');
-    node.style.setProperty('margin-top', `${marginTop}px`);
-    node.style.setProperty('display', 'flex');
-    node.style.setProperty('justify-content', 'center');
-    node.setAttribute('class', 'test-pie-chart');
+const PieChart = ({
+  data,
+  diameter = 160, // if the pie chart is responsive, this is the MAX diameter
+  enableInnerRadius = false,
+  isResponsive = false,
+  mappingId = 'id',
+  marginTop = 0,
+  path = 'file_count',
+  setTooltip,
+  size: { width: responsiveDiameter },
+}) => {
+  const dataHasColors = data.every(datum =>
+    typeof datum.color !== 'undefined' && datum.color !== '');
+  const color = d3.scaleOrdinal(d3.schemeCategory20);
+  const getSliceColor = i => (dataHasColors
+    ? data[i].color
+    : color(i));
 
-    const pie = d3.pie().value(d => getNestedValue(d, path.split('.')));
+  const pieDiameter = isResponsive && responsiveDiameter < diameter
+    ? responsiveDiameter
+    : diameter;
+  const outerRadius = pieDiameter / 2 + 10;
+  const innerRadius = enableInnerRadius ? outerRadius / 2 : 0;
 
-    const arc = d3
-      .arc()
-      .padRadius(outerRadius)
-      .innerRadius(innerRadius);
+  const node = ReactFauxDOM.createElement('div');
+  node.style.setProperty('margin-top', `${marginTop}px`);
+  node.style.setProperty('display', 'flex');
+  node.style.setProperty('justify-content', 'center');
+  node.setAttribute('class', 'test-pie-chart');
 
-    const svg = d3
-      .select(node)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .append('g')
-      .attr('transform', `translate(${width / 2}, ${height / 2})`);
+  const pie = d3.pie().value(d => getNestedValue(d, path.split('.')));
 
-    const g = svg
-      .selectAll('.arc')
-      .data(pie(data))
-      .enter()
-      .append('g')
-      .each(d => {
-        d.outerRadius = outerRadius - 20;
-      })
-      .attr('class', 'arc');
+  const arc = d3
+    .arc()
+    .padRadius(outerRadius)
+    .innerRadius(innerRadius);
 
-    const gHover = svg
-      .selectAll('.arc-hover')
-      .data(pie(data))
-      .enter()
-      .append('g')
-      .each(d => {
-        d.outerRadius = outerRadius - 15;
-      })
-      .attr('class', 'arc-hover');
+  const svg = d3
+    .select(node)
+    .append('svg')
+    .attr('width', pieDiameter)
+    .attr('height', pieDiameter)
+    .append('g')
+    .attr('transform', `translate(${pieDiameter / 2}, ${pieDiameter / 2})`);
 
-    const gPath = g.append('path');
-    const gHoverPath = gHover.append('path');
+  const g = svg
+    .selectAll('.arc')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .each(d => {
+      d.outerRadius = outerRadius - 20;
+    })
+    .attr('class', 'arc');
 
-    gPath.attr('d', arc).style('fill', (d, i) => color(i));
+  const gHover = svg
+    .selectAll('.arc-hover')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .each(d => {
+      d.outerRadius = outerRadius - 15;
+    })
+    .attr('class', 'arc-hover');
 
-    const fillHover = gHoverPath
-      .attr('d', arc)
-      .style('fill', (d, i) => color(i))
-      .style('opacity', 0);
+  const gPath = g.append('path');
+  const gHoverPath = gHover.append('path');
 
-    fillHover
-      .attr('class', d => `pointer arc-hover-${_.snakeCase(d.data[mappingId])}`)
-      .on('mouseenter', d => {
-        document.querySelector(
-          `.arc-hover-${_.snakeCase(d.data[mappingId])}`
-        ).style.opacity = 0.5;
-        setTooltip(d.data.tooltip);
-      })
-      .on('mouseleave', d => {
-        document.querySelector(
-          `.arc-hover-${_.snakeCase(d.data[mappingId])}`
-        ).style.opacity = 0;
+  gPath.attr('d', arc).style('fill', (d, i) => getSliceColor(i));
+
+  const fillHover = gHoverPath
+    .attr('d', arc)
+    .style('fill', (d, i) => getSliceColor(i))
+    .style('opacity', 0);
+
+  fillHover
+    .attr('class', d => `pointer arc-hover-${_.snakeCase(d.data[mappingId])}`)
+    .on('mouseenter', d => {
+      document.querySelector(
+        `.arc-hover-${_.snakeCase(d.data[mappingId])}`
+      ).style.opacity = 0.5;
+      setTooltip(d.data.tooltip);
+    })
+    .on('mouseleave', d => {
+      document.querySelector(
+        `.arc-hover-${_.snakeCase(d.data[mappingId])}`
+      ).style.opacity = 0;
+      setTooltip();
+    })
+    .on('mousedown', d => {
+      if (d.data.clickHandler) {
+        d.data.clickHandler(d);
         setTooltip();
-      })
-      .on('mousedown', d => {
-        if (d.data.clickHandler) {
-          d.data.clickHandler(d);
-          setTooltip();
-        }
-      });
+      }
+    });
 
-    return node.toReact();
-  }
-);
-
-PieChart.propTypes = {
-  data: PropTypes.array,
+  return node.toReact();
 };
 
 /*----------------------------------------------------------------------------*/
 
-export default PieChart;
+export default compose(
+  withTheme,
+  withTooltip,
+  withSize({ refreshRate: 16 }),
+  pure,
+)(PieChart);

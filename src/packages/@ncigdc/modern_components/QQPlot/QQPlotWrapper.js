eb096951a753cc0017a739b5fc3c390a055a7665
Sam Rich <samrichca@users.noreply.github.com>
2019-11-22 12:24:25 -0500

    alphabetize

--- src/packages/@ncigdc/modern_components/QQPlot/QQPlotWrapper.js
+++ src/packages/@ncigdc/modern_components/QQPlot/QQPlotWrapper.js
@@ -16,10 +16,10 @@ export default compose(
   withProps(() => ({
     styles: { 
       margin: {
-        top: 0,
         bottom: 0,
-        right: 0,
         left: 0,
+        right: 0,
+        top: 0,
       }
     }
   })),

774359e18e70ecc83fe6b018492c23704b2e3612
Henry Zhao <yuanheng.zhao@gmail.com>
2019-12-02 11:00:37 -0500

    Fix Human Body Image Chart X-Axis overlapping

--- src/packages/@ncigdc/modern_components/HumanBody/HumanBody.js
+++ src/packages/@ncigdc/modern_components/HumanBody/HumanBody.js
@@ -11,7 +11,7 @@ import {
 import { connect } from 'react-redux';
 import sapien from '@oncojs/sapien';
 import { withTooltip } from '@ncigdc/uikit/Tooltip';
-import { Column } from '@ncigdc/uikit/Flex';
+import { Column, Row } from '@ncigdc/uikit/Flex';
 import withRouter from '@ncigdc/utils/withRouter';
 import { makeFilter } from '@ncigdc/utils/filters';
 import { stringifyJSONParam } from '@ncigdc/utils/uri';
@@ -21,7 +21,12 @@ import {
   HUMAN_BODY_SITES_MAP,
   HUMAN_BODY_ALL_ALLOWED_SITES,
 } from '@ncigdc/utils/constants';
-import { groupBy, map } from 'lodash';
+import {
+  groupBy,
+  map,
+  maxBy,
+  floor,
+} from 'lodash';
 
 const containerStyle = {
   flex: 1,
@@ -66,23 +71,22 @@ export default compose(
   })),
   lifecycle({
     async componentDidMount(): Promise<*> {
-      const { setTooltip, push, groupedData } = this.props;
+      const { groupedData, push, setTooltip } = this.props;
+      // groupedData[0].docCount = 120000;
+      let factor;
+      // factor = floor(maxBy(groupedData, 'docCount').docCount * 21 / 24, -2) / 5;
 
-      const data = groupedData
-        .map(({ key, docCount, fileCount, allPrimarySites }) => ({
-          _key: key,
-          _count: docCount,
-          fileCount,
-          allPrimarySites,
-        }))
-        .sort((a, b) => (a._key > b._key ? 1 : -1));
+      const data = groupedData.map(d => ({
+        ...d,
+        docCount: d.docCount / 1000,
+      })).sort((a, b) => (a.key > b.key ? 1 : -1));
 
       setTimeout(() => {
         const highlights = document.querySelectorAll(
           '#human-body-highlights > svg',
         );
         for (const h of highlights) {
-          if (!data.some(d => d._key === h.id.replace(/-/g, ' '))) {
+          if (!data.some(d => d.key === h.id.replace(/-/g, ' '))) {
             h.style.display = 'none';
           }
         }
@@ -92,8 +96,8 @@ export default compose(
         sapien({
           title: 'Cases by Major Primary Site',
           clickHandler: d => {
-            const key = d._key.replace(/-/g, ' ');
-            const datum = data.find(x => x._key === key);
+            const key = d.key.replace(/-/g, ' ');
+            const datum = data.find(x => x.key === key);
             if (datum) {
               const query = {
                 filters: stringifyJSONParam(
@@ -105,7 +109,10 @@ export default compose(
                   ]),
                 ),
               };
-              push({ pathname: '/exploration', query });
+              push({
+                pathname: '/exploration',
+                query,
+              });
             } else {
               setTooltip();
             }
@@ -113,12 +120,26 @@ export default compose(
           mouseOverHandler: d => {
             setTooltip(
               <span>
-                <div style={{ fontSize: '16px', color: '#bb0e3d' }}>
-                  {d._key}
+                <div
+                  style={{
+                    fontSize: '16px',
+                    color: '#bb0e3d',
+                  }}
+                  >
+                  {d.key}
                 </div>
-                <div style={{ fontSize: '12px', color: 'rgb(20, 20, 20)' }}>
-                  {d._count.toLocaleString()} cases (
-                  {d.fileCount.toLocaleString()} files)
+                <div
+                  style={{
+                    fontSize: '12px',
+                    color: 'rgb(20, 20, 20)',
+                  }}
+                  >
+                  {(d.docCount * 1000).toLocaleString()}
+                  {' '}
+                  cases (
+                  {d.fileCount.toLocaleString()}
+                  {' '}
+                  files)
                 </div>
               </span>,
             );
@@ -128,11 +149,11 @@ export default compose(
           selector: '#human-body-root',
           width: 380,
           height: 435,
-          tickInterval: 1000,
+          tickInterval: 1,
           offsetLeft: root.offsetLeft,
           offsetTop: root.offsetTop,
-          primarySiteKey: '_key',
-          caseCountKey: '_count',
+          primarySiteKey: 'key',
+          caseCountKey: 'docCount',
           fileCountKey: 'fileCount',
         });
       });
@@ -141,5 +162,17 @@ export default compose(
 )(() => (
   <Column className="test-home">
     <InsideContainer id="human-body-root" />
+    <div
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 650,
+        top: 465,
+        color: 'black',
+        fontSize: 12,
+      }}
+      >
+      (in 1000s)
+    </div>
   </Column>
 ));

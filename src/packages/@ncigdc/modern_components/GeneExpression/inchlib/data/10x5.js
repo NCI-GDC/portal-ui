55f34acce06ce371ad6ae0b47f6b19cea12065a7
Sam Rich <samrichca@users.noreply.github.com>
2019-12-20 10:22:17 -0500

    update stub data, add 2000x500 data

--- src/packages/@ncigdc/modern_components/GeneExpression/inchlib/data/10x5.js
+++ src/packages/@ncigdc/modern_components/GeneExpression/inchlib/data/10x5.js
@@ -1,349 +1,385 @@
-// created with inchlib_clust on the command line
-
 const data = {
-  column_metadata: {
-    features: [
-      [
-        'female',
-        'female',
-        'female',
-        'female',
-        'female',
-        'female',
-        'female',
-        'female',
-        'female',
-        'female',
-      ],
-      [
-        'native_hawaiian_or_other_pacific_islander',
-        'not_reported',
-        'american_indian_or_alaska_native',
-        'not_reported',
-        'american_indian_or_alaska_native',
-        'native_hawaiian_or_other_pacific_islander',
-        'black_or_african_american',
-        'american_indian_or_alaska_native',
-        'american_indian_or_alaska_native',
-        'american_indian_or_alaska_native',
-      ],
-      [
-        'hispanic_or_latino',
-        'hispanic_or_latino',
-        'hispanic_or_latino',
-        'not_hispanic_or_latino',
-        'hispanic_or_latino',
-        'hispanic_or_latino',
-        'hispanic_or_latino',
-        'not_hispanic_or_latino',
-        'not_hispanic_or_latino',
-        'hispanic_or_latino',
-      ],
-      [
-        '85',
-        '60',
-        '80',
-        '75',
-        '85',
-        '75',
-        '75',
-        '65',
-        '80',
-        '70',
-      ],
-      [
-        'alive',
-        'alive',
-        'alive',
-        'alive',
-        'alive',
-        'alive',
-        'alive',
-        'alive',
-        'alive',
-        'alive',
-      ],
-      [
-        '1000',
-        '2000',
-        '0',
-        '1500',
-        '0',
-        '3000',
-        '1500',
-        '2000',
-        '1000',
-        '2500',
-      ],
-    ],
-    feature_names: [
-      'Gender',
-      'Race',
-      'Ethnicity',
-      'Age at Diagnosis',
-      'Vital Status',
-      'Days to Death',
-    ],
-  },
-  metadata: {
-    nodes: {
-      0: ['ENSG00000163539', 'CLASP2'],
-      1: ['ENSG00000188167', 'TMPPE'],
-      2: ['ENSG00000163539', 'CLASP2'],
-      3: ['ENSG00000173705', 'SUSD5'],
-      4: ['ENSG00000163539', 'CLASP2'],
-    },
-    feature_names: ['gene_ensembl', 'gene_symbol'],
-  },
-  data: {
-    nodes: {
-      0: {
-        count: 1,
-        distance: 0,
-        objects: ['1'],
-        features: [
-          0.891765966,
-          1.520964521,
-          0.87335077,
-          0.912801252,
-          0.756931363,
-          1.630903247,
-          0.469534344,
-          3.024852528,
-          3.756673529,
-          4.214124519,
-        ],
-        parent: 7,
-      },
-      1: {
-        count: 1,
-        distance: 0,
-        objects: ['2'],
-        features: [
-          0.833596105,
-          1.922797235,
-          1.511470976,
-          3.293583292,
-          3.901656775,
-          3.791245628,
-          3.74818425,
-          3.042025518,
-          3.506343323,
-          0.728504579,
-        ],
-        parent: 6,
-      },
-      2: {
-        count: 1,
-        distance: 0,
-        objects: ['3'],
-        features: [
-          6.737533679,
-          6.752444135,
-          5.731165098,
-          6.381841943,
-          6.603843283,
-          6.24237503,
-          6.267921376,
-          6.766716194,
-          6.232230099,
-          6.471666261,
-        ],
-        parent: 5,
-      },
-      3: {
-        count: 1,
-        distance: 0,
-        objects: ['4'],
-        features: [
-          4.112896548,
-          4.246957042,
-          3.454459512,
-          4.0834809,
-          4.246900529,
-          4.314367357,
-          3.926651073,
-          4.586471745,
-          4.316788857,
-          4.680016384,
-        ],
-        parent: 6,
-      },
-      4: {
-        count: 1,
-        distance: 0,
-        objects: ['5'],
-        features: [
-          6.498741724,
-          6.199710451,
-          7.666878596,
-          6.621190304,
-          6.519565266,
-          6.61969068,
-          7.532756366,
-          6.627534392,
-          7.174262606,
-          6.692850735,
-        ],
-        parent: 5,
-      },
-      5: {
-        count: 2,
-        distance: 2.621,
-        left_child: 2,
-        parent: 8,
-        right_child: 4,
-      },
-      6: {
-        count: 2,
-        distance: 6.296,
-        left_child: 1,
-        parent: 7,
-        right_child: 3,
-      },
-      7: {
-        count: 3,
-        distance: 7.864,
-        left_child: 0,
-        parent: 8,
-        right_child: 6,
-      },
-      8: {
-        count: 5,
-        distance: 18.567,
-        left_child: 5,
-        right_child: 7,
-      },
-    },
-    feature_names: [
-      'TCGA-B5-A3FC_ec2040a3-788b-4f2c-a093-c5c4b3dc4b6c',
-      'TCGA-A5-A0G2_4abbd258-0f0c-4428-901d-625d47ad363a',
-      'TCGA-AX-A2HC_53e8e07e-18f9-4c9b-9783-a5b67ceddcf7',
-      'TCGA-AX-A1CE_4db38349-28d2-4af5-a12f-3d861937b0e0',
-      'TCGA-AX-A2HC_53e8e07e-18f9-4c9b-9783-a5b67ceddcf7',
-      'TCGA-EO-A22R_508bf716-46f9-44dc-8e39-09261dfb073e',
-      'TCGA-IB-7651_3559f697-8fec-4f36-962c-632272fe9c9c',
-      'TCGA-EO-A22R_508bf716-46f9-44dc-8e39-09261dfb073e',
-      'TCGA-AX-A2HC_53e8e07e-18f9-4c9b-9783-a5b67ceddcf7',
-      'TCGA-B5-A3FC_ec2040a3-788b-4f2c-a093-c5c4b3dc4b6c',
-    ],
-  },
-  column_dendrogram: {
-    nodes: {
-      0: {
-        count: 1,
-        distance: 0,
-        parent: 12,
-      },
-      1: {
-        count: 1,
-        distance: 0,
-        parent: 13,
-      },
-      2: {
-        count: 1,
-        distance: 0,
-        parent: 10,
-      },
-      3: {
-        count: 1,
-        distance: 0,
-        parent: 15,
-      },
-      4: {
-        count: 1,
-        distance: 0,
-        parent: 11,
-      },
-      5: {
-        count: 1,
-        distance: 0,
-        parent: 16,
-      },
-      6: {
-        count: 1,
-        distance: 0,
-        parent: 12,
-      },
-      7: {
-        count: 1,
-        distance: 0,
-        parent: 10,
-      },
-      8: {
-        count: 1,
-        distance: 0,
-        parent: 13,
-      },
-      9: {
-        count: 1,
-        distance: 0,
-        parent: 14,
-      },
-      10: {
-        count: 2,
-        distance: 0.693,
-        left_child: 2,
-        parent: 11,
-        right_child: 7,
-      },
-      11: {
-        count: 3,
-        distance: 1.006,
-        left_child: 4,
-        parent: 14,
-        right_child: 10,
-      },
-      12: {
-        count: 2,
-        distance: 1.187,
-        left_child: 0,
-        parent: 16,
-        right_child: 6,
-      },
-      13: {
-        count: 2,
-        distance: 1.3,
-        left_child: 1,
-        parent: 15,
-        right_child: 8,
-      },
-      14: {
-        count: 4,
-        distance: 1.451,
-        left_child: 9,
-        parent: 17,
-        right_child: 11,
-      },
-      15: {
-        count: 3,
-        distance: 2.135,
-        left_child: 3,
-        parent: 17,
-        right_child: 13,
-      },
-      16: {
-        count: 3,
-        distance: 3.11,
-        left_child: 5,
-        parent: 18,
-        right_child: 12,
-      },
-      17: {
-        count: 7,
-        distance: 4.214,
-        left_child: 14,
-        parent: 18,
-        right_child: 15,
-      },
-      18: {
-        count: 10,
-        distance: 5.567,
-        left_child: 16,
-        right_child: 17,
-      },
-    },
-  },
+  "column_metadata": {
+      "features": [
+          [
+              "female", 
+              "female", 
+              "female", 
+              "male", 
+              "female"
+          ], 
+          [
+              "native_hawaiian_or_other_pacific_islander", 
+              "not_reported", 
+              "not_reported", 
+              "american_indian_or_alaska_native", 
+              "white"
+          ], 
+          [
+              "hispanic_or_latino", 
+              "hispanic_or_latino", 
+              "not_hispanic_or_latino", 
+              "not_reported", 
+              "not_reported"
+          ], 
+          [
+              "70", 
+              "70", 
+              "70", 
+              "70", 
+              "65"
+          ], 
+          [
+              "dead", 
+              "dead", 
+              "dead", 
+              "alive", 
+              "dead"
+          ], 
+          [
+              "1500", 
+              "3000", 
+              "1000", 
+              "1000", 
+              "2000"
+          ]
+      ], 
+      "feature_names": [
+          "Gender", 
+          "Race", 
+          "Ethnicity", 
+          "Age at Diagnosis", 
+          "Vital Status", 
+          "Days to Death"
+      ]
+  }, 
+  "metadata": {
+      "nodes": {
+          "0": [
+              "ENSG00000163539", 
+              "CLASP2"
+          ], 
+          "1": [
+              "ENSG00000170275", 
+              "CRTAP"
+          ], 
+          "2": [
+              "ENSG00000153560", 
+              "UBP1"
+          ], 
+          "3": [
+              "ENSG00000173705", 
+              "SUSD5"
+          ], 
+          "4": [
+              "ENSG00000188167", 
+              "TMPPE"
+          ], 
+          "5": [
+              "ENSG00000170266", 
+              "GLB1"
+          ], 
+          "6": [
+              "ENSG00000153558", 
+              "FBXL2"
+          ], 
+          "7": [
+              "ENSG00000206557", 
+              "TRIM71"
+          ], 
+          "8": [
+              "ENSG00000153560", 
+              "UBP1"
+          ], 
+          "9": [
+              "ENSG00000162510", 
+              "MATN1"
+          ]
+      }, 
+      "feature_names": [
+          "gene_ensembl", 
+          "gene_symbol"
+      ]
+  }, 
+  "data": {
+      "nodes": {
+          "0": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "1"
+              ], 
+              "features": [
+                  1.630903247, 
+                  0.756931363, 
+                  1.520964521, 
+                  3.756673529, 
+                  0.87335077
+              ], 
+              "parent": 14
+          }, 
+          "1": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "2"
+              ], 
+              "features": [
+                  3.791245628, 
+                  3.901656775, 
+                  1.922797235, 
+                  3.506343323, 
+                  1.511470976
+              ], 
+              "parent": 15
+          }, 
+          "2": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "3"
+              ], 
+              "features": [
+                  6.24237503, 
+                  6.603843283, 
+                  6.752444135, 
+                  6.232230099, 
+                  5.731165098
+              ], 
+              "parent": 10
+          }, 
+          "3": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "4"
+              ], 
+              "features": [
+                  4.314367357, 
+                  4.246900529, 
+                  4.246957042, 
+                  4.316788857, 
+                  3.454459512
+              ], 
+              "parent": 12
+          }, 
+          "4": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "5"
+              ], 
+              "features": [
+                  6.61969068, 
+                  6.519565266, 
+                  6.199710451, 
+                  7.174262606, 
+                  7.666878596
+              ], 
+              "parent": 11
+          }, 
+          "5": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "6"
+              ], 
+              "features": [
+                  3.290051877, 
+                  1.406435381, 
+                  2.315182899, 
+                  2.328373308, 
+                  4.310545394
+              ], 
+              "parent": 15
+          }, 
+          "6": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "7"
+              ], 
+              "features": [
+                  4.185199899, 
+                  3.751393093, 
+                  4.655490821, 
+                  4.742380331, 
+                  4.126962277
+              ], 
+              "parent": 12
+          }, 
+          "7": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "8"
+              ], 
+              "features": [
+                  6.283817206, 
+                  6.230066057, 
+                  6.150283333, 
+                  6.332635401, 
+                  6.103919076
+              ], 
+              "parent": 10
+          }, 
+          "8": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "9"
+              ], 
+              "features": [
+                  6.577745875, 
+                  6.490210048, 
+                  6.412696068, 
+                  6.983515911, 
+                  6.742395297
+              ], 
+              "parent": 11
+          }, 
+          "9": {
+              "count": 1, 
+              "distance": 0, 
+              "objects": [
+                  "10"
+              ], 
+              "features": [
+                  0.522948869, 
+                  0.578001818, 
+                  0.211652291, 
+                  0.333735282, 
+                  0.64297053
+              ], 
+              "parent": 14
+          }, 
+          "10": {
+              "count": 2, 
+              "distance": 0.808, 
+              "left_child": 2, 
+              "parent": 13, 
+              "right_child": 7
+          }, 
+          "11": {
+              "count": 2, 
+              "distance": 0.969, 
+              "left_child": 4, 
+              "parent": 13, 
+              "right_child": 8
+          }, 
+          "12": {
+              "count": 2, 
+              "distance": 1.031, 
+              "left_child": 3, 
+              "parent": 16, 
+              "right_child": 6
+          }, 
+          "13": {
+              "count": 4, 
+              "distance": 2.206, 
+              "left_child": 10, 
+              "parent": 18, 
+              "right_child": 11
+          }, 
+          "14": {
+              "count": 2, 
+              "distance": 3.84, 
+              "left_child": 0, 
+              "parent": 17, 
+              "right_child": 9
+          }, 
+          "15": {
+              "count": 2, 
+              "distance": 3.982, 
+              "left_child": 1, 
+              "parent": 16, 
+              "right_child": 5
+          }, 
+          "16": {
+              "count": 4, 
+              "distance": 4.717, 
+              "left_child": 12, 
+              "parent": 17, 
+              "right_child": 15
+          }, 
+          "17": {
+              "count": 6, 
+              "distance": 9.004, 
+              "left_child": 14, 
+              "parent": 18, 
+              "right_child": 16
+          }, 
+          "18": {
+              "count": 10, 
+              "distance": 18.644, 
+              "left_child": 13, 
+              "right_child": 17
+          }
+      }, 
+      "feature_names": [
+          "TCGA-2W-A8YY_5aeac31a-176a-4f93-a376-a93a670821bb", 
+          "TCGA-E6-A1LX_435b57e7-5cd4-486a-b3d4-ccb1ef160d52", 
+          "TCGA-B5-A3FC_ec2040a3-788b-4f2c-a093-c5c4b3dc4b6c", 
+          "TCGA-EO-A22U_30bc72d5-07b5-48d2-b025-bba9bcf2f09f", 
+          "TCGA-EO-A22R_508bf716-46f9-44dc-8e39-09261dfb073e"
+      ]
+  }, 
+  "column_dendrogram": {
+      "nodes": {
+          "0": {
+              "count": 1, 
+              "distance": 0, 
+              "parent": 7
+          }, 
+          "1": {
+              "count": 1, 
+              "distance": 0, 
+              "parent": 6
+          }, 
+          "2": {
+              "count": 1, 
+              "distance": 0, 
+              "parent": 5
+          }, 
+          "3": {
+              "count": 1, 
+              "distance": 0, 
+              "parent": 8
+          }, 
+          "4": {
+              "count": 1, 
+              "distance": 0, 
+              "parent": 5
+          }, 
+          "5": {
+              "count": 2, 
+              "distance": 2.161, 
+              "left_child": 2, 
+              "parent": 6, 
+              "right_child": 4
+          }, 
+          "6": {
+              "count": 3, 
+              "distance": 2.495, 
+              "left_child": 1, 
+              "parent": 7, 
+              "right_child": 5
+          }, 
+          "7": {
+              "count": 4, 
+              "distance": 3.308, 
+              "left_child": 0, 
+              "parent": 8, 
+              "right_child": 6
+          }, 
+          "8": {
+              "count": 5, 
+              "distance": 4.107, 
+              "left_child": 3, 
+              "right_child": 7
+          }
+      }
+  }
 };
 
 export default data;
--- src/packages/@ncigdc/modern_components/GeneExpression/inchlib/data/index.js
+++ src/packages/@ncigdc/modern_components/GeneExpression/inchlib/data/index.js
@@ -1,13 +1,13 @@
-import data3x2 from './3x2';
-import data10x5 from './10x5';
-import data100x100 from './100x100';
-// import data2000x500 from './2000x500';
+// import data3x2 from './3x2';
+// import data10x5 from './10x5';
+// import data100x100 from './100x100';
+import data2000x500 from './2000x500';
 
 const data = {
-  data3x2,
-  data10x5,
-  data100x100,
-  // data2000x500,
+  // data3x2,
+  // data10x5,
+  // data100x100,
+  data2000x500,
 };
 
 export default data;

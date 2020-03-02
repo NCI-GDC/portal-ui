bdf5474ab384f3ea42fc71269bd07bcb68ffd310
Sam Rich <samrichca@users.noreply.github.com>
2020-03-02 15:42:08 -0500

    Update src/packages/@ncigdc/modern_components/SCRNASeq/SCRNASeq.js
    Co-Authored-By: Anders Richardsson <2107110+caravinci@users.noreply.github.com>

--- src/packages/@ncigdc/modern_components/SCRNASeq/SCRNASeq.js
+++ src/packages/@ncigdc/modern_components/SCRNASeq/SCRNASeq.js
@@ -53,7 +53,7 @@ class SCRNASeq extends Component {
   };
 
   render() {
-    const { data, dataType } = this.state;
+    const { data = [], dataType = ''} = this.state;
 
     return (
       <Column style={{ marginBottom: '1rem' }}>
@@ -69,7 +69,7 @@ class SCRNASeq extends Component {
             }}
             >
             <h1 style={{ margin: '0 0 20px' }}>SCRNA-Seq</h1>
-            {showDataButtons && (
+            {showDataButtons && dataTypes.length > 0 (
               // for viz demo
               <Row>
                 {dataTypes.map(dType => (
@@ -83,7 +83,7 @@ class SCRNASeq extends Component {
                 ))}
               </Row>
             )}
-            {data && (
+            {data.length > 0 && (
               <SCRNASeqChart
                 data={data}
                 dataType={dataType}

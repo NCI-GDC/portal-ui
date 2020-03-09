ad4a0b63e5a46d685e6c3a7dfe2ca8751b22b130
Sam Rich <samrichca@users.noreply.github.com>
2020-03-09 15:15:29 -0400

    add PCA data

--- src/packages/@ncigdc/modern_components/SCRNASeq/data/index.js
+++ src/packages/@ncigdc/modern_components/SCRNASeq/data/index.js
@@ -2,12 +2,14 @@ import tsne from './tsne';
 // import tsne3d from './tsne3d';
 import umap from './umap';
 // import umap3d from './umap3d';
+import pca from './pca';
 
 const data = {
   tsne,
   // tsne3d,
   umap,
   // umap3d,
+  pca,
 };
 
 export default data;

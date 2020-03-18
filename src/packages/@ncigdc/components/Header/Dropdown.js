00c5a4c7ae27b520c5a5b9e9e573046a1d26447e
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2020-03-18 09:59:57 -0400

    [PRTL-3037] add exploration dropdown for DAVE-CA (#2831)

---
+++ src/packages/@ncigdc/components/Header/Dropdown.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Dropdown from '@ncigdc/uikit/Dropdown';
+import DropdownItem from '@ncigdc/uikit/DropdownItem';
+import styled from '@ncigdc/theme/styled';
+import Link from '@ncigdc/components/Links/Link';
+
+const iconStyle = {
+  fontSize: '1.65rem',
+  marginRight: '0.5rem',
+};
+
+const DropdownItemStyled = styled(DropdownItem, {
+  cursor: 'pointer',
+});
+
+const HeaderDropdown = ({
+  activeStyle = {},
+  children: button,
+  items,
+  onClick: baseOnClick,
+  pathname: basePath = '',
+  query: baseQuery = {},
+  ...linkProps
+}) => (
+  <Dropdown
+    activeStyle={activeStyle}
+    basePath={basePath}
+    button={button}
+    style={{ position: 'initial' }}
+    >
+    {items.map(({
+      description = '',
+      icon: IconComponent = null,
+      onClick = () => {},
+      pathname = '',
+      query = {},
+    }) => (
+      <DropdownItemStyled
+        key={description}
+        >
+        <Link
+          {...linkProps}
+          onClick={event => {
+            baseOnClick(event);
+            onClick(event);
+          }}
+          pathname={basePath + pathname}
+          query={{
+            ...baseQuery,
+            ...query,
+          }}
+          testTag={description.split(' ').join('')}
+          >
+          {IconComponent && (
+            <IconComponent
+              style={{
+                ...iconStyle,
+                fontSize: '1.8rem',
+                marginRight: '0.6rem',
+              }}
+              />
+          )}
+
+          <span>{description}</span>
+        </Link>
+      </DropdownItemStyled>
+    ))}
+  </Dropdown>
+);
+
+export default HeaderDropdown;

f4b4d0a68fb0a805b36429e5eec703b673d1c5b1
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2020-05-13 08:39:50 -0400

    [PRTL-3092] add Study param to Cases results table

--- src/packages/@ncigdc/modern_components/DownloadClinicalDropdown/DownloadClinicalDropdown.relay.js
+++ src/packages/@ncigdc/modern_components/DownloadClinicalDropdown/DownloadClinicalDropdown.relay.js
@@ -17,17 +17,17 @@ export default (Component: ReactClass<*>) =>
         const downloadFilters =
           selectedIds && selectedIds.length
             ? addInFilters(
-                ...filters,
-                makeFilter(
-                  [
-                    {
-                      field: 'cases.case_id',
-                      value: selectedIds,
-                    },
-                  ],
-                  false,
-                ),
-              )
+              ...filters,
+              makeFilter(
+                [
+                  {
+                    field: 'cases.case_id',
+                    value: selectedIds,
+                  },
+                ],
+                false,
+              ),
+            )
             : filters;
         return {
           filters: downloadFilters,
@@ -72,11 +72,12 @@ export default (Component: ReactClass<*>) =>
           `;
     return (
       <Query
-        parentProps={props}
-        variables={props.variables}
+        cacheConfig={{ requiresStudy: props.scope === 'explore' }}
         Component={Component}
-        style={{ width: 'auto' }}
+        parentProps={props}
         query={caseQuery}
-      />
+        style={{ width: 'auto' }}
+        variables={props.variables}
+        />
     );
   });

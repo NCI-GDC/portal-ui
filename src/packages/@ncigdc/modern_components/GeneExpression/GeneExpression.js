f4fda776e3631e4cea46d3382e28992db13f551a
Sam Rich <samrichca@users.noreply.github.com>
2020-07-03 17:33:57 -0400

    [prtl-3154] testing performance of inchlib

--- src/packages/@ncigdc/modern_components/GeneExpression/GeneExpression.js
+++ src/packages/@ncigdc/modern_components/GeneExpression/GeneExpression.js
@@ -30,7 +30,7 @@ const enhance = compose(
 
 class GeneExpression extends Component {
   state = {
-    data: dataObj.data50x50, // for viz demo
+    data: dataObj.datax, // for viz demo
   };
 
   handleClickInchlibLink = (

7f8ad9b22149c388ea720f72302317e9edb4edb8
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2020-08-04 10:49:58 -0400

    [PRTL-3173] update scrna stub path

--- src/packages/@ncigdc/modern_components/SCRNASeq/SCRNASeq.relay.js
+++ src/packages/@ncigdc/modern_components/SCRNASeq/SCRNASeq.relay.js
@@ -62,7 +62,7 @@ export default (Component: ReactClass<*>) =>
       }) => async () => {
         // const file = await import('./stubData/seurat.tsv');
         // const { body: stream } = await fetch(`http://localhost:3000${file}`);
-        const { body: stream } = await fetch('https://raw.githubusercontent.com/NCI-GDC/portal-ui/PRTL-3173/src/packages/%40ncigdc/modern_components/SCRNASeq/stubData/seurat.tsv');
+        const { body: stream } = await fetch('https://gist.githubusercontent.com/caravinci/e538391f6681348446af21127d30e4e7/raw/e3c8569f3b8fa50a00823486ccd7a0e3c723bee1/seurat.tsv');
 
         const tsvToJSON = wholeXSVtoJSON(
           'EnhancedSCRNASeq.relay',

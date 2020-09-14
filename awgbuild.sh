fa25df675c8582be604bb2ecf91e46c65d25a1ef
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2020-09-14 12:06:50 -0400

    [PRTL-3195] add AWG token refreshing capabilities

--- awgbuild.sh
+++ awgbuild.sh
@@ -5,10 +5,10 @@ export REACT_APP_SLIDE_IMAGE_ENDPOINT="/auth/api/v0/tile/"
 export REACT_APP_COMMIT_HASH=`git rev-parse --short HEAD`
 export REACT_APP_COMMIT_TAG=`git tag -l --points-at HEAD`
 export REACT_APP_API="https://portal.awg.gdc.cancer.gov/auth/api"
+export REACT_APP_FENCE="https://portal.awg.gdc.cancer.gov/fence/"
 export REACT_APP_GDC_AUTH="https://portal.awg.gdc.cancer.gov/auth/"
-export REACT_APP_FENCE="https://login.awg.gdc.cancer.gov"
+# export AWG_TOKEN_EXPIRY=5
 export GDC_BASE="/"
-export REACT_APP_GDC_AUTH_API="https://portal.awg.gdc.cancer.gov/auth/api"
 export REACT_APP_AWG=true
 export REACT_APP_IS_AUTH_PORTAL=true
 npm run build

18cf8d03c5715f77291a5588e165a132f5e790e5
Salman Sikandar <salmansikandar@Salmans-MBP.attlocal.net.cdis>
2020-09-14 16:15:24 +0500

    Cleaning up docker resources after pushing

--- docker-build-jenkins.sh
+++ docker-build-jenkins.sh
@@ -1,7 +1,9 @@
 echo "Build and push PORTAL docker image."
 docker build -t ${REGISTRY}/portal-ui:${GIT_TAG} -f portal.Dockerfile . --build-arg http_proxy=http://cloud-proxy:3128 --build-arg https_proxy=http://cloud-proxy:3128
 docker push ${REGISTRY}/portal-ui:${GIT_TAG}
+docker rmi ${REGISTRY}/portal-ui:${GIT_TAG}
 
 echo "Build and push AWGPORTAL docker image."
 docker build -t ${REGISTRY}/awgportal:${GIT_TAG} -f awgportal.Dockerfile . --build-arg http_proxy=http://cloud-proxy:3128 --build-arg https_proxy=http://cloud-proxy:3128
 docker push ${REGISTRY}/awgportal:${GIT_TAG}
+docker rmi ${REGISTRY}/awgportal:${GIT_TAG}

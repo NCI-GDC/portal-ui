491d4c9788151e6bcda1aea8ec08c53f03aaa130
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2020-09-29 12:07:19 -0400

    [PRTL-3233] improve GeneEx loading spinner (#2925)

--- src/packages/@ncigdc/modern_components/GeneExpression/inchlib/index.js
+++ src/packages/@ncigdc/modern_components/GeneExpression/inchlib/index.js
@@ -3915,23 +3915,30 @@ import { getLowerAgeYears } from '@ncigdc/utils/ageDisplay';
     */
   InCHlib.prototype.init = function () {
     const self = this;
-    // setTimeout is used to force synchronicity in canvas
-    const loading_div = $('<div style="width: 300px; height: 300px; display: flex; align-items: center; justify-content: center;"></div>').html('<i class="fa fa-spinner fa-pulse" style="font-size: 32px"></i>');
-    self.$element.after(loading_div);
-    self.$element.hide();
-
-    setTimeout(function () {
+    if (self.extHandlers.handleLoading) {
       self.read_data(self.options.data);
       self.draw();
-    }, 50);
+      // Future implementation: passing a string here could update the message in the loader. "Loading heatmap"
+      self.extHandlers.handleLoading(false);
+    } else {
+      // setTimeout is used to force synchronicity in canvas
+      const loading_div = $('<div style="width: 300px; height: 300px; display: flex; align-items: center; justify-content: center;"></div>').html('<i class="fa fa-spinner fa-pulse" style="font-size: 32px"></i>');
+      self.$element.after(loading_div);
+      self.$element.hide();
 
-    setTimeout(function () {
-      loading_div.fadeOut().remove();
-      self.$element.show();
-    }, 50);
+      setTimeout(function () {
+        self.read_data(self.options.data);
+        self.draw();
+      }, 50);
+
+      setTimeout(function () {
+        loading_div.fadeOut().remove();
+        self.$element.show();
+      }, 50);
+    }
   };
 
-  $.fn[plugin_name] = function (options, extHandlers) {
+  $.fn[plugin_name] = function (options = {}, extHandlers = {}) {
     // note: this plugin only supports ONE instance
     return this.each(function () {
       if ($.data(this, 'plugin_' + plugin_name)) {

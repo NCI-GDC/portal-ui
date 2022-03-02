73a7abfb427f50c8358b511857621fa2b2de337f
Michael Solano <96085518+mpsolano@users.noreply.github.com>
2022-03-02 15:28:52 -0600

    DEV-1042: Update links to point to ensembl release 102 (nov 2020 archive)

--- src/packages/@ncigdc/utils/externalReferenceLinks.js
+++ src/packages/@ncigdc/utils/externalReferenceLinks.js
@@ -15,10 +15,10 @@ export default {
   cosm: id => `http://cancer.sanger.ac.uk/cosmic/mutation/overview?id=${id}`,
   cosn: id => `http://cancer.sanger.ac.uk/cosmic/ncv/overview?id=${id}`,
   dbsnp: id => `https://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=${id}`,
-  ensembl: id => `http://may2015.archive.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=${id}`,
+  ensembl: id => `http://nov2020.archive.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=${id}`,
   entrez_gene: id => `http://www.ncbi.nlm.nih.gov/gene/${id}`,
   hgnc: id => `https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/${id}`,
   omim_gene: id => `http://omim.org/entry/${id}`,
-  transcript: id => `http://feb2014.archive.ensembl.org/Homo_sapiens/Transcript/Summary?db=core;t=${id}`,
+  transcript: id => `http://nov2020.archive.ensembl.org/Homo_sapiens/Transcript/Summary?db=core;t=${id}`,
   uniprotkb_swissprot: id => `http://www.uniprot.org/uniprot/${id}`,
 };

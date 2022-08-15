aba4d7c15d3e664aff22d9e2752e68f4252cfc0f
Tejas <hegdetejass@gmail.com>
2022-08-15 11:48:31 -0500

    removed rum init from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 
-import '@datadog/browser-rum/bundle/datadog-rum'
-
-window.DD_RUM.init({
-  applicationId: 'c94f127b-dd40-44b7-9c28-17632610a9c6',
-  clientToken: 'pub20202bcc06c0249365fd3445a71f7004',
-  site: 'datadoghq.com',
-  sampleRate: 100,
-  premiumSampleRate: 100,
-})
-
 const Compose = process.env.NODE_ENV === 'development' ? hot(Root) : Root;
 ReactDOM.render(<Compose />, document.getElementById('root'));

5071aa5ebb2620d5d3bb855dbd2cc3c8473b306c
Tejas <hegdetejass@gmail.com>
2022-08-22 07:32:39 -0500

    added comment for version update to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 
+
+// version has been updated from v3.1.3 to v3.8.2
 const Compose = process.env.NODE_ENV === 'development' ? hot(Root) : Root;
 ReactDOM.render(<Compose />, document.getElementById('root'));

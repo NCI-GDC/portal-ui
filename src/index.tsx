b8c92cc7de1bc23e87bc74e7d4ac43efbed6af1f
Tejas <hegdetejass@gmail.com>
2022-08-22 08:09:22 -0500

    updated typescript version in package and package lock

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 
 
-// version has been updated from v3.1.3 to v3.8.2
-// additional push
+// typescript version has been updated from v3.1.3 to v3.8.2
 const Compose = process.env.NODE_ENV === 'development' ? hot(Root) : Root;
 ReactDOM.render(<Compose />, document.getElementById('root'));

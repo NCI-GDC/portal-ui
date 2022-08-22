52fc71a2297ef626717cc0dd358cff9bdf557c13
Tejas <hegdetejass@gmail.com>
2022-08-22 07:42:01 -0500

    updated index.tsx for push

--- src/index.tsx
+++ src/index.tsx
@@ -7,5 +7,6 @@ import Root from './Root';
 
 
 // version has been updated from v3.1.3 to v3.8.2
+// additional push
 const Compose = process.env.NODE_ENV === 'development' ? hot(Root) : Root;
 ReactDOM.render(<Compose />, document.getElementById('root'));

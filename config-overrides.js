b2df00c65a5b843ff7c5eeec55b4d3de5d91e714
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2023-12-20 11:02:15 -0600

    a few more changes to the webpack's config

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,16 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireDefinePlugin = require('react-app-rewire-define-plugin');
-const rewireProvidePlugin = require('react-app-rewire-provide-plugin');
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const rewireSass = require('react-app-rewire-scss');
-const WebpackNotifierPlugin = require('webpack-notifier');
+const { injectBabelPlugin } = require("react-app-rewired");
+const rewireDefinePlugin = require("react-app-rewire-define-plugin");
+const rewireProvidePlugin = require("react-app-rewire-provide-plugin");
+const rewireReactHotLoader = require("react-app-rewire-hot-loader");
+const rewireSass = require("react-app-rewire-scss");
+const WebpackNotifierPlugin = require("webpack-notifier");
 
-const { version } = require('./package.json');
+const { version } = require("./package.json");
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
     [
-      'import-inspector',
+      "import-inspector",
       {
         serverSideRequirePath: false,
         webpackRequireWeakId: true,
@@ -21,26 +21,31 @@ module.exports = function override(config, env) {
 
   config = injectBabelPlugin(
     [
-      'relay',
+      "relay",
       {
         compat: true,
-        schema: 'data/schema.graphql',
+        schema: "data/schema.graphql",
       },
     ],
     config,
   );
 
+  if (env === 'production' && config.output && config.output.publicPath) {
+    config.output.publicPath = process.env.GDC_BASE;
+  }
+
   config = rewireDefinePlugin(config, env, {
     __VERSION__: JSON.stringify(version),
+    "process.env.PUBLIC_URL": JSON.stringify(
+      env === "production" ? process.env.GDC_BASE : "/",
+    ),
   });
 
   config = rewireProvidePlugin(config, env, {
-    React: 'react',
+    React: "react",
   });
 
-  config.plugins = config.plugins.concat(
-    new WebpackNotifierPlugin(),
-  );
+  config.plugins = config.plugins.concat(new WebpackNotifierPlugin());
 
   config = rewireSass(config, env);
   // config = rewireSass.withLoaderOptions(someLoaderOptions)(config, env);
@@ -52,7 +57,7 @@ module.exports = function override(config, env) {
   //   test: /\.js$/,
   // });
 
-  env === 'development' && (config.devtool = 'eval-source-map');
+  env === "development" && (config.devtool = "eval-source-map");
   config = rewireReactHotLoader(config, env);
 
   return config;
